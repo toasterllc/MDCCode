8e46e19805720b83579a9ca3ea91408257bf9afe
Dave <dave@toaster.llc>
2019-01-10 02:01:09 -0800

    Got stick working, successfully flashing LEDs with a flip flop

--- code/blink.sv
+++ code/blink.sv
@@ -1,13 +1,12 @@
-module main(
-    input logic clk,
-    input logic rst,
-    output logic out
-);
-    logic[31:0] ctr;
+module main(input clk, output led1, output led2, output led3, output led4, output led5);
+	logic[24:0] ctr;
     
-    always_ff @(posedge clk, negedge rst)
-        if (~rst) ctr <= 0;
-        else ctr <= ctr+1;
+	always_ff @(posedge clk)
+		ctr <= ctr + 1;
     
-    assign out = ctr[31];
+	assign led1 = ctr[19];
+	assign led2 = ctr[20];
+	assign led3 = ctr[21];
+	assign led4 = ctr[22];
+	assign led5 = ctr[23];
 endmodule
