c6491d95ab4618c11fdf827571739c91f1997489
Dave <dave@toaster.llc>
2019-01-24 03:48:10 -0800

    .txt -> .asc. Add .gitignore

--- code/BuildAndFlash.sh
+++ code/BuildAndFlash.sh
@@ -6,11 +6,11 @@ proj="Top"
 # Synthesize the design from Verilog (.sv -> .blif)
 yosys -p "synth_ice40 -top Top -blif $proj.blif" "$proj.sv"
 
-# Place and route the design ({.pcf, .blif} -> .txt)
-arachne-pnr -q -d 1k -o "$proj.txt" -p "$proj.pcf" "$proj.blif"
+# Place and route the design ({.pcf, .blif} -> .asc)
+arachne-pnr -q -d 1k -o "$proj.asc" -p "$proj.pcf" "$proj.blif"
 
-# Generate the bitstream file (.txt -> .bin)
-icepack "$proj.txt" "$proj.bin"
+# Generate the bitstream file (.asc -> .bin)
+icepack "$proj.asc" "$proj.bin"
 
 # Flash the bitstream (.bin)
 iceprog "$proj.bin"
