b0b290e96f514d428d7b2a7eb64e476f8f8e5b46
Dave <dave@toaster.llc>
2020-01-24 16:15:16 -0800

    FIFOTest: implemented both one-hot and integer implementations. One-hot is a lot better apparently.

--- code/FIFOTest/top.sv
+++ code/FIFOTest/top.sv
@@ -9,36 +9,60 @@ module FIFOTest(
     output logic[11:0]  q,
     output logic        qValid,
 );
-    localparam PixBufferSize = 3;
-    logic[(12*PixBufferSize)-1:0] pixBuffer;
-    logic[PixBufferSize-1:0] pixBufferValidData; // One-hot
+    // // One-hot implementation
+    // localparam PixBufferSize = 3;
+    // logic[(12*PixBufferSize)-1:0] pixBuffer;
+    // logic[PixBufferSize-1:0] pixBufferValidData; // One-hot
+    //
+    // always @(posedge pix_clk) begin
+    //     // Produce data
+    //     if (pix_frameValid && pix_lineValid) begin
+    //         if (!pixBufferValidData[0]) begin
+    //             pixBuffer[11:0] <= pix_d;
+    //             pixBufferValidData[0] <= 1;
+    //
+    //         end else if (!pixBufferValidData[1]) begin
+    //             pixBuffer[23:12] <= pix_d;
+    //             pixBufferValidData[1] <= 1;
+    //         end
+    //
+    //         end else if (!pixBufferValidData[2]) begin
+    //             pixBuffer[35:24] <= pix_d;
+    //             pixBufferValidData[2] <= 1;
+    //         end
+    //
+    //     // Consume data
+    //     else begin
+    //         pixBuffer <= pixBuffer>>12;
+    //         pixBufferValidData <= pixBufferValidData>>1;
+    //     end
+    // end
+    //
+    // assign q = pixBuffer[11:0];
+    // assign qValid = pixBufferValidData[0];
     
-    always @(posedge pix_clk) begin
-        // Produce data
-        if (pix_frameValid && pix_lineValid) begin
-            if (!pixBufferValidData[0]) begin
-                pixBuffer[11:0] <= pix_d;
-                pixBufferValidData[0] <= 1;
-            
-            end else if (!pixBufferValidData[1]) begin
-                pixBuffer[23:12] <= pix_d;
-                pixBufferValidData[1] <= 1;
-            end
-            
-            end else if (!pixBufferValidData[2]) begin
-                pixBuffer[35:24] <= pix_d;
-                pixBufferValidData[2] <= 1;
-            end
-        
-        // Consume data
-        else begin
-            pixBuffer <= pixBuffer>>12;
-            pixBufferValidData <= pixBufferValidData>>1;
-        end
-    end
-    
-    assign q = pixBuffer[11:0];
-    assign qValid = pixBufferValidData[0];
+    // // Integer implementation
+    // localparam PixBufferSize = 3;
+    // logic[(12*PixBufferSize)-1:0] pixBuffer;
+    // logic[$clog2(PixBufferSize)-1:0] pixBufferCount;
+    //
+    // always @(posedge pix_clk) begin
+    //     // Produce data
+    //     if (pix_frameValid && pix_lineValid) begin
+    //         if (pixBufferCount < PixBufferSize) begin
+    //             pixBuffer <= pixBuffer|(pix_d<<(pixBufferCount*12));
+    //             pixBufferCount <= pixBufferCount+1;
+    //         end
+    //
+    //     // Consume data
+    //     end else begin
+    //         pixBuffer <= pixBuffer>>12;
+    //         pixBufferCount <= (pixBufferCount>0 ? pixBufferCount-1 : 0);
+    //     end
+    // end
+    //
+    // assign q = pixBuffer[11:0];
+    // assign qValid = pixBufferCount>0;
 endmodule
 
 `ifdef SIM
