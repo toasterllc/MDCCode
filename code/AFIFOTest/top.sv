76052bbf613b0ede2903beac93e8c793dbfcd510
Dave <dave@toaster.llc>
2020-01-27 01:07:25 -0800

    AFIFOTest: almost ready to test on device

--- code/AFIFOTest/top.sv
+++ code/AFIFOTest/top.sv
@@ -4,23 +4,23 @@
 `define assert(cond) if (!(cond)) $error("Assertion failed: %s (%s:%0d)", `stringify(cond), `__FILE__, `__LINE__)
 
 module AFIFO(
-    input logic rclk,
-    input logic r,
-    output logic[Width-1:0] rd,
-    output logic rempty,
+    input wire rclk,
+    input wire r,
+    output wire[Width-1:0] rd,
+    output reg rempty,
     
-    input logic wclk,
-    input logic w,
-    input logic[Width-1:0] wd,
-    output logic wfull
+    input wire wclk,
+    input wire w,
+    input wire[Width-1:0] wd,
+    output reg wfull = 0
 );
     parameter Width = 12;
-    parameter Size = 4; // Must be a power of 2
+    parameter Size = 4; // Must be a power of 2 and >=4
     localparam N = $clog2(Size)-1;
     
-    logic[Width-1:0] mem[Size-1:0];
-    logic[N:0] rbaddr, rgaddr; // Read addresses (binary, gray)
-    logic[N:0] wbaddr, wgaddr; // Write addresses (binary, gray)
+    reg[Width-1:0] mem[Size-1:0];
+    reg[N:0] rbaddr=0, rgaddr=0; // Read addresses (binary, gray)
+    reg[N:0] wbaddr=0, wgaddr=0; // Write addresses (binary, gray)
     
     // ====================
     // Read handling
@@ -32,7 +32,7 @@ module AFIFO(
             rgaddr <= (rbaddrNext>>1)^rbaddrNext;
         end
     
-    logic rempty2;
+    reg rempty2 = 0;
     always @(posedge rclk, posedge aempty)
         // TODO: ensure that before the first clock, empty==true so outside entities don't think they can read
         if (aempty) {rempty, rempty2} <= 2'b11;
@@ -51,7 +51,7 @@ module AFIFO(
             wgaddr <= (wbaddrNext>>1)^wbaddrNext;
         end
     
-    logic wfull2;
+    reg wfull2 = 0;
     always @(posedge wclk, posedge afull)
         if (afull) {wfull, wfull2} <= 2'b11;
         else {wfull, wfull2} <= {wfull2, 1'b0};
@@ -59,7 +59,7 @@ module AFIFO(
     // ====================
     // Async signal generation
     // ====================
-    logic dir;
+    reg dir = 0;
     wire aempty = (rgaddr==wgaddr) & !dir;
     wire afull = (rgaddr==wgaddr) & dir;
     wire dirclr = (rgaddr[N]!=wgaddr[N-1]) & (rgaddr[N-1]==wgaddr[N]);
@@ -67,186 +67,175 @@ module AFIFO(
     always @(posedge dirclr, posedge dirset)
         if (dirclr) dir <= 0;
         else dir <= 1;
-    
-`ifdef SIM
-    initial begin
-        wfull = 0;
-        wfull2 = 0;
-        // logic[Width-1:0] rd
-        // rempty
-        // rempty2
-        for (int i=0; i<Size; i++)
-            mem[i] = 0;
-        
-        rbaddr = 0;
-        rgaddr = 0;
-        wbaddr = 0;
-        wgaddr = 0;
-        dir = 0;
-    end
-`endif
 endmodule
 
 
 
 
-// `ifndef SIM
-//
-// /**
-//  * PLL configuration
-//  *
-//  * This Verilog module was generated automatically
-//  * using the icepll tool from the IceStorm project.
-//  * Use at your own risk.
-//  *
-//  * Given input frequency:        12.000 MHz
-//  * Requested output frequency:   50.000 MHz
-//  * Achieved output frequency:    50.250 MHz
-//  */
-//
-// module wclkPLL(
-//     input  clock_in,
-//     output clock_out,
-//     output locked
-//     );
-//
-// SB_PLL40_CORE #(
-//         .FEEDBACK_PATH("SIMPLE"),
-//         .DIVR(4'b0000),        // DIVR =  0
-//         .DIVF(7'b1000010),    // DIVF = 66
-//         .DIVQ(3'b100),        // DIVQ =  4
-//         .FILTER_RANGE(3'b001)    // FILTER_RANGE = 1
-//     ) uut (
-//         .LOCK(locked),
-//         .RESETB(1'b1),
-//         .BYPASS(1'b0),
-//         .REFERENCECLK(clock_in),
-//         .PLLOUTCORE(clock_out)
-//         );
-//
-// endmodule
-//
-//
-//
-//
-// /**
-//  * PLL configuration
-//  *
-//  * This Verilog module was generated automatically
-//  * using the icepll tool from the IceStorm project.
-//  * Use at your own risk.
-//  *
-//  * Given input frequency:        12.000 MHz
-//  * Requested output frequency:   90.000 MHz
-//  * Achieved output frequency:    90.000 MHz
-//  */
-//
-// module rclkPll(
-//     input  clock_in,
-//     output clock_out,
-//     output locked
-//     );
-//
-// SB_PLL40_CORE #(
-//         .FEEDBACK_PATH("SIMPLE"),
-//         .DIVR(4'b0000),        // DIVR =  0
-//         .DIVF(7'b0111011),    // DIVF = 59
-//         .DIVQ(3'b011),        // DIVQ =  3
-//         .FILTER_RANGE(3'b001)    // FILTER_RANGE = 1
-//     ) uut (
-//         .LOCK(locked),
-//         .RESETB(1'b1),
-//         .BYPASS(1'b0),
-//         .REFERENCECLK(clock_in),
-//         .PLLOUTCORE(clock_out)
-//         );
-//
-// endmodule
-//
-// `endif
+`ifndef SIM
+
+/**
+ * PLL configuration
+ *
+ * This Verilog module was generated automatically
+ * using the icepll tool from the IceStorm project.
+ * Use at your own risk.
+ *
+ * Given input frequency:        12.000 MHz
+ * Requested output frequency:   50.000 MHz
+ * Achieved output frequency:    50.250 MHz
+ */
+
+module wclkPLL(
+    input  clock_in,
+    output clock_out,
+    output locked
+    );
+
+SB_PLL40_CORE #(
+        .FEEDBACK_PATH("SIMPLE"),
+        .DIVR(4'b0000),        // DIVR =  0
+        .DIVF(7'b1000010),    // DIVF = 66
+        .DIVQ(3'b100),        // DIVQ =  4
+        .FILTER_RANGE(3'b001)    // FILTER_RANGE = 1
+    ) uut (
+        .LOCK(locked),
+        .RESETB(1'b1),
+        .BYPASS(1'b0),
+        .REFERENCECLK(clock_in),
+        .PLLOUTCORE(clock_out)
+        );
+
+endmodule
 
 
-// module AFIFOTest(input logic clk12mhz);
-//     logic rrst_;
-//     logic wrst_;
-//
-//     logic wclk;
-//     logic w;
-//     logic[11:0] wd;
-//     logic wfull;
-//     logic rclk;
-//     logic r;
-//     logic[11:0] rd;
-//     logic rempty;
-//
-//     AFIFO afifo(.*);
-//
-// `ifdef SIM
-//     initial begin
-//         $dumpfile("top.vcd");
-//         $dumpvars(0, AFIFOTest);
-//         #10000000;
-//         $finish;
-//     end
-//
-//     // rclk
-//     initial begin
-//         rclk = 0;
-//         #7;
-//         forever begin
-//             rclk = !rclk;
-//             #3;
-//         end
-//     end
-//
-//     // wclk
-//     initial begin
-//         wclk = 0;
-//         forever begin
-//             wclk = !wclk;
-//             #42;
-//         end
-//     end
-// `else
-//     wclkPLL pll1(.clock_in(clk12mhz), .clock_out(wclk));
-//     rclkPLL pll2(.clock_in(clk12mhz), .clock_out(rclk));
-// `endif
-//
-//     // Produce values
-//     always @(posedge wclk, negedge wrst_) begin
-//         if (!wrst_) begin
-//             w <= 1;
-//             wd <= 0;
-//         end else if (!wfull) begin
-//             // We wrote a value, continue to the next one
-//             wd <= wd+1'b1;
-//         end
-//     end
-//
-//     // Consume values
-//     logic[11:0] rval;
-//     logic rvalValid;
-//     logic rok;
-//     always @(posedge rclk, negedge rrst_) begin
-//         if (!rrst_) begin
-//             rvalValid <= 0;
-//             rok <= 1;
-//
-//         end else if (rok) begin
-//             if (!rempty) begin
-//                 rval <= rd;
-//                 rvalValid <= 1;
-//
-//                 $display("Read value: %h", rd);
-//
-//                 // Check if the current value is the previous value +1
-//                 if (rvalValid & (rd!=(rval+1'b1))) begin
-//                     rok <= 0;
-//                 end
-//             end
-//         end
-//     end
-// endmodule
+
+
+/**
+ * PLL configuration
+ *
+ * This Verilog module was generated automatically
+ * using the icepll tool from the IceStorm project.
+ * Use at your own risk.
+ *
+ * Given input frequency:        12.000 MHz
+ * Requested output frequency:   90.000 MHz
+ * Achieved output frequency:    90.000 MHz
+ */
+
+module rclkPll(
+    input  clock_in,
+    output clock_out,
+    output locked
+    );
+
+SB_PLL40_CORE #(
+        .FEEDBACK_PATH("SIMPLE"),
+        .DIVR(4'b0000),        // DIVR =  0
+        .DIVF(7'b0111011),    // DIVF = 59
+        .DIVQ(3'b011),        // DIVQ =  3
+        .FILTER_RANGE(3'b001)    // FILTER_RANGE = 1
+    ) uut (
+        .LOCK(locked),
+        .RESETB(1'b1),
+        .BYPASS(1'b0),
+        .REFERENCECLK(clock_in),
+        .PLLOUTCORE(clock_out)
+        );
+
+endmodule
+
+`endif
+
+
+module AFIFOTest(input logic clk12mhz);
+    reg rclk = 0;
+    reg wclk = 0;
+    
+    reg r = 0;
+    wire[11:0] rd;
+    wire rempty;
+    
+    reg w = 0;
+    reg[11:0] wd;
+    wire wfull;
+    
+    AFIFO afifo(
+        .rclk(rclk),
+        .r(r),
+        .rd(rd),
+        .rempty(rempty),
+        .wclk(wclk),
+        .w(w),
+        .wd(wd),
+        .wfull(wfull)
+    );
+
+`ifdef SIM
+    initial begin
+        $dumpfile("top.vcd");
+        $dumpvars(0, AFIFOTest);
+        #10000000;
+        $finish;
+    end
+    
+    // rclk
+    initial begin
+        #7;
+        forever begin
+            rclk = !rclk;
+            #3;
+        end
+    end
+
+    // wclk
+    initial begin
+        forever begin
+            wclk = !wclk;
+            #42;
+        end
+    end
+`else
+    wclkPLL pll1(.clock_in(clk12mhz), .clock_out(wclk));
+    rclkPLL pll2(.clock_in(clk12mhz), .clock_out(rclk));
+`endif
+
+    // Produce values
+    always @(posedge wclk) begin
+        // Init
+        if (!w) begin
+            w <= 1;
+            wd <= 0;
+        end else if (!wfull) begin
+            $display("Wrote value: %h", wd);
+            
+            // We wrote a value, continue to the next one
+            wd <= wd+1'b1;
+        end
+    end
+
+    // Consume values
+    reg[11:0] rval;
+    reg rvalValid = 0;
+    reg rfail = 0;
+    always @(posedge rclk) begin
+        // Init
+        if (!r) begin
+            r <= 1;
+        
+        end else if (!rfail & !rempty) begin
+            $display("Read value: %h", rd);
+            rval <= rd;
+            rvalValid <= 1;
+            
+            // Check if the current value is the previous value +1
+            `assert(!rvalValid | ((rval+1'b1)==rd));
+            if (rvalValid & ((rval+1'b1)!=rd)) begin
+                rfail <= 1;
+            end
+        end
+    end
+endmodule
 
 
 
