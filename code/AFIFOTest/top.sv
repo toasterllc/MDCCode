632caae7db9fe2987b13c507ed2504b252d68c7e
Dave <dave@toaster.llc>
2020-01-27 00:02:16 -0800

    Undo adding resets to AFIFO

--- code/AFIFOTest/top.sv
+++ code/AFIFOTest/top.sv
@@ -1,17 +1,14 @@
 `timescale 1ns/1ps
-`include "../GrayCode.v"
 
 `define stringify(x) `"x```"
 `define assert(cond) if (!(cond)) $error("Assertion failed: %s (%s:%0d)", `stringify(cond), `__FILE__, `__LINE__)
 
 module AFIFO(
-    input logic rrst_,
     input logic rclk,
     input logic r,
     output logic[Width-1:0] rd,
     output logic rempty,
     
-    input logic wrst_,
     input logic wclk,
     input logic w,
     input logic[Width-1:0] wd,
@@ -29,11 +26,8 @@ module AFIFO(
     // Read handling
     // ====================
     wire[N:0] rbaddrNext = rbaddr+1'b1;
-    always @(posedge rclk, negedge rrst_)
-        if (!rrst_) begin
-            rbaddr <= 0;
-            rgaddr <= 0;
-        end else if (r & !rempty) begin
+    always @(posedge rclk)
+        if (r & !rempty) begin
             rbaddr <= rbaddrNext;
             rgaddr <= (rbaddrNext>>1)^rbaddrNext;
         end
@@ -50,20 +44,16 @@ module AFIFO(
     // Write handling
     // ====================
     wire[N:0] wbaddrNext = wbaddr+1'b1;
-    always @(posedge wclk, negedge wrst_)
-        if (!wrst_) begin
-            wbaddr <= 0;
-            wgaddr <= 0;
-        end else if (w & !wfull) begin
+    always @(posedge wclk)
+        if (w & !wfull) begin
             mem[wbaddr] <= wd;
             wbaddr <= wbaddrNext;
             wgaddr <= (wbaddrNext>>1)^wbaddrNext;
         end
     
     logic wfull2;
-    always @(posedge wclk, posedge afull, negedge wrst_)
-        if (!wrst_) {wfull, wfull2} <= 2'b00;
-        else if (afull) {wfull, wfull2} <= 2'b11;
+    always @(posedge wclk, posedge afull)
+        if (afull) {wfull, wfull2} <= 2'b11;
         else {wfull, wfull2} <= {wfull2, 1'b0};
     
     // ====================
@@ -72,91 +62,108 @@ module AFIFO(
     logic dir;
     wire aempty = (rgaddr==wgaddr) & !dir;
     wire afull = (rgaddr==wgaddr) & dir;
-    wire dirset = (rgaddr[N]==wgaddr[N-1]) & (rgaddr[N-1]!=wgaddr[N]);
     wire dirclr = (rgaddr[N]!=wgaddr[N-1]) & (rgaddr[N-1]==wgaddr[N]);
-    always @(posedge dirset, posedge dirclr, negedge wrst_)
-        if (!wrst_) dir <= 0;
-        else if (dirset) dir <= 1;
-        else dir <= 0;
-endmodule
-
-
-
-
-`ifndef SIM
-
-/**
- * PLL configuration
- *
- * This Verilog module was generated automatically
- * using the icepll tool from the IceStorm project.
- * Use at your own risk.
- *
- * Given input frequency:        12.000 MHz
- * Requested output frequency:   50.000 MHz
- * Achieved output frequency:    50.250 MHz
- */
-
-module wclkPLL(
-    input  clock_in,
-    output clock_out,
-    output locked
-    );
-
-SB_PLL40_CORE #(
-        .FEEDBACK_PATH("SIMPLE"),
-        .DIVR(4'b0000),        // DIVR =  0
-        .DIVF(7'b1000010),    // DIVF = 66
-        .DIVQ(3'b100),        // DIVQ =  4
-        .FILTER_RANGE(3'b001)    // FILTER_RANGE = 1
-    ) uut (
-        .LOCK(locked),
-        .RESETB(1'b1),
-        .BYPASS(1'b0),
-        .REFERENCECLK(clock_in),
-        .PLLOUTCORE(clock_out)
-        );
-
-endmodule
-
-
-
-
-/**
- * PLL configuration
- *
- * This Verilog module was generated automatically
- * using the icepll tool from the IceStorm project.
- * Use at your own risk.
- *
- * Given input frequency:        12.000 MHz
- * Requested output frequency:   90.000 MHz
- * Achieved output frequency:    90.000 MHz
- */
-
-module rclkPll(
-    input  clock_in,
-    output clock_out,
-    output locked
-    );
-
-SB_PLL40_CORE #(
-        .FEEDBACK_PATH("SIMPLE"),
-        .DIVR(4'b0000),        // DIVR =  0
-        .DIVF(7'b0111011),    // DIVF = 59
-        .DIVQ(3'b011),        // DIVQ =  3
-        .FILTER_RANGE(3'b001)    // FILTER_RANGE = 1
-    ) uut (
-        .LOCK(locked),
-        .RESETB(1'b1),
-        .BYPASS(1'b0),
-        .REFERENCECLK(clock_in),
-        .PLLOUTCORE(clock_out)
-        );
-
-endmodule
-
+    wire dirset = (rgaddr[N]==wgaddr[N-1]) & (rgaddr[N-1]!=wgaddr[N]);
+    always @(posedge dirclr, posedge dirset)
+        if (dirclr) dir <= 0;
+        else dir <= 1;
+    
+`ifdef SIM
+    initial begin
+        wfull = 0;
+        wfull2 = 0;
+        // logic[Width-1:0] rd
+        // rempty
+        // rempty2
+        for (int i=0; i<Size; i++)
+            mem[i] = 0;
+        
+        rbaddr = 0;
+        rgaddr = 0;
+        wbaddr = 0;
+        wgaddr = 0;
+        dir = 0;
+    end
 `endif
+endmodule
+
+
+
+
+// `ifndef SIM
+//
+// /**
+//  * PLL configuration
+//  *
+//  * This Verilog module was generated automatically
+//  * using the icepll tool from the IceStorm project.
+//  * Use at your own risk.
+//  *
+//  * Given input frequency:        12.000 MHz
+//  * Requested output frequency:   50.000 MHz
+//  * Achieved output frequency:    50.250 MHz
+//  */
+//
+// module wclkPLL(
+//     input  clock_in,
+//     output clock_out,
+//     output locked
+//     );
+//
+// SB_PLL40_CORE #(
+//         .FEEDBACK_PATH("SIMPLE"),
+//         .DIVR(4'b0000),        // DIVR =  0
+//         .DIVF(7'b1000010),    // DIVF = 66
+//         .DIVQ(3'b100),        // DIVQ =  4
+//         .FILTER_RANGE(3'b001)    // FILTER_RANGE = 1
+//     ) uut (
+//         .LOCK(locked),
+//         .RESETB(1'b1),
+//         .BYPASS(1'b0),
+//         .REFERENCECLK(clock_in),
+//         .PLLOUTCORE(clock_out)
+//         );
+//
+// endmodule
+//
+//
+//
+//
+// /**
+//  * PLL configuration
+//  *
+//  * This Verilog module was generated automatically
+//  * using the icepll tool from the IceStorm project.
+//  * Use at your own risk.
+//  *
+//  * Given input frequency:        12.000 MHz
+//  * Requested output frequency:   90.000 MHz
+//  * Achieved output frequency:    90.000 MHz
+//  */
+//
+// module rclkPll(
+//     input  clock_in,
+//     output clock_out,
+//     output locked
+//     );
+//
+// SB_PLL40_CORE #(
+//         .FEEDBACK_PATH("SIMPLE"),
+//         .DIVR(4'b0000),        // DIVR =  0
+//         .DIVF(7'b0111011),    // DIVF = 59
+//         .DIVQ(3'b011),        // DIVQ =  3
+//         .FILTER_RANGE(3'b001)    // FILTER_RANGE = 1
+//     ) uut (
+//         .LOCK(locked),
+//         .RESETB(1'b1),
+//         .BYPASS(1'b0),
+//         .REFERENCECLK(clock_in),
+//         .PLLOUTCORE(clock_out)
+//         );
+//
+// endmodule
+//
+// `endif
 
 
 // module AFIFOTest(input logic clk12mhz);
