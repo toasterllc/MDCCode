13dbe3f68ccc99b5e07c3bdea35edddd9069cc5a
Dave <dave@toaster.llc>
2020-01-28 22:24:16 -0800

    Icestick_AFIFOProducer: add rst signal

--- code/Icestick_AFIFOProducer/top.v
+++ code/Icestick_AFIFOProducer/top.v
@@ -1,37 +1,53 @@
 `timescale 1ns/1ps
+`include "../ClockGen.v"
 
 module Icestick_AFIFOProducer(
     input wire clk12mhz,
-    output wire clk,
-    output reg w = 0,
-    output reg[11:0] wd = 0
+    output wire wclk,
+    output reg w,
+    output reg[11:0] wd
 );
     
-`ifdef SIM
-    reg[7:0] clkDivider = 0;
-`else
-    reg[8:0] clkDivider = 0; // 23kHz
-`endif
+    wire clk;
+    wire rst;
     
-    always @(posedge clk12mhz) clkDivider <= clkDivider+1;
-    assign clk = clkDivider[$size(clkDivider)-1];
+    // 16 MHz clock
+    ClockGen #(
+        .FREQ(16),
+		.DIVR(0),
+		.DIVF(84),
+		.DIVQ(6),
+		.FILTER_RANGE(1)
+    ) cg(.clk12mhz(clk12mhz), .clk(clk), .rst(rst));
     
-    reg[1:0] rstCounter = 0;
-    wire rst = !(&rstCounter);
-    always @(posedge clk)
-        if (rst)
-            rstCounter <= rstCounter+1;
+    reg[8:0] wclkCounter;
+    assign wclk = wclkCounter[$size(wclkCounter)-1];
+    reg wclkLast;
     
     // Produce values
     always @(posedge clk) begin
-        if (!rst) begin
-            if (w) begin
-                // We wrote a value, continue to the next one
-                $display("Wrote value: %h", wd);
+        if (rst) begin
+            wclkCounter <= 0;
+            w <= 0;
+            wd <= 0;
+            wclkLast <= 0;
+        
+        end else begin
+            if (wclk & !wclkLast) begin
+                if (w) begin
+                    // We wrote a value, continue to the next one
+                    $display("Wrote value: %h", wd);
+                end
+                
+                w <= 1;
+                wd <= wd+1'b1;
+                wclkLast <= 1;
+            end else if (!wclk & wclkLast) begin
+                wclkLast <= 0;
             end
             
-            w <= 1;
-            wd <= wd+1'b1;
+            wclkCounter <= wclkCounter+1;
         end
     end
+    
 endmodule
