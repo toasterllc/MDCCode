b3a9459646ee0951b97b42d5cc1d53087d64ed40
Dave <dave@toaster.llc>
2020-06-29 16:13:09 -0700

    Add code/Rev2Board_CopyImage, code/Rev2Board_BlinkyDebug, tools/MDCDebugger

--- code/Rev2Board_Blinky/top.v
+++ code/Rev2Board_Blinky/top.v
@@ -4,15 +4,12 @@ module Top(
     input wire clk12mhz,
     output wire[3:0] led
 );
-        wire clk = clk12mhz;
-
-    reg[20:0] counter = 0;
-        always @(posedge clk) begin
-        counter <= counter+1;
-        end
-
-    assign led[3:0] = {4{counter[$size(counter)-1]}};
-
-    // assign led[3:0] = 4'b1111;
+    wire clk = clk12mhz;
     
+    reg[20:0] counter = 0;
+    always @(posedge clk) begin
+        counter <= counter+1;
+    end
+    
+    assign led[3:0] = {4{counter[$size(counter)-1]}};
 endmodule
