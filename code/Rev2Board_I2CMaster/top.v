81c38d9dbf1958be3cea75fa34449242a5aebbd3
Dave <dave@toaster.llc>
2020-07-05 00:17:37 -0700

    Rev2Board_I2CMaster: figuring out Clocks()/Delay() code

--- code/Rev2Board_I2CMaster/top.v
+++ code/Rev2Board_I2CMaster/top.v
@@ -19,17 +19,48 @@ module I2CMaster #(
     output reg          i2c_clk = 0,
     inout wire          i2c_data
 );
+    // Delay() returns the value to store in a counter, such that when
+    // the counter reaches 0, the given time has elapsed.
+    // `sub` is subtracted from that value, with the result clipped to zero.
+    function [63:0] Delay;
+        input [63:0] t;
+        input [63:0] sub;
+        begin
+            Delay = (t*ClkFreq)/1000000000;
+            if (Delay >= sub) Delay = Delay-sub;
+            else Delay = 0;
+        end
+    endfunction
+    
+    
+    
+    // √√√ problem 1: what about fractional nanoseconds supplied to Clocks()?
+    //      we should make sure that we ceil the conversion from i2c freq -> nanoseconds
+    
+    // problem 2: i think we actually want Clocks() to return the value to load into the counter.
+    //      otherwise, we have to modify the value in 2 places to account for the clock cycle transitioning to the next state:
+    //          1. when calculating the width of the delay register
+    //          2. when loading the delay into the delay register
+    //      by having Clocks() return the value that should be loaded into the delay register,
+    //      we don't have to modify it in 2 separate places.
+    localparam NSecPerSec = 1000000000;
     // Clocks() returns the number of clock cycles required for >= `t` nanoseconds to elapse.
     // `sub` is subtracted from that value, with the result clipped to zero.
     function [63:0] Clocks;
         input [63:0] t;
         input [63:0] sub;
-        
-        localparam NSecPerSec = 1000000000;
         begin
             Clocks = (t*ClkFreq+NSecPerSec-1)/NSecPerSec;
             if (Clocks >= sub) Clocks = Clocks-sub;
             else Clocks = 0;
+        end
+    endfunction
+    
+    function [63:0] CeilDiv;
+        input [63:0] a;
+        input [63:0] b;
+        begin
+            CeilDiv = (a+b-1)/b;
         end
     endfunction
     
@@ -42,9 +73,12 @@ module I2CMaster #(
         end
     endfunction
     
+    
+    
     // Number of `clk` cycles for half of the `i2c_clk` cycle.
     // In other words, this is how often we need to toggle `i2c_clk`.
-    localparam I2CHalfCycleClocks = Clocks(I2CClkFreq/2, 0);
+    // -1 since one clock cycle is burned by the state machine delay mec
+    localparam I2CHalfCycleDelay = Clocks(NSecPerSec/(2*I2CClkFreq), 1);
     
     // Width of `delay`
     localparam DelayWidth = $clog2(I2CHalfCycleClocks+1);
@@ -87,6 +121,15 @@ module I2CMaster #(
             if (cmd_trigger) begin
                 // Start condition
                 i2c_dataOut <= 0;
+                
+                delay <= 
+                state <= 2;
+            end
+        end
+        
+        2: begin
+            if (delay) begin
+                delay <= delay-1;
             end
         end
         endcase
