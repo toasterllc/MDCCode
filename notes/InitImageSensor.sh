102a37046865091d724f725b93abba6147a05576
Dave <dave@toaster.llc>
2020-07-19 06:44:09 -0700

    Finally got test pattern working (needed to set a special register to enable parallel mode, not mentioned in the datasheet -- 0x31AE=0x0301)

--- notes/InitImageSensor.sh
+++ notes/InitImageSensor.sh
@@ -18,6 +18,7 @@ sudo ./MDCDebugger pixreg16 0x301A=0x10D8
 # pre_pll_clk_div=2     ->  CLK_OP=98 MHz
 # pre_pll_clk_div=4     ->  CLK_OP=49 MHz
 sudo ./MDCDebugger pixreg16 0x302E=0x0004
+# sudo ./MDCDebugger pixreg16 0x302E=0x0002
 
 # Set output slew rate
 # Slow              = 0x0010
@@ -25,6 +26,22 @@ sudo ./MDCDebugger pixreg16 0x302E=0x0004
 # Fast              = 0xFC10
 sudo ./MDCDebugger pixreg16 0x306E=0x9010
 # sudo ./MDCDebugger pixreg16 0x306E=0x4810
+# sudo ./MDCDebugger pixreg16 0x306E=0xFC10
+
+# Set test_data_red
+sudo ./MDCDebugger pixreg16 0x3072=0x0B2A   # AAA
+
+# Set test_data_greenr
+sudo ./MDCDebugger pixreg16 0x3074=0x0C3B   # BBB
+
+# Set test_data_blue
+sudo ./MDCDebugger pixreg16 0x3076=0x0D4C   # CCC
+
+# Set test_data_greenb
+sudo ./MDCDebugger pixreg16 0x3078=0x0E5D   # DDD
+
+# # Set op_pix_clk_div
+# sudo ./MDCDebugger pixreg16 0x3036=0x000A
 
 # Set test_pattern_mode
 # 0: Normal operation (generate output data from pixel array)
@@ -32,43 +49,66 @@ sudo ./MDCDebugger pixreg16 0x306E=0x9010
 # 2: Full color bar test pattern
 # 3: Fade-to-gray color bar test pattern
 # 256: Walking 1s test pattern (12 bit)
-sudo ./MDCDebugger pixreg16 0x3070=0x0001
+# sudo ./MDCDebugger pixreg16 0x3070=0x0000       # Normal operation
+sudo ./MDCDebugger pixreg16 0x3070=0x0001       # Solid color
+# sudo ./MDCDebugger pixreg16 0x3070=0x0002       # Color bars
+# sudo ./MDCDebugger pixreg16 0x3070=0x0003       # Fade-to-gray
+# sudo ./MDCDebugger pixreg16 0x3070=0x0100       # Walking 1s
 
-# Set test_data_red
-sudo ./MDCDebugger pixreg16 0x3072=0x0000
+# Set data_pedestal
+# sudo ./MDCDebugger pixreg16 0x301E=0x00A8           # Default
+# sudo ./MDCDebugger pixreg16 0x301E=0x0000
 
-# Set test_data_greenr
-sudo ./MDCDebugger pixreg16 0x3074=0x0000
+# Set serial_format
+# *** This register write is necessary for parallel mode.
+# *** The datasheet doesn't mention this. :(
+# *** Discovered looking at Linux kernel source.
+sudo ./MDCDebugger pixreg16 0x31AE=0x0301
 
-# Set test_data_blue
-sudo ./MDCDebugger pixreg16 0x3076=0x0000
-
-# Set test_data_greenb
-sudo ./MDCDebugger pixreg16 0x3078=0x0000
+# Set data_format_bits
+# Datasheet:
+#   "The serial format should be configured using R0x31AC.
+#   This register should be programmed to 0x0C0C when
+#   using the parallel interface."
+sudo ./MDCDebugger pixreg16 0x31AC=0x0C0C
 
 # Set row_speed
 # 0 cycle delay             = 0x0000
 # 1/2 cycle delay (default) = 0x0010
-# sudo ./MDCDebugger pixreg16 0x3028=0x0000
-sudo ./MDCDebugger pixreg16 0x3028=0x0010
+# sudo ./MDCDebugger pixreg16 0x3028=0x0000       # 0 cycle delay
+# sudo ./MDCDebugger pixreg16 0x3028=0x0010       # 1/2 cycle delay (default)
 
 # # Set the x-start address
 # # Default = 0x0006
-# sudo ./MDCDebugger pixreg16 0x3004=0x000e
+# # sudo ./MDCDebugger pixreg16 0x3004=0x0006       # Default
+# sudo ./MDCDebugger pixreg16 0x3004=0x0010
 #
 # # Set the x-end address
 # # Default = 0x0905
-# sudo ./MDCDebugger pixreg16 0x3008=0x0900
-# 
+# # sudo ./MDCDebugger pixreg16 0x3008=0x0905       # Default
+# sudo ./MDCDebugger pixreg16 0x3008=0x0060
+#
+# # Set the y-start address
+# # Default = 0x007C
+# # sudo ./MDCDebugger pixreg16 0x3002=0x007C       # Default
+# sudo ./MDCDebugger pixreg16 0x3002=0x007C
+#
 # # Set the y-end address
 # # Default = 0x058b
-# sudo ./MDCDebugger pixreg16 0x3006=0x007C
+# # sudo ./MDCDebugger pixreg16 0x3006=0x058b       # Default
+# sudo ./MDCDebugger pixreg16 0x3006=0x00CC
 
 # Implement "Recommended Default Register Changes and Sequencer"
 sudo ./MDCDebugger pixreg16 0x3ED2=0x0146
 sudo ./MDCDebugger pixreg16 0x3EDA=0x88BC
 sudo ./MDCDebugger pixreg16 0x3EDC=0xAA63
 sudo ./MDCDebugger pixreg16 0x305E=0x00A0
+
+# Disable embedded_data (first 2 rows of statistic info)
+# See AR0134_RR_D.pdf for info on statistics format
+# Stats enabled (default)   = 0x1902
+# Stats disabled            = 0x1802
+sudo ./MDCDebugger pixreg16 0x3064=0x1802
 
 # Start streaming
 # (Previous value of 0x301A is 0x10D8, as set above)
