53e7dfa63bed68af36d853e109e90f00fd285135
Dave <dave@toaster.llc>
2020-08-28 15:22:00 -0700

    Better ShiftAdder implementation

--- code/FastCounterTest/top.v
+++ code/FastCounterTest/top.v
@@ -28,43 +28,137 @@ endmodule
 //     assign cout = s[4];
 // endmodule
 
+// module ShiftAdder #(
+//     parameter W = 16,
+//     parameter N = 4
+// )(
+//     input wire clk,
+//     input wire load_,
+//     input wire[W-1:0] a,
+//     input wire[W-1:0] b,
+//     output reg[W-1:0] sum = 0,
+//     output wire done
+// );
+//     reg[W/N:0] doneReg = 0;
+//     reg[W-1:0] aReg = 0;
+//     reg[W-1:0] bReg = 0;
+//     reg cin = 0;
+//     // reg[W-1:0] sumReg = 0;
+//
+//     wire[N:0] s = aReg[N-1:0] + bReg[N-1:0] + cin;
+//     wire[N-1:0] sumPart = s[N-1:0];
+//     wire cout = s[N];
+//
+//     assign done = doneReg[W/N];
+//     always @(posedge clk) begin
+//         // sumReg <= {sumPart, sumReg[W-1:N]};
+//         // if (doneReg[(W/N)-1]) sum <= {sumPart, sumReg[W-1:N]};
+//
+//         if (!load_) begin
+//             doneReg <= 1;
+//             aReg <= a;
+//             bReg <= b;
+//             cin <= 0;
+//
+//         end else begin
+//             aReg <= aReg>>N;
+//             bReg <= bReg>>N;
+//             cin <= cout;
+//         end
+//
+//         if (!done) begin
+//             doneReg <= doneReg<<1;
+//             sum <= {sumPart, sum[W-1:N]};
+//         end
+//     end
+// endmodule
+
+
+
+
+// module ShiftAdder #(
+//     parameter W = 16,
+//     parameter N = 4
+// )(
+//     input wire clk,
+//     input wire load_,
+//     input wire[15:0] a,
+//     input wire[15:0] b,
+//     output reg[15:0] sum = 0,
+//     output wire done
+// );
+//     reg[4:0] doneReg = 0;
+//     reg[15:0] aReg = 0;
+//     reg[15:0] bReg = 0;
+//     reg cin = 0;
+//     // reg[15:0] sumReg = 0;
+//
+//     wire[4:0] s = aReg[4-1:0] + bReg[4-1:0] + cin;
+//     wire[4-1:0] sumPart = s[4-1:0];
+//     wire cout = s[4];
+//
+//     assign done = doneReg[4];
+//     always @(posedge clk) begin
+//         // sumReg <= {sumPart, sumReg[15:4]};
+//         // if (doneReg[(4)-1]) sum <= {sumPart, sumReg[15:4]};
+//
+//         if (!load_) begin
+//             doneReg <= 1;
+//             aReg <= a;
+//             bReg <= b;
+//             cin <= 0;
+//
+//         end else begin
+//             aReg <= aReg>>4;
+//             bReg <= bReg>>4;
+//             cin <= cout;
+//         end
+//
+//         if (!done) begin
+//             doneReg <= doneReg<<1;
+//             sum <= {sumPart, sum[15:4]};
+//         end
+//     end
+// endmodule
+
+
+
+
+
 module ShiftAdder #(
-    parameter W = 16,
-    parameter N = 4
+    parameter W = 16,   // Total width
+    parameter N = 4     // Width of a single adder
 )(
     input wire clk,
-    input wire load_,
     input wire[W-1:0] a,
     input wire[W-1:0] b,
-    output reg[W-1:0] sum = 0,
-    output wire done
+    output reg[W-1:0] sum = 0
 );
-    reg[W/N:0] doneReg = 0;
-    reg[W-1:0] aReg = 0;
-    reg[W-1:0] bReg = 0;
-    reg cin = 0;
+    localparam S = W/N; // Number of adders
+    genvar i;
+    reg[S-1:0] cin = 0;
+    wire[W-1:0] sumParts;
+    wire[S-1:0] cout;
+    for (i=0; i<S; i=i+1) begin
+        Adder #(
+            .N(N)
+        ) adder (
+            .a(a[((i+1)*N)-1 : i*N]),
+            .b(b[((i+1)*N)-1 : i*N]),
+            .cin(cin[i]),
+            .sum(sumParts[((i+1)*N)-1 : i*N]),
+            .cout(cout[i])
+        );
+    end
     
-    wire[N:0] s = aReg[N-1:0] + bReg[N-1:0] + cin;
-    wire[N-1:0] sumPart = s[N-1:0];
-    wire cout = s[N];
-    
-    assign done = doneReg[W/N];
     always @(posedge clk) begin
-        if (!load_) begin
-            doneReg <= 1;
-            aReg <= a;
-            bReg <= b;
-            cin <= 0;
-        
-        end else begin
-            doneReg <= doneReg<<1;
-            aReg <= aReg>>N;
-            bReg <= bReg>>N;
-            cin <= cout;
-            sum <= {sumPart, sum[W-1:N]};
-        end
+        cin[S-1:1] <= cout[S-2:0];
+        sum <= sumParts;
     end
 endmodule
+
+
+
 
 
 module Top(
@@ -72,42 +166,58 @@ module Top(
     input wire          clk12mhz,
 `endif
     input wire[15:0]    a,
-    output wire[15:0]   out,
-    
-    output reg test = 0
+    output wire[15:0]   out
 );
 `ifdef SIM
     reg clk12mhz = 0;
 `endif
-    localparam W = 32;
-    localparam N = 4;
+    localparam W = 16;
+    localparam N = 1;
     
-    reg load_ = 0;
-    wire done;
-    reg[W-1:0] acum = 0;
     wire[W-1:0] sum;
-    assign out = acum[W-1 : W-16-1];
+    reg[W-1:0] accum = 0;
+    assign out = accum[W-1 : W-16];
     ShiftAdder #(
         .W(W),
         .N(N)
     ) adder(
         .clk(clk12mhz),
-        .load_(load_),
-        .a(acum),
-        .b(a),
-        .sum(sum),
-        .done(done)
+        .a(a),
+        .b(accum),
+        .sum(sum)
     );
 
+    reg init = 0;
+    reg[(W/N)-1:0] counter = 0;
     always @(posedge clk12mhz) begin
-        if (!load_) load_ <= 1;
-        else if (done) begin
-            load_ <= 0;
-            acum <= sum;
+        counter <= counter<<1 | (!init) | counter[(W/N)-1];
+        init <= 1;
+        if (counter[(W/N)-1]) begin
+            accum <= sum;
+            $display("accum: %d", accum);
         end
-        $display("acum: %b", acum);
     end
+    
+    
+    
 
+    // wire[W-1:0] sum;
+    // reg[W-1:0] accum = 0;
+    // assign out = accum[W-1 : W-16];
+    // always @(posedge clk12mhz) begin
+    //     accum <= a+accum;
+    //     $display("accum: %d", accum);
+    // end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     // reg[W-1:0] acum = 0;
@@ -231,7 +341,8 @@ module Top(
     
 `ifdef SIM
     // assign a = 16'b1111_1111_1111_1111;
-    assign a = 16'b0000_0000_0000_0001;
+    // assign b = 16'b0000_0000_0000_0011;
+    assign a = 16'd32;
     
     initial begin
         $dumpfile("top.vcd");
