7a4215e9c5b7ba299e9831c830fa546bca7ac736
Dave <dave@toaster.llc>
2020-09-02 01:16:32 -0700

    Revert "Experimenting with new one-hot FSM style"
    This reverts commit e2093cc8992b699ca87504ddd8dd29b9eba9ce41.

--- code/Rev2Board_SDCardController/top.v
+++ code/Rev2Board_SDCardController/top.v
@@ -73,24 +73,15 @@ module Top(
         .sd_dat(sd_dat)
     );
     
-    localparam StateGo = 0;
-    localparam StateStop = 1;
-    reg[1:0] state = 0;
-    reg init = 0;
+    reg[3:0] state = 0;
     
     // ====================
     // State Machine
     //   Toggle between reading/writing blocks
     // ====================
     always @(posedge clk) begin
-        if (!init) begin
-            init <= 1;
-            state <= 0;
-            state[StateGo] <= 1;
-        end
-        
-        case (1'b1)
-        state[StateGo]: begin
+        case (state)
+        0: begin
             sd_cmd_trigger <= 1;
             if (sd_cmd_accepted) begin
                 if (sd_cmd_write) begin
@@ -98,18 +89,16 @@ module Top(
                 end else begin
                     $display("[SD HOST] Read accepted");
                 end
-                state <= 0;
-                state[StateStop] <= 1;
+                state <= 1;
             end
         end
         
-        state[StateStop]: begin
+        1: begin
             sd_cmd_trigger <= 0;
             if (sd_cmd_accepted) begin
                 $display("[SD HOST] Stop accepted");
                 sd_cmd_write <= !sd_cmd_write;
                 state <= 0;
-                state[StateGo] <= 1;
             end
         end
         endcase
