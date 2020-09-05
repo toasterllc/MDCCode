21aa2e54128fab36b5788d8e4816bea722cfc03e
Dave <dave@toaster.llc>
2020-09-04 18:01:22 -0700

    CRC16Test: test CRC algorithm with tuning data from spec -- seems to be working

--- code/CRC16Test/top.v
+++ code/CRC16Test/top.v
@@ -20,7 +20,7 @@ module Top();
     end
     
     reg crc_rst_ = 0;
-    reg[7:0] crc_din = 0;
+    reg[511:0] crc_din = 512'hFF0FFF00_FFCCC3CC_C33CCCFF_FEFFFEEF_FFDFFFDD_FFFBFFFB_BFFF7FFF_77F7BDEF_FFF0FFF0_0FFCCC3C_CC33CCCF_FFEFFFEE_FFFDFFFD_DFFFBFFF_BBFFF7FF_F77F7BDE;
     wire[15:0] crc_dout;
     CRC16 crc(
         .clk(clk),
@@ -38,16 +38,12 @@ module Top();
         wait(!clk);
         crc_rst_ = 1;
         
-        for (i=0; i<128; i=i+1) begin
-            $display("crc_din = %0d", i);
-            crc_din = i;
-            repeat (8) begin
-                wait(clk);
-                wait(!clk);
-                crc_din = crc_din<<1;
-                
-                $display("CRC: %h", crc_dout);
-            end
+        repeat (128) begin
+            wait(clk);
+            wait(!clk);
+            crc_din = crc_din<<4;
+            
+            $display("CRC: %h", crc_dout);
         end
         
         #1000;
