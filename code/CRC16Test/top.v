402239fda2beb1d8d79c5b554594970301768bcd
Dave <dave@toaster.llc>
2020-09-04 17:31:30 -0700

    CRC16Test: calculate CRC of incrementing numbers

--- code/CRC16Test/top.v
+++ code/CRC16Test/top.v
@@ -20,28 +20,34 @@ module Top();
     end
     
     reg crc_rst_ = 0;
-    reg[1023:0] crc_din = {1024{1'b1}};
+    reg[7:0] crc_din = 0;
     wire[15:0] crc_dout;
     CRC16 crc(
         .clk(clk),
         .rst_(crc_rst_),
-        .din(crc_din[1023]),
+        .din(crc_din[$size(crc_din)-1]),
         .dout(crc_dout),
         .doutNext()
     );
     
     
     initial begin
+        reg[7:0] i;
+        
         #1000;
         wait(!clk);
         crc_rst_ = 1;
         
-        repeat (1024) begin
-            wait(clk);
-            wait(!clk);
-            crc_din = crc_din<<1;
-            
-            $display("CRC: %h", crc_dout);
+        for (i=0; i<128; i=i+1) begin
+            $display("crc_din = %0d", i);
+            crc_din = i;
+            repeat (8) begin
+                wait(clk);
+                wait(!clk);
+                crc_din = crc_din<<1;
+                
+                $display("CRC: %h", crc_dout);
+            end
         end
         
         #1000;
