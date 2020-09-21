0b20ab1c3a7b413945faddd82b6bbc1f921e7469
Dave <dave@toaster.llc>
2020-09-21 14:36:02 -0700

    BankFifo progress

--- code/BankFifo.v
+++ code/BankFifo.v
@@ -18,15 +18,16 @@ module BankFifo #(
     // Write domain
     // ====================
     reg[N-1:0] w_addr = 0;
+    wire[N-1:0] w_addrNext = w_addr+1;
     wire w_bank = w_addr[N-1];
-    reg w_lastBank = 0;
+    wire w_bankNext = w_addrNext[N-1];
     reg[1:0] w_bits = 0;
     reg[1:0] w_rbits=0, w_rbitsTmp=0;
     assign w_ok = (w_bits[0]===w_rbits[0]) || (w_bits[1]===w_rbits[1]);
     always @(posedge w_clk) begin
         if (w_trigger && w_ok) begin
             mem[w_addr] <= w_data;
-            w_addr <= w_addr+1;
+            w_addr <= w_addrNext;
         end
     end
     
@@ -37,15 +38,16 @@ module BankFifo #(
     // Read domain
     // ====================
     reg[N-1:0] r_addr = 0;
+    wire[N-1:0] r_addrNext = r_addr+1;
     wire r_bank = r_addr[N-1];
-    reg r_lastBank = 0;
+    wire r_bankNext = r_addrNext[N-1];
     reg[1:0] r_bits = 0;
     reg[1:0] r_wbits=0, r_wbitsTmp=0;
     assign r_data = mem[r_addr];
     assign r_ok = (r_bits[0]!==r_wbits[0]) || (r_bits[1]!==r_wbits[1]);
     always @(posedge r_clk) begin
         if (r_trigger && r_ok) begin
-            r_addr <= r_addr+1;
+            r_addr <= r_addrNext;
         end
     end
     
@@ -57,8 +59,7 @@ module BankFifo #(
     // w_bits
     // ====================
     always @(posedge w_clk) begin
-        w_lastBank <= w_bank;
-        case ({w_lastBank, w_bank})
+        case ({w_bank, w_bankNext})
         2'b01:  w_bits[0] <= !w_bits[0];
         2'b10:  w_bits[1] <= !w_bits[1];
         endcase
@@ -68,8 +69,7 @@ module BankFifo #(
     // r_bits
     // ====================
     always @(posedge r_clk) begin
-        r_lastBank <= r_bank;
-        case ({r_lastBank, r_bank})
+        case ({r_bank, r_bankNext})
         2'b01:  r_bits[0] <= !r_bits[0];
         2'b10:  r_bits[1] <= !r_bits[1];
         endcase
