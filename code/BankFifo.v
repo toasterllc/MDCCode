e4154fc54b0ae5f845101b409d00eed1d7c14996
Dave <dave@toaster.llc>
2020-09-21 20:08:39 -0700

    BankFifo: cleanup

--- code/BankFifo.v
+++ code/BankFifo.v
@@ -20,18 +20,16 @@ module BankFifo #(
     // ====================
     reg[N-1:0] w_addr = 0;
     wire w_bank = w_addr[N-1];
-    reg w_rbank = 0;
+    reg w_rbank=0, w_rbankTmp=0;
     assign w_ok = w_bank!==w_rbank || !dir;
     always @(posedge w_clk) begin
         if (w_trigger && w_ok) begin
             mem[w_addr] <= w_data;
             w_addr <= w_addr+1;
         end
-    end
-    
-    reg w_rbankTmp = 0;
-    always @(posedge w_clk)
+        
         {w_rbank, w_rbankTmp} <= {w_rbankTmp, r_bank};
+    end
     
     // ====================
     // Read domain
@@ -41,27 +39,27 @@ module BankFifo #(
     initial r_addr = 0;
 `endif
     wire r_bank = r_addr[N-1];
-    reg r_wbank = 0;
+    reg r_wbank=0, r_wbankTmp=0;
     assign r_data = mem[r_addr];
     assign r_ok = r_bank!==r_wbank || dir;
     always @(posedge r_clk) begin
-        if (r_trigger && r_ok) begin
+        if (r_trigger && r_ok)
             r_addr <= r_addr+1;
-        end
+        
+        {r_wbank, r_wbankTmp} <= {r_wbankTmp, w_bank};
     end
     
-    reg r_wbankTmp = 0;
-    always @(posedge r_clk)
-        {r_wbank, r_wbankTmp} <= {r_wbankTmp, w_bank};
     
-    
+    // ====================
+    // `dir` Handling
+    // ====================
     wire[1:0] r = r_addr[N-1:N-2];
     wire[1:0] w = w_addr[N-1:N-2];
-    wire almostEmpty = (r===2'b01 && w===2'b10) || (r===2'b11 && w===2'b00);
-    wire almostFull = (w===2'b01 && r===2'b10) || (w===2'b11 && r===2'b00);
+    wire emptyish = (r===2'b01 && w===2'b10) || (r===2'b11 && w===2'b00);
+    wire fullish = (w===2'b01 && r===2'b10) || (w===2'b11 && r===2'b00);
     
-    always @(posedge almostEmpty, posedge almostFull)
-        if (almostEmpty) dir <= 0;
+    always @(posedge emptyish, posedge fullish)
+        if (emptyish) dir <= 0;
         else dir <= 1;
     
 endmodule
