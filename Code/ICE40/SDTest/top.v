a2a607ce3dafad85f51d84200df8baa84881cf82
Dave <dave@toaster.llc>
2020-10-26 17:01:31 -0700

    SDTest: progress

--- Code/ICE40/SDTest/top.v
+++ Code/ICE40/SDTest/top.v
@@ -2,14 +2,17 @@
 
 module Top(
     input wire clk24mhz,
-    output wire[3:0] led
+    output wire[3:0] led,
+    output wire sd_init,
+    output wire sd_clk,
+    output wire sd_cmd,
 );
-    wire clk = clk24mhz;
-    
     reg[20:0] counter = 0;
-    always @(posedge clk) begin
+    always @(posedge clk24mhz) begin
         counter <= counter+1;
     end
-    
     assign led[3:0] = {4{counter[$size(counter)-1]}};
+    assign sd_clk = counter[$size(counter)-1];
+    assign sd_cmd = counter[$size(counter)-1];
+    assign sd_init = 1'b1;
 endmodule
