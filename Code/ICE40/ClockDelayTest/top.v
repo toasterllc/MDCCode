e0ed2404cf3d0530c1211c868960bc17dcf33abf
Dave <dave@toaster.llc>
2020-11-02 23:10:25 -0800

    Add ClockDelayTest/Util/VariableDelay.v

---
+++ Code/ICE40/ClockDelayTest/top.v
@@ -0,0 +1,121 @@
+`include "../Util/ClockGen.v"
+`include "../Util/Util.v"
+`include "../Util/VariableDelay.v"
+
+`ifdef SIM
+`include "/usr/local/share/yosys/ice40/cells_sim.v"
+`endif
+
+`timescale 1ns/1ps
+
+module Top(
+    input wire clk24mhz,
+    output wire clk,
+    output wire clkDelayed,
+    output reg[3:0] led = 0
+);
+    // ====================
+    // Fast Clock (120 MHz)
+    // ====================
+    localparam FastClkFreq = 120_000_000;
+    ClockGen #(
+        .FREQ(FastClkFreq),
+        .DIVR(0),
+        .DIVF(39),
+        .DIVQ(3),
+        .FILTER_RANGE(2)
+    ) ClockGen_fastClk(.clkRef(clk24mhz), .clk(clk));
+    
+    // // ====================
+    // // Fast Clock (48 MHz)
+    // // ====================
+    // localparam FastClkFreq = 48_000_000;
+    // ClockGen #(
+    //     .FREQ(FastClkFreq),
+    //     .DIVR(0),
+    //     .DIVF(31),
+    //     .DIVQ(4),
+    //     .FILTER_RANGE(2)
+    // ) ClockGen_fastClk(.clkRef(clk24mhz), .clk(clk));
+    
+    // ====================
+    // Slow Clock (1 Hz)
+    // ====================
+`ifdef SIM
+    localparam SlowClkFreq = 4000000;
+`else
+    localparam SlowClkFreq = 1;
+`endif
+    localparam SlowClkDividerWidth = $clog2(DivCeil(32'd24_000_000, SlowClkFreq));
+    reg[SlowClkDividerWidth-1:0] slowClkDivider = 0;
+    wire slowClk = slowClkDivider[SlowClkDividerWidth-1];
+    always @(posedge clk24mhz) begin
+        slowClkDivider <= slowClkDivider+1;
+    end
+    
+    
+    // // One-hot `sel`
+    // localparam DelayCount = 10;
+    // reg[DelayCount-1:0] sel = 0;
+    // reg selInit = 0;
+    // always @(posedge slowClk) begin
+    //     led[0] <= !led[0];
+    //     sel <= sel<<1|sel[$size(sel)-1]|!selInit;
+    //     selInit <= 1;
+    // end
+    //
+    // VariableDelay #(
+    //     .Count(DelayCount)
+    // ) VariableDelay(
+    //     .in(clk),
+    //     .sel(sel),
+    //     .out(clkDelayed)
+    // );
+    
+    // Binary `sel`
+    localparam DelayCount = 16;
+    reg[$clog2(DelayCount)-1:0] sel = 0;
+    always @(posedge slowClk) begin
+        led[0] <= !led[0];
+        sel <= sel+1;
+    end
+    
+    VariableDelay #(
+        .Count(DelayCount)
+    ) VariableDelay(
+        .in(clk),
+        .sel(sel),
+        .out(clkDelayed)
+    );
+endmodule
+
+
+`ifdef SIM
+module Testbench();
+    reg clk24mhz;
+    wire clk;
+    wire clkDelayed;
+    wire[3:0] led;
+    
+    Top Top(.*);
+    
+    initial begin
+        $dumpfile("top.vcd");
+        $dumpvars(0, Testbench);
+    end
+    
+    initial begin
+        #10000;
+        `Finish;
+    end
+    
+    initial begin
+        forever begin
+            clk24mhz = 0;
+            #21;
+            clk24mhz = 1;
+            #21;
+        end
+    end
+endmodule
+`endif
---
+++ Code/ICE40/Util/VariableDelay.v
@@ -0,0 +1,62 @@
+`timescale 1ns/1ps
+
+// // One-hot `sel`
+// module VariableDelay #(
+//     parameter Count = 1
+// )(
+//     input wire in,
+//     input wire[Count-1:0] sel,
+//     output wire out
+// );
+//     wire[Count-1:0] bits;
+//     assign bits[0] = in;
+//     genvar i;
+//     for (i=0; i<Count-1; i=i+1) begin
+//         wire #(1) dbits = bits[i];
+//         SB_LUT4 #(
+//             .LUT_INIT(16'bxxxx_xxxx_xxxx_xx10)
+//         ) SB_LUT4(
+//             .I3(1'b0),
+//             .I2(1'b0),
+//             .I1(1'b0),
+//             .I0(dbits),
+//             .O(bits[i+1])
+//         );
+//     end
+//
+//     for (i=0; i<Count; i=i+1) begin
+//         assign out = (sel[i] ? bits[i] : 1'bz);
+//     end
+// endmodule
+
+
+
+// Binary `sel`
+module VariableDelay #(
+    parameter Count = 1,
+    localparam SelWidth = $clog2(Count)
+)(
+    input wire in,
+    input wire[SelWidth-1:0] sel,
+    output wire out
+);
+    wire[Count-1:0] bits;
+    assign bits[0] = in;
+    genvar i;
+    for (i=0; i<Count-1; i=i+1) begin
+        wire #(1) dbits = bits[i];
+        SB_LUT4 #(
+            .LUT_INIT(16'bxxxx_xxxx_xxxx_xx10)
+        ) SB_LUT4(
+            .I3(1'b0),
+            .I2(1'b0),
+            .I1(1'b0),
+            .I0(dbits),
+            .O(bits[i+1])
+        );
+    end
+
+    for (i=0; i<Count; i=i+1) begin
+        assign out = (sel===i ? bits[i] : 1'bz);
+    end
+endmodule
