bc93838c40c4e20d5628387faa971f378a937c71
Dave <dave@toaster.llc>
2020-11-26 10:28:48 -0800

    AFIFOPixRAMTest: switch back to old AFIFO implemenation. More stripping. Still reproduces.

--- Code/ICE40/AFIFOPixRAMTest/Top.v
+++ Code/ICE40/AFIFOPixRAMTest/Top.v
@@ -39,37 +39,30 @@ module Top(
     // ====================
     // FIFO
     // ====================
-    reg fifo_rst = 0;
-    wire fifo_rst_done;
     reg fifo_writeEn = 0;
     wire fifo_writeReady;
     reg fifo_readTrigger = 0;
     wire[15:0] fifo_readData;
     wire fifo_readReady;
     reg[`RegWidth(ImageSize-1)-1:0] fifo_counter = 0;
-    
+        
     AFIFO #(
-        .W(16),
-        .N(8)
+        .Width(16),
+        .Size(256)
     ) AFIFO (
-        .rst(fifo_rst),
-        .rst_done(fifo_rst_done),
+        .wclk(pix_dclk),
+        .wok(fifo_writeReady),
+        .wtrigger(fifo_writeEn),
+        .wdata({3'b0, fifo_counter}),
         
-        .w_clk(pix_dclk),
-        .w_ready(fifo_writeReady),
-        .w_trigger(fifo_writeEn),
-        .w_data({3'b0, fifo_counter}),
-        
-        .r_clk(clk),
-        .r_ready(fifo_readReady),
-        .r_trigger(fifo_readTrigger),
-        .r_data(fifo_readData)
+        .rclk(clk),
+        .rok(fifo_readReady),
+        .rtrigger(fifo_readTrigger),
+        .rdata(fifo_readData)
     );
     
     reg ctrl_fifoCaptureTrigger = 0;
     `TogglePulse(fifo_captureTrigger, ctrl_fifoCaptureTrigger, posedge, pix_dclk);
-    `TogglePulse(fifo_rstDone, fifo_rst_done, posedge, pix_dclk);
-    `TogglePulse(ctrl_fifoRstDone, fifo_rst_done, posedge, clk);
     
     reg[2:0] fifo_state = 0;
     always @(posedge pix_dclk) begin
@@ -80,23 +73,15 @@ module Top(
         0: begin
         end
         
-        // Reset FIFO / ourself
+        // Wait for FIFO to be done resetting
         1: begin
-            fifo_rst <= !fifo_rst;
+            $display("[FIFO] Frame start");
+            fifo_counter <= ImageSize-1;
             fifo_state <= 2;
         end
         
-        // Wait for FIFO to be done resetting
-        2: begin
-            if (fifo_rstDone) begin
-                $display("[FIFO] Frame start");
-                fifo_counter <= ImageSize-1;
-                fifo_state <= 3;
-            end
-        end
-        
         // Wait until the end of the frame
-        3: begin
+        2: begin
             fifo_writeEn <= 1;
             if (fifo_writeEn) begin
                 fifo_counter <= fifo_counter-1;
@@ -158,22 +143,12 @@ module Top(
         Ctrl_State_Capture+1: begin
             // Start the FIFO data flow
             ctrl_fifoCaptureTrigger <= !ctrl_fifoCaptureTrigger;
+            ctrl_pixelCounter <= ImageSize-1;
             ctrl_state <= Ctrl_State_Capture+2;
         end
         
-        Ctrl_State_Capture+2: begin
-            // Wait until the FIFO is reset
-            // This is necessary so that when we observe `fifo_readReady`,
-            // we know it's from the start of this session, not from a previous one.
-            if (ctrl_fifoRstDone) begin
-                $display("[CTRL] Receiving data from FIFO...");
-                ctrl_pixelCounter <= ImageSize-1;
-                ctrl_state <= Ctrl_State_Capture+3;
-            end
-        end
-        
         // Receive data out of FIFO
-        Ctrl_State_Capture+3: begin
+        Ctrl_State_Capture+2: begin
             fifo_readTrigger <= 1;
             
             if (fifo_readTrigger && fifo_readReady) begin
@@ -181,13 +156,13 @@ module Top(
                 ctrl_pixelCounter <= ctrl_pixelCounter-1;
                 if (!ctrl_pixelCounter) begin
                     $display("[CTRL] Received full image");
-                    ctrl_state <= Ctrl_State_Capture+4;
+                    ctrl_state <= Ctrl_State_Capture+3;
                 end
             end
         end
         
         // Wait for extra pixels that we don't expect
-        Ctrl_State_Capture+4: begin
+        Ctrl_State_Capture+3: begin
             if (&ctrl_counter) begin
                 if (fifo_readReady) begin
                     // We got a pixel we didn't expect
--- Code/ICE40/Util/AFIFO.v
+++ Code/ICE40/Util/AFIFO.v
@@ -1,151 +1,106 @@
 `ifndef AFIFO_v
 `define AFIFO_v
 
-`include "TogglePulse.v"
-
 // Based on Clifford E. Cummings paper:
 //   http://www.sunburst-design.com/papers/CummingsSNUG2002SJ_FIFO2.pdf
 module AFIFO #(
-    parameter W=16, // Word width
-    parameter N=8   // Word count (2^N)
+    parameter Width=12,
+    parameter Size=4 // Must be a power of 2 and >=4
 )(
-    // Reset port (clock domain: async)
-    input wire rst, // Toggle
-    output reg rst_done = 0, // Toggle
+    input wire rclk,                // Read clock
+    input wire rtrigger,            // Read trigger
+    output wire[Width-1:0] rdata,   // Read data
+    output wire rok,                // Read OK (data available -- not empty)
     
-    input wire w_clk,           // Write clock
-    input wire w_trigger,       // Write trigger
-    input wire[W-1:0] w_data,   // Write data
-    output wire w_ready,        // Write OK (space available -- not full)
-    
-    input wire r_clk,           // Read clock
-    input wire r_trigger,       // Read trigger
-    output wire[W-1:0] r_data,  // Read data
-    output wire r_ready         // Read OK (data available -- not empty)
+    input wire wclk,                // Write clock
+    input wire wtrigger,            // Write trigger
+    input wire[Width-1:0] wdata,    // Write data
+    output wire wok                 // Write OK (space available -- not full)
 );
-    reg[W-1:0] mem[0:(1<<N)-1];
-    
-    
-    // ====================
-    // Reset Handling
-    // ====================
-    reg w_rst = 0;
-    reg w_rstAck=0, w_rstAckTmp=0, w_rstAckPrev=0;
-    reg r_rst=0, r_rstTmp=0;
-    `TogglePulse(w_rstTrigger, rst, posedge, w_clk);
-    always @(posedge w_clk) begin
-        if (w_rstTrigger) w_rst <= 1;
-        // Synchronize `r_rst` into `w_rstAck`, representing the
-        // acknowledgement of the reset from the read domain.
-        {w_rstAck, w_rstAckTmp} <= {w_rstAckTmp, r_rst};
-        // Clear `w_rstReq` upon the ack from the read domain
-        if (w_rstAck) w_rst <= 0;
-        // We're done resetting when the ack goes 1->0
-        w_rstAckPrev <= w_rstAck;
-        if (w_rstAckPrev && !w_rstAck) rst_done <= !rst_done;
-    end
-    
-    always @(posedge r_clk) begin
-        {r_rst, r_rstTmp} <= {r_rstTmp, w_rst};
-    end
-    
-    
-    
-    // ====================
-    // Write handling
-    // ====================
-    reg[N:0] w_baddr=0, w_gaddr=0, w_gaddrDelayed=0; // Write address (binary, gray, gray delayed)
-    wire[N:0] w_baddrNext = w_baddr+1'b1;
-    always @(posedge w_clk) begin
-        if (w_rst) begin
-            w_baddr <= 0;
-            w_gaddr <= 0;
-            w_gaddrDelayed <= 0;
-        
-        end else begin
-            w_gaddrDelayed <= w_gaddr;
-            if (w_trigger & w_ready) begin
-                mem[w_baddr[N-1:0]] <= w_data;
-                w_baddr <= w_baddrNext;
-                w_gaddr <= (w_baddrNext>>1)^w_baddrNext;
-            end
-        end
-    end
-    
-    reg[1:0] w_readyReg_ = 0; // Inverted logic so we come out of reset with w_ready==true
-    always @(posedge w_clk, negedge w_readyAsync)
-        if (!w_readyAsync) w_readyReg_ <= 2'b11;
-        else w_readyReg_ <= (w_readyReg_<<1)|1'b0;
-    
-    assign w_ready = !w_readyReg_[1];
+    localparam N = $clog2(Size);
+    reg[Width-1:0] mem[Size-1:0];
     
     // ====================
     // Read handling
     // ====================
-    // Don't init r_baddr=0, since that breaks RAM inference with Icestorm,
-    // since it thinks r_baddr is async instead of being clocked by r_clk
-    reg[N:0] r_baddr, r_gaddr=0, r_gaddrDelayed=0; // Read addresses (binary, gray)
+    // Don't init rbaddr=0, since that breaks RAM inference with Icestorm,
+    // since it thinks rbaddr is async instead of being clocked by rclk
+    reg[N:0] rbaddr, rgaddr=0, rgaddrDelayed=0; // Read addresses (binary, gray)
     
 `ifdef SIM
-    initial r_baddr = 0; // For simulation (see r_baddr comment above)
+    initial rbaddr = 0; // For simulation (see rbaddr comment above)
 `endif
     
-    wire[N:0] r_baddrNext = r_baddr+1'b1;
-    always @(posedge r_clk) begin
-        if (r_rst) begin
-            r_baddr <= 0;
-            r_gaddr <= 0;
-            r_gaddrDelayed <= 0;
-        
-        end else begin
-            r_gaddrDelayed <= r_gaddr;
-            if (r_trigger & r_ready) begin
-                r_baddr <= r_baddrNext;
-                r_gaddr <= (r_baddrNext>>1)^r_baddrNext;
-            end
+    wire[N:0] rbaddrNext = rbaddr+1'b1;
+    always @(posedge rclk) begin
+        rgaddrDelayed <= rgaddr;
+        if (rtrigger & rok) begin
+            rbaddr <= rbaddrNext;
+            rgaddr <= (rbaddrNext>>1)^rbaddrNext;
         end
     end
     
-    reg[1:0] r_readyReg = 0;
-    always @(posedge r_clk, negedge r_readyAsync)
-        if (!r_readyAsync) r_readyReg <= 2'b00;
-        else r_readyReg <= (r_readyReg<<1)|1'b1;
+    reg[1:0] rokReg = 0;
+    always @(posedge rclk, negedge arok)
+        if (!arok) rokReg <= 2'b00;
+        else rokReg <= (rokReg<<1)|1'b1;
     
-    assign r_data = mem[r_baddr[N-1:0]];
-    assign r_ready = r_readyReg[1];
+    assign rdata = mem[rbaddr[N-1:0]];
+    assign rok = rokReg[1];
+    
+    // ====================
+    // Write handling
+    // ====================
+    reg[N:0] wbaddr=0, wgaddr=0, wgaddrDelayed=0; // Write address (binary, gray, gray delayed)
+    wire[N:0] wbaddrNext = wbaddr+1'b1;
+    always @(posedge wclk) begin
+        wgaddrDelayed <= wgaddr;
+        if (wtrigger & wok) begin
+            mem[wbaddr[N-1:0]] <= wdata;
+            wbaddr <= wbaddrNext;
+            wgaddr <= (wbaddrNext>>1)^wbaddrNext;
+        end
+    end
+    
+    reg[1:0] wokReg_ = 0; // Inverted logic so we come out of reset with wok==true
+    always @(posedge wclk, negedge awok)
+        if (!awok) wokReg_ <= 2'b11;
+        else wokReg_ <= (wokReg_<<1)|1'b0;
+    
+    assign wok = !wokReg_[1];
     
     // ====================
     // Async signal generation
     // ====================
-    wire r_empty = (r_gaddr == w_gaddrDelayed);
-    wire w_full = (w_gaddr == {~r_gaddrDelayed[N:N-1], r_gaddrDelayed[N-2:0]});
+    wire rempty = (rgaddr == wgaddrDelayed);
+    wire wfull = (wgaddr == {~rgaddrDelayed[N:N-1], rgaddrDelayed[N-2:0]});
     
-    wire r_readyAsync = !r_empty; // Read OK == !empty
-    wire w_readyAsync = !w_full || w_rst; // Write OK == !full
+    wire arok = !rempty; // Read OK == !empty
+    wire awok = !wfull; // Write OK == !full
     
     // reg dir = 0;
-    // wire arok = (r_gaddr!=w_gaddrDelayed) || dir; // Read OK == not empty
-    // wire awok = (r_gaddrDelayed!=w_gaddr) || !dir; // Write OK == not full
+    // wire arok = (rgaddr!=wgaddrDelayed) || dir; // Read OK == not empty
+    // wire awok = (rgaddrDelayed!=wgaddr) || !dir; // Write OK == not full
     //
     // // ICESTORM: USED TO WORK, NOW FAILS (WFAST, RSLOW)
     // // ICECUBE: WORKS
-    // wire dirclr = (r_gaddrDelayed[N]!=w_gaddrDelayed[N-1]) && (r_gaddrDelayed[N-1]==w_gaddrDelayed[N]);
-    // wire dirset = (r_gaddrDelayed[N]==w_gaddrDelayed[N-1]) && (r_gaddrDelayed[N-1]!=w_gaddrDelayed[N]);
+    // wire dirclr = (rgaddrDelayed[N]!=wgaddrDelayed[N-1]) && (rgaddrDelayed[N-1]==wgaddrDelayed[N]);
+    // wire dirset = (rgaddrDelayed[N]==wgaddrDelayed[N-1]) && (rgaddrDelayed[N-1]!=wgaddrDelayed[N]);
     //
     // // // ICESTORM: WORKS
     // // // ICECUBE: WORKS
-    // // wire dirclr = (r_gaddr[N]!=w_gaddrDelayed[N-1]) && (r_gaddr[N-1]==w_gaddrDelayed[N]);
-    // // wire dirset = (r_gaddrDelayed[N]==w_gaddr[N-1]) && (r_gaddrDelayed[N-1]!=w_gaddr[N]);
+    // // wire dirclr = (rgaddr[N]!=wgaddrDelayed[N-1]) && (rgaddr[N-1]==wgaddrDelayed[N]);
+    // // wire dirset = (rgaddrDelayed[N]==wgaddr[N-1]) && (rgaddrDelayed[N-1]!=wgaddr[N]);
     //
     // // // ICESTORM: FAILS (WFAST, RSLOW)
     // // // ICECUBE: WORKS
-    // // wire dirclr = (r_gaddrDelayed[N]!=w_gaddr[N-1]) && (r_gaddrDelayed[N-1]==w_gaddr[N]);
-    // // wire dirset = (r_gaddr[N]==w_gaddrDelayed[N-1]) && (r_gaddr[N-1]!=w_gaddrDelayed[N]);
+    // // wire dirclr = (rgaddrDelayed[N]!=wgaddr[N-1]) && (rgaddrDelayed[N-1]==wgaddr[N]);
+    // // wire dirset = (rgaddr[N]==wgaddrDelayed[N-1]) && (rgaddr[N-1]!=wgaddrDelayed[N]);
     //
     // // // ICESTORM: FAILS (WFAST, RSLOW)
     // // // ICECUBE: WORKS
-    // // wire dirclr = (r_gaddr[N]!=w_gaddr[N-1]) && (r_gaddr[N-1]==w_gaddr[N]);
-    // // wire dirset = (r_gaddr[N]==w_gaddr[N-1]) && (r_gaddr[N-1]!=w_gaddr[N]);
+    // // wire dirclr = (rgaddr[N]!=wgaddr[N-1]) && (rgaddr[N-1]==wgaddr[N]);
+    // // wire dirset = (rgaddr[N]==wgaddr[N-1]) && (rgaddr[N-1]!=wgaddr[N]);
     //
     // always @(posedge dirclr, posedge dirset) begin
     //     if (dirclr) dir <= 0;
