f882c87633594231be976e69ad5fd6adc68b237e
Dave <dave@toaster.llc>
2020-11-26 17:19:57 -0800

    AFIFO: might have a working implementation

--- Code/ICE40/Util/AFIFO.v
+++ Code/ICE40/Util/AFIFO.v
@@ -46,75 +46,53 @@ module AFIFO #(
         {r_rst, r_rstTmp} <= {r_rstTmp, w_rst};
     end
     
-    
-    
-    
-    
     // ====================
     // Write handling
     // ====================
-    reg[N-1:0] w_baddr=0, w_gaddr=0, w_gaddrDelayed=0; // Write address (binary, gray)
-    wire[N-1:0] w_baddrNext = (w_trigger&&w_ready ? w_baddr+1'b1 : w_baddr);
+    reg[N:0] w_baddr=0, w_gaddr=0; // Write address (binary, gray)
+    wire[N:0] w_baddrNext = (w_trigger&&w_ready ? w_baddr+1'b1 : w_baddr);
+    wire[N:0] w_gaddrNext = (w_baddrNext>>1)^w_baddrNext;
     always @(posedge w_clk) begin
-        if (w_rst) begin
-            w_gaddrDelayed <= 0;
-            w_baddr <= 0;
-            w_gaddr <= 0;
-        
-        end else begin
-            w_gaddrDelayed <= w_gaddr;
-            w_baddr <= w_baddrNext;
-            w_gaddr <= (w_baddrNext>>1)^w_baddrNext;
-        end
+        if (w_rst)  {w_baddr, w_gaddr} <= 0;
+        else        {w_baddr, w_gaddr} <= {w_baddrNext, w_gaddrNext};
     end
     
-    reg[1:0] w_readyReg_ = 0; // Inverted logic so we come out of reset with w_ready==true
-    always @(posedge w_clk, negedge a_full_)
-        if (!a_full_) w_readyReg_ <= ~0;
-        else w_readyReg_ <= (w_readyReg_<<1)|1'b0;
+    reg[N:0] w_rgaddr=0, w_rgaddrTmp=0;
+    always @(posedge w_clk)
+        if (w_rst)  {w_rgaddr, w_rgaddrTmp} <= 0;
+        else        {w_rgaddr, w_rgaddrTmp} <= {w_rgaddrTmp, r_gaddr};
     
-    assign w_ready = !`LeftBit(w_readyReg_, 0);
+    reg w_full = 0;
+    always @(posedge w_clk)
+        if (w_rst)  w_full <= 0;
+        else        w_full <= (w_gaddrNext === {~w_rgaddr[N:N-1], w_rgaddr[N-2:0]});
+    
+    assign w_ready = !w_full;
     
     // ====================
     // Read handling
     // ====================
     // Don't init r_baddr=0, since that breaks RAM inference with Icestorm,
     // since it thinks r_baddr is async instead of being clocked by r_clk
-    reg[N-1:0] r_baddr=0, r_gaddr=0, r_gaddrDelayed=0; // Read addresses (binary, gray)
-    wire[N-1:0] r_baddrNext = (r_trigger&&r_ready ? r_baddr+1'b1 : r_baddr);
+    reg[N:0] r_baddr=0, r_gaddr=0; // Read addresses (binary, gray)
+    wire[N:0] r_baddrNext = (r_trigger&&r_ready ? r_baddr+1'b1 : r_baddr);
+    wire[N:0] r_gaddrNext = (r_baddrNext>>1)^r_baddrNext;
     always @(posedge r_clk) begin
-        if (r_rst) begin
-            r_gaddrDelayed <= 0;
-            r_baddr <= 0;
-            r_gaddr <= 0;
-        
-        end else begin
-            r_gaddrDelayed <= r_gaddr;
-            r_baddr <= r_baddrNext;
-            r_gaddr <= (r_baddrNext>>1)^r_baddrNext;
-        end
+        if (r_rst)  {r_baddr, r_gaddr} <= 0;
+        else        {r_baddr, r_gaddr} <= {r_baddrNext, r_gaddrNext};
     end
     
-    reg[1:0] r_readyReg = 0;
-    always @(posedge r_clk, negedge a_empty_)
-        if (!a_empty_) r_readyReg <= 0;
-        else r_readyReg <= (r_readyReg<<1)|1'b1;
+    reg[N:0] r_wgaddr=0, r_wgaddrTmp=0;
+    always @(posedge r_clk)
+        if (r_rst)  {r_wgaddr, r_wgaddrTmp} <= 0;
+        else        {r_wgaddr, r_wgaddrTmp} <= {r_wgaddrTmp, w_gaddr};
     
-    assign r_ready = `LeftBit(r_readyReg, 0);
+    reg r_empty_ = 0;
+    always @(posedge r_clk)
+        if (r_rst)  r_empty_ <= 1'b0;
+        else        r_empty_ <= !(r_gaddrNext == r_wgaddr);
     
-    // ====================
-    // Async signal generation
-    // ====================
-    reg a_dir = 0;
-    wire a_dirSet = (w_gaddr[N-1]^r_gaddrDelayed[N-2]) && ~(w_gaddr[N-2]^r_gaddrDelayed[N-1]);
-    wire a_dirClr = (~(w_gaddrDelayed[N-1]^r_gaddr[N-2]) && (w_gaddrDelayed[N-2]^r_gaddr[N-1]));
-    
-    always @(posedge a_dirSet, posedge a_dirClr)
-        if (a_dirClr) a_dir <= 0;
-        else a_dir <= 1;
-    
-    assign a_full_ = ~((w_gaddr == r_gaddrDelayed) && a_dir);
-    assign a_empty_ = ~((w_gaddrDelayed == r_gaddr) && !a_dir);
+    assign r_ready = r_empty_;
     
     // ====================
     // RAM
@@ -123,50 +101,16 @@ module AFIFO #(
         .WCLK(w_clk),
         .WCLKE(1'b1),
         .WE(w_trigger && w_ready),
-        .WADDR({3'b000, w_baddr}),
+        .WADDR({3'b000, w_baddr[N-1:0]}),
         .WDATA(w_data),
         .MASK(16'h0000),
         
         .RCLK(r_clk),
         .RCLKE(1'b1),
         .RE(1'b1),
-        .RADDR({3'b000, r_baddrNext}),
+        .RADDR({3'b000, r_baddrNext[N-1:0]}),
         .RDATA(r_data)
     );
-    
-    
-    
-    
-    
-    
-    // reg dir = 0;
-    // wire arok = (r_gaddr!=w_gaddrDelayed) || dir; // Read OK == not empty
-    // wire awok = (r_gaddrDelayed!=w_gaddr) || !dir; // Write OK == not full
-    //
-    // // ICESTORM: USED TO WORK, NOW FAILS (WFAST, RSLOW)
-    // // ICECUBE: WORKS
-    // wire dirclr = (r_gaddrDelayed[N]!=w_gaddrDelayed[N-1]) && (r_gaddrDelayed[N-1]==w_gaddrDelayed[N]);
-    // wire dirset = (r_gaddrDelayed[N]==w_gaddrDelayed[N-1]) && (r_gaddrDelayed[N-1]!=w_gaddrDelayed[N]);
-    //
-    // // // ICESTORM: WORKS
-    // // // ICECUBE: WORKS
-    // // wire dirclr = (r_gaddr[N]!=w_gaddrDelayed[N-1]) && (r_gaddr[N-1]==w_gaddrDelayed[N]);
-    // // wire dirset = (r_gaddrDelayed[N]==w_gaddr[N-1]) && (r_gaddrDelayed[N-1]!=w_gaddr[N]);
-    //
-    // // // ICESTORM: FAILS (WFAST, RSLOW)
-    // // // ICECUBE: WORKS
-    // // wire dirclr = (r_gaddrDelayed[N]!=w_gaddr[N-1]) && (r_gaddrDelayed[N-1]==w_gaddr[N]);
-    // // wire dirset = (r_gaddr[N]==w_gaddrDelayed[N-1]) && (r_gaddr[N-1]!=w_gaddrDelayed[N]);
-    //
-    // // // ICESTORM: FAILS (WFAST, RSLOW)
-    // // // ICECUBE: WORKS
-    // // wire dirclr = (r_gaddr[N]!=w_gaddr[N-1]) && (r_gaddr[N-1]==w_gaddr[N]);
-    // // wire dirset = (r_gaddr[N]==w_gaddr[N-1]) && (r_gaddr[N-1]!=w_gaddr[N]);
-    //
-    // always @(posedge dirclr, posedge dirset) begin
-    //     if (dirclr) dir <= 0;
-    //     else dir <= 1;
-    // end
 endmodule
 
 `endif
