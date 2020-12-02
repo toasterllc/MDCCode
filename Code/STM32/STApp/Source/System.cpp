3c1a4a1f2ac392402619960efd81f9442fe33efe
Dave <dave@toaster.llc>
2020-12-01 21:19:20 -0800

    Revert "STApp: Disabling QSPI and sending uninitialized data gets us to 44.7 MB/sec"
    This reverts commit 2f6b170f943cf801e3a4a4c81756f6ebb314ddec.

--- Code/STM32/STApp/Source/System.cpp
+++ Code/STM32/STApp/Source/System.cpp
@@ -721,7 +721,8 @@ void System::_handleCmd(const USB::Cmd& ev) {
     case Cmd::Op::PixStream: {
         if (cmd.arg.pixStream.enable && !_pixStreamEnabled) {
             _pixStreamEnabled = true;
-            _sendPixDataViaUSB();
+            _pixRemLen = 2304*1296*2;
+            _recvPixDataViaICE40();
         
         } else if (!cmd.arg.pixStream.enable && _pixStreamEnabled) {
             // TODO: how do we disable streaming?
@@ -760,7 +761,7 @@ void System::_handleQSPIEvent(const QSPI::DoneEvent& ev) {
     // Enqueue the buffer
     {
         // Update the number of remaining bytes to receive from the host
-//        _pixRemLen -= _pixBufs.writeBuf().len;
+        _pixRemLen -= _pixBufs.writeBuf().len;
         _pixBufs.writeEnqueue();
     }
     
@@ -778,7 +779,27 @@ void System::_handleQSPIEvent(const QSPI::DoneEvent& ev) {
 
 void System::_handlePixUSBEvent(const USB::DoneEvent& ev) {
     Assert(_pixStreamEnabled);
-    _sendPixDataViaUSB();
+    Assert(_pixBufs.readable());
+    const bool wasWritable = _pixBufs.writable();
+    
+    // Dequeue the buffer
+    _pixBufs.readDequeue();
+    
+    // Start another USB transaction if there's more data to write
+    if (_pixBufs.readable()) {
+        _sendPixDataViaUSB();
+    }
+    
+    if (_pixRemLen) {
+        // Prepare to receive more data if we're expecting more,
+        // and we were previously un-writable
+        if (!wasWritable) {
+            _recvPixDataViaICE40();
+        }
+    } else if (!_pixBufs.readable()) {
+        // We're done32,768
+        // TODO: what do after we sent all the data?
+    }
 }
 
 // Arrange for pix data to be received from ICE40
@@ -792,9 +813,9 @@ void System::_recvPixDataViaICE40() {
 
 // Arrange for pix data to be sent over USB
 void System::_sendPixDataViaUSB() {
-//    Assert(_pixBufs.readable());
-    const auto& buf = _pixBufs.writeBuf();
-    _usb.pixSend(buf.data, sizeof(buf.data));
+    Assert(_pixBufs.readable());
+    const auto& buf = _pixBufs.readBuf();
+    _usb.pixSend(buf.data, buf.len);
 }
 
 System Sys;
