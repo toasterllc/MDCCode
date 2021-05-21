5929813a37b6397f90c35979a73e8b0684807fb8
Dave <dave@toaster.llc>
2021-05-21 15:40:04 -0700

    ICEStreamApp-Debug: finished whittling. The problem is that a PIO pair (ie 2 pads that share a PIO tile) must have the same INPUT_CLK / OUTPUT_CLK / CLOCK_ENABLE. See diagram of a ICE40 'PIO pair' in the datasheet.

--- Code/ICE40/ICEStreamApp-Debug/Top.v
+++ Code/ICE40/ICEStreamApp-Debug/Top.v
@@ -1,34 +1,27 @@
 `timescale 1ns/1ps
 
 module Top(
-    input wire          clk24mhz,
-    input wire          pix_dclk,
-    inout wire          pix_x,
-    inout wire          pix_y
+    input wire          clkX,
+    input wire          clkY,
+    
+    output wire         outX,
+    output wire         outY
 );
-    reg pix_x_do = 0;
-    wire pix_x_di;
     SB_IO #(
         .PIN_TYPE(6'b1101_00)
     ) IO_A (
-        .INPUT_CLK(clk24mhz),
-        .OUTPUT_CLK(clk24mhz),
-        .PACKAGE_PIN(pix_x),
-        .OUTPUT_ENABLE(1'b1),
-        .D_OUT_0(pix_x_do),
-        .D_IN_0(pix_x_di)
+        .OUTPUT_CLK(clkX),
+        .PACKAGE_PIN(outX),
+        .OUTPUT_ENABLE(1),
+        .D_OUT_0(0)
     );
     
-    reg pix_y_do = 0;
-    wire pix_y_di;
     SB_IO #(
         .PIN_TYPE(6'b1101_00)
     ) IO_B (
-        .INPUT_CLK(pix_dclk),
-        .OUTPUT_CLK(pix_dclk),
-        .PACKAGE_PIN(pix_y),
-        .OUTPUT_ENABLE(1'b1),
-        .D_OUT_0(pix_y_do),
-        .D_IN_0(pix_y_di)
+        .OUTPUT_CLK(clkY),
+        .PACKAGE_PIN(outY),
+        .OUTPUT_ENABLE(1),
+        .D_OUT_0(0)
     );
 endmodule
