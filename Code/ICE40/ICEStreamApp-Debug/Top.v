fa02ed39aae2bcd3b8cf2efa5bef24f0a455ec6e
Dave <dave@toaster.llc>
2021-05-21 14:27:19 -0700

    ICEStreamApp-Debug: whittle progress

--- Code/ICE40/ICEStreamApp-Debug/Top.v
+++ Code/ICE40/ICEStreamApp-Debug/Top.v
@@ -62,95 +62,6 @@
 `endif
 
 
-`ifndef AFIFO_v
-`define AFIFO_v
-
-// Based on Clifford E. Cummings paper:
-//   http://www.sunburst-design.com/papers/CummingsSNUG2002SJ_FIFO2.pdf
-module AFIFO #(
-    localparam W = 16, // Word width
-    localparam N = 8   // Word count (2^N)
-)(
-    // Reset port (clock domain: async)
-    input wire rst_,
-    
-    input wire w_clk,               // Write clock
-    input wire w_trigger,           // Write trigger
-    input wire[W-1:0] w_data,       // Write data
-    output wire w_ready,            // Write OK (space available -- not full)
-    
-    input wire r_clk,               // Read clock
-    input wire r_trigger,           // Read trigger
-    output wire[W-1:0] r_data,      // Read data
-    output wire r_ready             // Read OK (data available -- not empty)
-);
-    // ====================
-    // Write handling
-    // ====================
-    reg[N:0] w_baddr=0, w_gaddr=0; // Write address (binary, gray)
-    wire[N:0] w_baddrNext = (w_trigger&&w_ready ? w_baddr+1'b1 : w_baddr);
-    wire[N:0] w_gaddrNext = (w_baddrNext>>1)^w_baddrNext;
-    reg[N:0] w_rgaddr=0, w_rgaddrTmp=0;
-    reg w_full = 0;
-    always @(posedge w_clk, negedge rst_) begin
-        if (!rst_) begin
-            {w_baddr, w_gaddr} <= 0;
-            {w_rgaddr, w_rgaddrTmp} <= 0;
-            w_full <= 0;
-        end else begin
-            {w_baddr, w_gaddr} <= {w_baddrNext, w_gaddrNext};
-            {w_rgaddr, w_rgaddrTmp} <= {w_rgaddrTmp, r_gaddr};
-            w_full <= (w_gaddrNext === {~w_rgaddr[N:N-1], w_rgaddr[N-2:0]});
-        end
-    end
-    
-    assign w_ready = !w_full;
-    
-    // ====================
-    // Read handling
-    // ====================
-    reg[N:0] r_baddr=0, r_gaddr=0; // Read addresses (binary, gray)
-    wire[N:0] r_baddrNext = (r_trigger&&r_ready ? r_baddr+1'b1 : r_baddr);
-    wire[N:0] r_gaddrNext = (r_baddrNext>>1)^r_baddrNext;
-    reg[N:0] r_wgaddr=0, r_wgaddrTmp=0;
-    reg r_empty_ = 0;
-    always @(posedge r_clk, negedge rst_) begin
-        if (!rst_) begin
-            {r_baddr, r_gaddr} <= 0;
-            {r_wgaddr, r_wgaddrTmp} <= 0;
-            r_empty_ <= 0;
-        end else begin
-            {r_baddr, r_gaddr} <= {r_baddrNext, r_gaddrNext};
-            {r_wgaddr, r_wgaddrTmp} <= {r_wgaddrTmp, w_gaddr};
-            r_empty_ <= !(r_gaddrNext === r_wgaddr);
-        end
-    end
-    
-    assign r_ready = r_empty_;
-    
-    // ====================
-    // RAM
-    // ====================
-    SB_RAM40_4K SB_RAM40_4K(
-        .WCLK(w_clk),
-        .WCLKE(1'b1),
-        .WE(w_trigger && w_ready),
-        .WADDR({3'b000, w_baddr[N-1:0]}),
-        .WDATA(w_data),
-        .MASK(16'h0000),
-        
-        .RCLK(r_clk),
-        .RCLKE(1'b1),
-        .RE(1'b1),
-        .RADDR({3'b000, r_baddrNext[N-1:0]}),
-        .RDATA(r_data)
-    );
-endmodule
-
-`endif
-
-
-
 
 `ifndef RAMController_v
 `define RAMController_v
@@ -947,8 +858,6 @@ module PixController #(
     // ====================
     // Input FIFO (Pixels->RAM)
     // ====================
-    wire fifoIn_write_ready = 1'b1;
-    wire fifoIn_write_trigger;
     wire fifoIn_read_ready = 1'b1;
     wire[15:0] fifoIn_read_data = {4'b0, pix_d_reg};
     
@@ -1046,7 +955,6 @@ module PixController #(
             
             // Copy word from FIFO->RAM
             if (fifoIn_read_ready && fifoIn_read_trigger) begin
-                // $display("[PIXCTRL:Capture] Got pixel: %0d", fifoIn_read_data);
                 ramctrl_write_data <= fifoIn_read_data;
                 ramctrl_write_trigger <= 1;
             end
@@ -1065,8 +973,6 @@ module PixController #(
     // ====================
     // Connections
     // ====================
-    // Connect input FIFO write -> pixel data
-    assign fifoIn_write_trigger = fifoIn_writeEn && pix_lv_reg;
     
     // Connect input FIFO read -> RAM write
     assign fifoIn_read_trigger = (!ramctrl_write_trigger || ramctrl_write_ready);
