829031677a8b343f85feb4bc72bfab666780a224
Dave <dave@toaster.llc>
2021-06-07 01:21:25 -0700

    Progress

--- Code/MSP430/MSP430ProgrammerTest/GPIO.h
+++ Code/MSP430/MSP430ProgrammerTest/GPIO.h
@@ -4,20 +4,20 @@
 template <uint8_t Bit>
 class GPIO {
 private:
-    static constexpr uint8_t _b = (uint8_t)1 << Bit;
+    static constexpr uint8_t _Mask = (uint8_t)1 << Bit;
     
 public:
     bool read() {
-        return P1IN & _b;
+        return P1IN & _Mask;
     }
     
     void write(bool x) {
-        if (x)  P1OUT   |=  _b;
-        else    P1OUT   &= ~_b;
+        if (x)  P1OUT   |=  _Mask;
+        else    P1OUT   &= ~_Mask;
     }
     
     void config(bool out) {
-        if (out)    P1DIR   |=  _b;
-        else        P1DIR   &= ~_b;
+        if (out)    P1DIR   |=  _Mask;
+        else        P1DIR   &= ~_Mask;
     }
 };
