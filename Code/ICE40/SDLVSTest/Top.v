3169f7ce199c1acdbcba308cd74bd1749f26b280
Dave <dave@toaster.llc>
2021-06-09 21:00:24 -0700

    SDLVSTest: progress

--- Code/ICE40/SDLVSTest/Top.v
+++ Code/ICE40/SDLVSTest/Top.v
@@ -6,58 +6,104 @@
 module Top(
     input wire clk24mhz,
     output reg[3:0] led = 0,
-    output wire sd_clk = 0,
+    output reg sd_clk = 0,
     inout wire sd_cmd,
     inout wire[3:0] sd_dat,
 );
     // ====================
-    // Slow Clock (375 kHz)
+    // Clock (375 kHz)
     // ====================
-    reg[5:0] counter = 0;
+    reg[5:0] clkCounter = 0;
     always @(posedge clk24mhz) begin
-        counter <= counter+1;
+        clkCounter <= clkCounter+1;
     end
-    wire slowClk = counter[$size(counter)-1];
+    wire clk = `LeftBit(clkCounter, 0);
+    
+    // ====================
+    // Slow Clock (1.4 Hz)
+    // ====================
+    reg[18:0] slowClkCounter = 0;
+    always @(posedge clk) begin
+        slowClkCounter <= slowClkCounter+1;
+    end
+    wire slowClk = `LeftBit(slowClkCounter, 0);
+    
+    // ====================
+    // Pin: sd_cmd
+    // ====================
+    reg sd_cmdOut = 0;
+    reg sd_cmdOutEn = 0;
+    wire sd_cmdIn;
+    SB_IO #(
+        .PIN_TYPE(6'b1101_00)
+    ) SB_IO_sd_cmd (
+        .INPUT_CLK(clk),
+        .OUTPUT_CLK(clk),
+        .PACKAGE_PIN(sd_cmd),
+        .OUTPUT_ENABLE(sd_cmdOutEn),
+        .D_OUT_0(sd_cmdOut),
+        .D_IN_0(sd_cmdIn)
+    );
+
+    // ====================
+    // Pin: sd_dat[3:0]
+    // ====================
+    reg[3:0] sd_datOut = 0;
+    reg[3:0] sd_datOutEn = 0;
+    wire[3:0] sd_datIn;
+    genvar i;
+    for (i=0; i<4; i=i+1) begin
+        SB_IO #(
+            .PIN_TYPE(6'b1101_00)
+        ) SB_IO_sd_dat (
+            .INPUT_CLK(clk),
+            .OUTPUT_CLK(clk),
+            .PACKAGE_PIN(sd_dat[i]),
+            .OUTPUT_ENABLE(sd_datOutEn[i]),
+            .D_OUT_0(sd_datOut[i]),
+            .D_IN_0(sd_datIn[i])
+        );
+    end
+    
+    reg prevSlowClk = 0;
+    wire slowClkPulse = slowClk && !prevSlowClk;
+    
+    // ====================
+    // State Machine
+    // ====================
+    reg[1:0] state = 0;
+    always @(posedge clk) begin
+        prevSlowClk <= slowClk;
+        
+        case (state)
+        0: begin
+            led <= 4'h8;
+            state <= 1;
+        end
+        
+        1: begin
+            if (slowClkPulse) begin
+                led <= led-1;
+                if (led === 4'h1) begin
+                    state <= 2;
+                end
+            end
+        end
+        
+        2: begin
+            // led <= ~led;
+        end
+        endcase
+    end
     
     // // ====================
-    // // Pin: sd_cmd
+    // // Slow Clock (375 kHz)
     // // ====================
-    // SB_IO #(
-    //     .PIN_TYPE(6'b1101_00)
-    // ) SB_IO_sd_cmd (
-    //     .INPUT_CLK(clk_int),
-    //     .OUTPUT_CLK(clk_int),
-    //     .PACKAGE_PIN(sd_cmd),
-    //     .OUTPUT_ENABLE(cmd_active[0]),
-    //     .D_OUT_0(cmdresp_shiftReg[47]),
-    //     .D_IN_0(cmd_in)
-    // );
-    //
-    // // ====================
-    // // Pin: sd_dat[3:0]
-    // // ====================
-    // genvar i;
-    // for (i=0; i<4; i=i+1) begin
-    //     SB_IO #(
-    //         .PIN_TYPE(6'b1101_00)
-    //     ) SB_IO_sd_dat (
-    //         .INPUT_CLK(clk_int),
-    //         .OUTPUT_CLK(clk_int),
-    //         .PACKAGE_PIN(sd_dat[i]),
-    //         .OUTPUT_ENABLE(datOut_active[0]),
-    //         .D_OUT_0(datOut_reg[16+i]),
-    //         .D_IN_0(datIn[i])
-    //     );
+    // reg[23:0] wideCounter = 0;
+    // always @(posedge clk24mhz) begin
+    //     led <= `LeftBits(wideCounter, 0, 4);
+    //     wideCounter <= wideCounter+1;
     // end
-    
-    // ====================
-    // Slow Clock (375 kHz)
-    // ====================
-    reg[15:0] wideCounter = 0;
-    always @(posedge clk24mhz) begin
-        led <= `LeftBits(wideCounter, 0, 4);
-        wideCounter <= wideCounter+1;
-    end
     
     // // ====================
     // // Nets
