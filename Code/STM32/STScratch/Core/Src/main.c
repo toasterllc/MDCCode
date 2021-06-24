baff75d44386cc1529748b587103c5c38c309560
Dave <dave@toaster.llc>
2021-06-24 14:17:57 -0700

    STScratch: Migrate to 16MHz external source clock

--- Code/STM32/STScratch/Core/Src/main.c
+++ Code/STM32/STScratch/Core/Src/main.c
@@ -132,7 +132,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 12;
+  RCC_OscInitStruct.PLL.PLLM = 8;
   RCC_OscInitStruct.PLL.PLLN = 128;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 2;
