55956b73ed233730e84c7646495c2eac7a624b00
Dave <dave@toaster.llc>
2021-06-24 16:11:48 -0700

    Blinky: update for Rev4 board

--- Code/ICE40/Blinky/Top.v
+++ Code/ICE40/Blinky/Top.v
@@ -1,15 +1,15 @@
 `timescale 1ns/1ps
 
 module Top(
-    input wire clk24mhz,
-    output wire[3:0] led
+    input wire ice_img_clk16mhz,
+    output wire[3:0] ice_led
 );
-    wire clk = clk24mhz;
+    wire clk = ice_img_clk16mhz;
     
     reg[20:0] counter = 0;
     always @(posedge clk) begin
         counter <= counter+1;
     end
     
-    assign led[3:0] = {4{counter[$size(counter)-1]}};
+    assign ice_led[3:0] = {4{counter[$size(counter)-1]}};
 endmodule
