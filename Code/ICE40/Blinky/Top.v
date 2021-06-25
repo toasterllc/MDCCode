6138e3ca823c5357fa1f2d7ed01c58fff27d743b
Dave <dave@toaster.llc>
2021-06-24 18:51:36 -0700

    Blinky: blink at 1Hz

--- Code/ICE40/Blinky/Top.v
+++ Code/ICE40/Blinky/Top.v
@@ -1,4 +1,5 @@
 `include "ClockGen.v"
+`include "Util.v"
 `timescale 1ns/1ps
 
 module Top(
@@ -6,6 +7,7 @@ module Top(
     output wire[3:0] ice_led
 );
     wire clk16mhz = ice_img_clk16mhz;
+    
     wire clk;
     ClockGen #(
         .OUTFREQ(32000000),
@@ -15,12 +17,13 @@ module Top(
         .FILTER_RANGE(1)
     ) ClockGen(.clkRef(clk16mhz), .clk(clk));
     
-    reg[20:0] counter = 0;
+    // .95 Hz (assuming 32 MHz clock)
+    reg[24:0] counter = 0;
     always @(posedge clk) begin
         counter <= counter+1;
     end
     
-    assign ice_led[3:0] = {4{counter[$size(counter)-1]}};
+    assign ice_led[3:0] = {4{`LeftBit(counter, 0)}};
 endmodule
 
 `ifdef SIM
