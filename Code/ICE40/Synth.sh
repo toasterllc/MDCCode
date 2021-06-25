f841fba2d8012fefd7f731360034489abb6021e1
Dave <dave@toaster.llc>
2021-06-24 17:40:40 -0700

    Update synthesis scripts so we don't create a 'tmp' directory with a copy of all the source files

--- Code/ICE40/Synth.sh
+++ Code/ICE40/Synth.sh
@@ -4,35 +4,37 @@ set -e
 if [ "$#" -ne 3 ]; then
 	echo "Usage:"
     echo "  Synth.sh <DeviceType> <DevicePackage> <ProjName>"
-	echo "    DeviceType: 1k (iCEstick), 8k (MDC Rev4, iCE40HX board)"
+	echo "    DeviceType: hx1k (iCEstick), hx8k (MDC Rev4, iCE40HX board)"
 	echo "    DevicePackage: tq144 (iCEstick), bg121:4k (MDC Rev4), ct256 (iCE40HX board)"
 	
 	echo
 	echo "Examples:"
-	echo "  Synth.sh 8k bg121:4k Blinky # MDC Rev4"
-	echo "  Synth.sh 1k tq144 Blinky # iCEstick"
-	echo "  Synth.sh 8k ct256 Blinky # iCE40HX board"
+	echo "  Synth.sh hx8k bg121:4k Blinky # MDC Rev4"
+	echo "  Synth.sh hx1k tq144 Blinky # iCEstick"
+	echo "  Synth.sh hx8k ct256 Blinky # iCE40HX board"
 	exit 1
 fi
 
-dir=$(cd $(dirname "$0"); pwd)
+rootDir=$(cd $(dirname "$0"); pwd)
 dev="$1"
 pkg="$2"
 proj="$3"
 
-rm -Rf "$proj/tmp"
-mkdir -p "$proj/tmp"
-cp "$dir/Synth.ys" "$proj/tmp"
-cp -R "$dir/Shared/." "$proj/tmp"
-cp "$proj/Top.v" "$proj/tmp"
-cp "$dir/Pins.pcf" "$proj/tmp"
-cd "$proj/tmp"
+synthDir="$rootDir/$proj/Synth"
+
+# Remove existing synthesis files
+# (Delete specific files instead of the whole directory so we don't have to worry about
+# some crazy case that causes us to execute `rm -Rf /`.)
+rm -f "$synthDir/Top.asc" "$synthDir/Top.bin" "$synthDir/Top.json"
+# Create 'Synth' directory
+mkdir -p "$synthDir"
 
 # Synthesize the design from Verilog (.v -> .json)
-yosys -s Synth.ys Top.v
+cd "$proj"
+yosys -s "$rootDir/Synth.ys"
 
 # Place and route the design ({Top.json, Pins.pcf} -> .asc)
-nextpnr-ice40 -r "--hx$dev" --package "$pkg" --json Top.json --pcf Pins.pcf --asc Top.asc --pcf-allow-unconstrained --top Top
+nextpnr-ice40 -r "--$dev" --package "$pkg" --json "$synthDir/Top.json" --pcf "$rootDir/Pins.pcf" --asc "$synthDir/Top.asc" --pcf-allow-unconstrained --top Top
 
 # Generate the bitstream file (.asc -> .bin)
-icepack Top.asc Top.bin
+icepack "$synthDir/Top.asc" "$synthDir/Top.bin"
