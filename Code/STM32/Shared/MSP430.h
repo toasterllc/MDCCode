ab482d8a000a525bd110670087e2db5d4d4ce1f2
Dave <dave@toaster.llc>
2021-06-26 14:55:55 -0700

    STM32: update MSP430.h with regRead/regWrite/memRead/memWrite functions to allow different techniques to read/write memory (since writing registers doesn't seem to always work using the 'quick' writing technique)

--- Code/STM32/Shared/MSP430.h
+++ Code/STM32/Shared/MSP430.h
@@ -67,7 +67,7 @@ private:
         // <-- Run-Test/Idle
     }
     
-    void _startShiftIR() {
+    void _irShiftStart() {
         // <-- Run-Test/Idle
         _sbwio(1, _tclkSaved);
         // <-- Select DR-Scan
@@ -79,7 +79,7 @@ private:
         // <-- Shift-IR
     }
     
-    void _startShiftDR() {
+    void _drShiftStart() {
         // <-- Run-Test/Idle
         _sbwio(1, _tclkSaved);
         // <-- Select DR-Scan
@@ -165,40 +165,46 @@ private:
         return din;
     }
     
-    uint8_t _shiftIR(uint8_t d) {
-        _startShiftIR();
+    uint8_t _irShift(uint8_t d) {
+        _irShiftStart();
         const uint8_t din = _shift<8>(d);
         return din;
     }
     
     template <uint8_t W>
-    uint32_t _shiftDR(uint32_t d) {
-        _startShiftDR();
+    uint32_t _drShift(uint32_t d) {
+        _drShiftStart();
         const uint32_t din = _shift<W>(d);
         return din;
     }
     
-    uint8_t _readJTAGID() {
-        const uint8_t din = _shiftIR(_IR_CNTRL_SIG_CAPTURE);
+    uint8_t _jtagID() {
+        const uint8_t din = _irShift(_IR_CNTRL_SIG_CAPTURE);
         return din;
     }
     
-    bool _readJTAGFuseBlown() {
-        _shiftIR(_IR_CNTRL_SIG_CAPTURE);
-        const uint16_t din = _shiftDR<16>(0xAAAA);
+    bool _jtagFuseBlown() {
+        _irShift(_IR_CNTRL_SIG_CAPTURE);
+        const uint16_t din = _drShift<16>(0xAAAA);
         return din == 0x5555;
     }
     
-    uint16_t _readCoreID() {
-        _shiftIR(_IR_COREIP_ID);
-        const uint16_t din = _shiftDR<16>(0);
+    uint16_t _coreID() {
+        _irShift(_IR_COREIP_ID);
+        const uint16_t din = _drShift<16>(0);
         return din;
     }
     
-    uint32_t _readDeviceIDAddr() {
-        _shiftIR(_IR_DEVICE_ID);
-        const uint32_t din = _shiftDR<20>(0);
+    uint32_t _deviceIDAddr() {
+        _irShift(_IR_DEVICE_ID);
+        const uint32_t din = _drShift<20>(0);
         return din;
+    }
+    
+    uint16_t _deviceID() {
+        const uint32_t deviceIDAddr = _deviceIDAddr()+4;
+        const uint16_t deviceID = _regRead(deviceIDAddr);
+        return deviceID;
     }
     
     void _tclkSet(bool tclk) {
@@ -212,25 +218,25 @@ private:
         _tclkSet(1);
         
         // Prepare access to the JTAG CNTRL SIG register
-        _shiftIR(_IR_CNTRL_SIG_16BIT);
+        _irShift(_IR_CNTRL_SIG_16BIT);
         // Release CPUSUSP signal and apply POR signal
-        _shiftDR<16>(0x0C01);
+        _drShift<16>(0x0C01);
         // Release POR signal again
-        _shiftDR<16>(0x0401);
+        _drShift<16>(0x0401);
         
         // Set PC to 'safe' memory location
-        _shiftIR(_IR_DATA_16BIT);
+        _irShift(_IR_DATA_16BIT);
         _tclkSet(0);
         _tclkSet(1);
         _tclkSet(0);
         _tclkSet(1);
-        _shiftDR<16>(_SafePC);
+        _drShift<16>(_SafePC);
         // PC is set to 0x4 - MAB value can be 0x6 or 0x8
         
         // Drive safe address into PC
         _tclkSet(0);
         _tclkSet(1);
-        _shiftIR(_IR_DATA_CAPTURE);
+        _irShift(_IR_DATA_CAPTURE);
         // Two more clocks to release CPU internal POR delay signals
         _tclkSet(0);
         _tclkSet(1);
@@ -238,8 +244,8 @@ private:
         _tclkSet(1);
         
         // Set CPUSUSP signal again
-        _shiftIR(_IR_CNTRL_SIG_16BIT);
-        _shiftDR<16>(0x0501);
+        _irShift(_IR_CNTRL_SIG_16BIT);
+        _drShift<16>(0x0501);
         // One more clock
         _tclkSet(0);
         _tclkSet(1);
@@ -247,11 +253,11 @@ private:
         
         // Disable Watchdog Timer on target device now by setting the HOLD signal
         // in the WDT_CNTRL register
-        _writeMem(0x01CC, 0x5A80);
+        _regWrite(0x01CC, 0x5A80);
         
         // Check if device is in Full-Emulation-State and return status
-        _shiftIR(_IR_CNTRL_SIG_CAPTURE);
-        if (!(_shiftDR<16>(0) & 0x0301)) {
+        _irShift(_IR_CNTRL_SIG_CAPTURE);
+        if (!(_drShift<16>(0) & 0x0301)) {
             return false;
         }
         
@@ -260,114 +266,121 @@ private:
     
     bool _waitForCPUSync() {
         for (int i=0; i<50; i++) {
-            _shiftIR(_IR_CNTRL_SIG_CAPTURE);
-            if (_shiftDR<16>(0) & 0x0200) return true;
+            _irShift(_IR_CNTRL_SIG_CAPTURE);
+            if (_drShift<16>(0) & 0x0200) return true;
         }
         return false;
     }
     
-    bool _disableMPU() {
+    bool _mpuDisable() {
         constexpr uint16_t PasswordMask = 0xFF00;
         constexpr uint16_t Password = 0xA500;
         constexpr uint16_t MPUMask = 0x0003;
         constexpr uint16_t MPUDisabled = 0x0000;
-        uint16_t reg = _readMem(_SYSCFG0Addr);
+        uint16_t reg = _regRead(_SYSCFG0Addr);
         reg &= ~(PasswordMask|MPUMask); // Clear password and MPU protection bits
         reg |= (Password|MPUDisabled); // Password
-        _writeMem(_SYSCFG0Addr, reg);
+        _regWrite(_SYSCFG0Addr, reg);
         // Verify that the MPU protection bits are cleared
-        return (_readMem(_SYSCFG0Addr)&MPUMask) == MPUDisabled;
+        return (_regRead(_SYSCFG0Addr)&MPUMask) == MPUDisabled;
     }
     
     // CPU must be in Full-Emulation-State
-    void _setPC(uint32_t addr) {
+    void _pcSet(uint32_t addr) {
         constexpr uint16_t movInstr = 0x0080;
         const uint16_t pcHigh = ((addr>>8)&0xF00);
         const uint16_t pcLow = ((addr & 0xFFFF));
         
         _tclkSet(0);
         // Take over bus control during clock LOW phase
-        _shiftIR(_IR_DATA_16BIT);
+        _irShift(_IR_DATA_16BIT);
         _tclkSet(1);
-        _shiftDR<16>(pcHigh | movInstr);
+        _drShift<16>(pcHigh | movInstr);
         _tclkSet(0);
-        _shiftIR(_IR_CNTRL_SIG_16BIT);
-        _shiftDR<16>(0x1400);
-        _shiftIR(_IR_DATA_16BIT);
-        _tclkSet(0);
-        _tclkSet(1);
-        _shiftDR<16>(pcLow);
+        _irShift(_IR_CNTRL_SIG_16BIT);
+        _drShift<16>(0x1400);
+        _irShift(_IR_DATA_16BIT);
         _tclkSet(0);
         _tclkSet(1);
-        _shiftDR<16>(0x4303);
+        _drShift<16>(pcLow);
         _tclkSet(0);
-        _shiftIR(_IR_ADDR_CAPTURE);
-        _shiftDR<20>(0);
+        _tclkSet(1);
+        _drShift<16>(0x4303);
+        _tclkSet(0);
+        _irShift(_IR_ADDR_CAPTURE);
+        _drShift<20>(0);
     }
     
-    void _readMem(uint32_t addr, uint16_t* dst, uint32_t len) {
-        _setPC(addr);
+    // General purpose read
+    // Works for: peripherals, RAM, FRAM
+    //   This is the 'quick' read implementation suggested by JTAG guide
+    void _read(uint32_t addr, uint16_t* dst, uint32_t len) {
+        _pcSet(addr);
         _tclkSet(1);
-        _shiftIR(_IR_CNTRL_SIG_16BIT);
-        _shiftDR<16>(0x0501);
-        _shiftIR(_IR_ADDR_CAPTURE);
-        _shiftIR(_IR_DATA_QUICK);
+        _irShift(_IR_CNTRL_SIG_16BIT);
+        _drShift<16>(0x0501);
+        _irShift(_IR_ADDR_CAPTURE);
+        _irShift(_IR_DATA_QUICK);
         
         for (; len; len--) {
             _tclkSet(1);
             _tclkSet(0);
-            *dst = _shiftDR<16>(0);
+            *dst = _drShift<16>(0);
             dst++;
         }
     }
     
-    uint16_t _readMem(uint32_t addr) {
+    uint16_t _regRead(uint32_t addr) {
         uint16_t r = 0;
-        _readMem(addr, &r, 1);
+        _read(addr, &r, 1);
         return r;
     }
     
-    // This is a custom implementation using the 'quick' writing technique.
-    // The JTAG guide says "For the MSP430Xv2 architecture ... there is no
-    // specific implementation of a quick write operation", but this seems
-    // to work, and is a lot faster than the suggested implementation.
-//    void _writeMem(uint32_t addr, const uint16_t* src, uint32_t len) {
-//        constexpr uint16_t Poly = 0x0805;
-//        _setPC(addr-2);
-//        _tclkSet(1);
-//        _shiftIR(_IR_CNTRL_SIG_16BIT);
-//        _shiftDR<16>(0x0500);
-//        _shiftIR(_IR_DATA_QUICK);
-//        _tclkSet(0);
-//        
-//        for (; len; len--) {
-//            // Update CRC
-//            {
-//                if (_crc & 0x8000) {
-//                    _crc ^= Poly;
-//                    _crc <<= 1;
-//                    _crc |= 0x0001;
-//                } else {
-//                    _crc <<= 1;
-//                }
-//                
-//                _crc ^= *src;
-//            }
-//            
-//            _tclkSet(1);
-//            _shiftDR<16>(*src);
-//            src++;
-//            _tclkSet(0);
-//        }
-//        
-//        _shiftIR(_IR_CNTRL_SIG_16BIT);
-//        _shiftDR<16>(0x0501);
-//    }
+    // Register write
+    // Works for: peripherals
+    //   This is the 'slow' write implementation suggested by JTAG guide
+    void _regWrite(uint32_t addr, uint16_t val) {
+        _tclkSet(0);
+        _irShift(_IR_CNTRL_SIG_16BIT);
+        _drShift<16>(0x0500);
+        
+        _irShift(_IR_ADDR_16BIT);
+        _drShift<20>(addr);
+        _tclkSet(1);
+        
+        // Only apply data during clock high phase
+        _irShift(_IR_DATA_TO_ADDR);
+        _drShift<16>(val);
+        _tclkSet(0);
+        _irShift(_IR_CNTRL_SIG_16BIT);
+        _drShift<16>(0x0501);
+        _tclkSet(1);
+        // One or more cycle, so CPU is driving correct MAB
+        _tclkSet(0);
+        _tclkSet(1);
+    }
     
-    // Old _writeMem implementation suggested by JTAG guide
-    void _writeMem(uint32_t addr, const uint16_t* src, uint32_t len) {
+    void _memRead(uint32_t addr, uint16_t* dst, uint32_t len) {
+        _read(addr, dst, len);
+    }
+    
+    // TODO: confirm that this works for RAM
+    // Memory write
+    // Works for: RAM, FRAM
+    //   This is a custom implementation using the 'quick' writing technique.
+    //   The JTAG guide says "For the MSP430Xv2 architecture ... there is no
+    //   specific implementation of a quick write operation", but this seems
+    //   to work, and is a lot faster than the suggested implementation.
+    void _memWrite(uint32_t addr, const uint16_t* src, uint32_t len) {
         constexpr uint16_t Poly = 0x0805;
-        while (len) {
+        _pcSet(addr-2);
+        _tclkSet(1);
+        _irShift(_IR_CNTRL_SIG_16BIT);
+        _drShift<16>(0x0500);
+        _irShift(_IR_DATA_QUICK);
+        _tclkSet(0);
+        
+        for (; len; len--) {
             // Update CRC
             {
                 if (_crc & 0x8000) {
@@ -381,46 +394,27 @@ private:
                 _crc ^= *src;
             }
             
-            _tclkSet(0);
-            _shiftIR(_IR_CNTRL_SIG_16BIT);
-            _shiftDR<16>(0x0500);
-            
-            _shiftIR(_IR_ADDR_16BIT);
-            _shiftDR<20>(addr);
             _tclkSet(1);
-            
-            // Only apply data during clock high phase
-            _shiftIR(_IR_DATA_TO_ADDR);
-            _shiftDR<16>(*src);
-            _tclkSet(0);
-            _shiftIR(_IR_CNTRL_SIG_16BIT);
-            _shiftDR<16>(0x0501);
-            _tclkSet(1);
-            // One or more cycle, so CPU is driving correct MAB
-            _tclkSet(0);
-            _tclkSet(1);
-            
-            addr += 2;
+            _drShift<16>(*src);
             src++;
-            len--;
+            _tclkSet(0);
         }
+        
+        _irShift(_IR_CNTRL_SIG_16BIT);
+        _drShift<16>(0x0501);
     }
     
-    void _writeMem(uint32_t addr, uint16_t val) {
-        _writeMem(addr, &val, 1);
-    }
-    
-    uint16_t _calcCRC(uint32_t addr, uint32_t len) {
-        _setPC(addr);
+    uint16_t _crcCalc(uint32_t addr, uint32_t len) {
+        _pcSet(addr);
         _tclkSet(1);
         
-        _shiftIR(_IR_CNTRL_SIG_16BIT);
-        _shiftDR<16>(0x0501);
+        _irShift(_IR_CNTRL_SIG_16BIT);
+        _drShift<16>(0x0501);
         
-        _shiftIR(_IR_DATA_16BIT);
-        _shiftDR<16>(addr-2);
+        _irShift(_IR_DATA_16BIT);
+        _drShift<16>(addr-2);
         
-        _shiftIR(_IR_DATA_PSA);
+        _irShift(_IR_DATA_PSA);
         
         for (uint32_t i=0; i<len; i++) {
             _tclkSet(0);
@@ -439,8 +433,8 @@ private:
             _tclkSet(1);
         }
         
-        _shiftIR(_IR_SHIFT_OUT_PSA);
-        return _shiftDR<16>(0);
+        _irShift(_IR_SHIFT_OUT_PSA);
+        return _drShift<16>(0);
     }
     
     bool _jmbErase() {
@@ -450,16 +444,16 @@ private:
         constexpr uint16_t MagicNum = 0xA55A;
         constexpr uint16_t EraseCmd = 0x1A1A;
         
-        _shiftIR(_IR_JMB_EXCHANGE);
+        _irShift(_IR_JMB_EXCHANGE);
         bool ready = false;
         for (int i=0; i<3000 && !ready; i++) {
-            ready = _shiftDR<16>(0) & MailboxReady;
+            ready = _drShift<16>(0) & MailboxReady;
         }
         if (!ready) return false; // Timeout
         
-        _shiftDR<16>(Width32 | DirWrite);
-        _shiftDR<16>(MagicNum);
-        _shiftDR<16>(EraseCmd);
+        _drShift<16>(Width32 | DirWrite);
+        _drShift<16>(MagicNum);
+        _drShift<16>(EraseCmd);
         return true;
     }
     
@@ -533,23 +527,23 @@ public:
             _tapReset();
             
             // Validate the JTAG ID
-            if (_readJTAGID() != _JTAGID) {
+            if (_jtagID() != _JTAGID) {
                 continue; // Try again
             }
             
             // Check JTAG fuse blown state
-            if (_readJTAGFuseBlown()) {
+            if (_jtagFuseBlown()) {
                 return Status::JTAGDisabled;
             }
             
             // Validate the Core ID
-            if (_readCoreID() == 0) {
+            if (_coreID() == 0) {
                 continue; // Try again
             }
             
             // Set device into JTAG mode + read
-            _shiftIR(_IR_CNTRL_SIG_16BIT);
-            _shiftDR<16>(0x1501);
+            _irShift(_IR_CNTRL_SIG_16BIT);
+            _drShift<16>(0x1501);
             
             // Wait until CPU is sync'd
             if (!_waitForCPUSync()) {
@@ -563,15 +557,14 @@ public:
             
             // Validate the Device ID
             {
-                const uint32_t deviceIDAddr = _readDeviceIDAddr()+4;
-                uint16_t deviceID = _readMem(deviceIDAddr);
+                const uint16_t deviceID = _deviceID();
                 if (deviceID != _DeviceID) {
                     continue; // Try again
                 }
             }
             
             // Disable MPU (so we can write to FRAM)
-            if (!_disableMPU()) {
+            if (!_mpuDisable()) {
                 continue; // Try again
             }
             
@@ -600,23 +593,31 @@ public:
         return Status::OK;
     }
     
-    void read(uint32_t addr, uint16_t* dst, uint32_t len) {
-        _readMem(addr, dst, len);
+    uint16_t regRead(uint32_t addr) {
+        return _regRead(addr);
     }
     
-    void write(uint32_t addr, const uint16_t* src, uint32_t len) {
+    void regWrite(uint32_t addr, uint16_t val) {
+        _regWrite(addr, val);
+    }
+    
+    void memRead(uint32_t addr, uint16_t* dst, uint32_t len) {
+        _memRead(addr, dst, len);
+    }
+    
+    void memWrite(uint32_t addr, const uint16_t* src, uint32_t len) {
         if (!_crcValid) {
             _crc = addr-2;
             _crcValid = true;
         }
-        _writeMem(addr, src, len);
+        _regWrite(addr, src, len);
     }
     
-    void resetCRC() {
+    void memCRCReset() {
         _crcValid = false;
     }
     
-    Status verifyCRC(uint32_t addr, uint32_t len) {
-        return (_crc==_calcCRC(addr, len) ? Status::OK : Status::Error);
+    Status memCRCVerify(uint32_t addr, uint32_t len) {
+        return (_crc==_crcCalc(addr, len) ? Status::OK : Status::Error);
     }
 };
