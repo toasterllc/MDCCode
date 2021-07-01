a00e4763987bc75ee3246dcba1c136f0643985ec
Dave <dave@toaster.llc>
2021-06-30 22:13:04 -0700

    MSP430: TogglePin: got 32768 Hz crystal working, and sourcing MCLK/SCLK/ACLK

--- Code/MSP430/TogglePin/main.cpp
+++ Code/MSP430/TogglePin/main.cpp
@@ -4,12 +4,13 @@
 #include <inttypes.h>
 
 int main() {
-    // ## Stop watchdog timer
+    
+    // Stop watchdog timer
     {
         WDTCTL = WDTPW | WDTHOLD;
     }
     
-    // ## Reset pin states
+    // Reset pin states
     {
         PAOUT   = 0x0000;
         PADIR   = 0x0000;
@@ -20,57 +21,72 @@ int main() {
     
     // Configure clock system
     {
+        constexpr uint32_t MCLKFreqHz = 16000000;
+        constexpr uint32_t XT1FreqHz = 32768;
+        
         // Configure one FRAM wait state, as required by the device datasheet for MCLK > 8MHz.
         // This must happen before configuring the clock system.
         FRCTL0 = FRCTLPW | NWAITS_1;
         
-        P2SEL1 |= BIT0 | BIT1; // Set XT1 pin as second function
-        P2SEL0 &= ~(BIT0 | BIT1);
+        // Make PA.8 and PA.9 XOUT/XIN
+        PASEL1 |=  (BIT8 | BIT9);
+        PASEL0 &= ~(BIT8 | BIT9);
+        
         do {
             CSCTL7 &= ~(XT1OFFG | DCOFFG); // Clear XT1 and DCO fault flag
             SFRIFG1 &= ~OFIFG;
         } while (SFRIFG1 & OFIFG); // Test oscillator fault flag
         
-        __bis_SR_register(SCG0); // Disable FLL
-        CSCTL3 |= SELREF__XT1CLK; // Set XT1 as FLL reference source
-        CSCTL0 = 0; // Clear DCO and MOD registers
-        CSCTL1 &= ~(DCORSEL_7); // Clear DCO frequency select bits first
-        CSCTL1 |= DCORSEL_5; // Set DCO = 16MHz
-        CSCTL2 = FLLD_0 + 487; // DCOCLKDIV = 16MHz
-        __delay_cycles(3);
-        __bic_SR_register(SCG0); // Enable FLL
+        // Disable FLL
+        __bis_SR_register(SCG0);
+            // Set XT1 as FLL reference source
+            CSCTL3 |= SELREF__XT1CLK;
+            // Clear DCO and MOD registers
+            CSCTL0 = 0;
+            // Clear DCO frequency select bits first
+            CSCTL1 &= ~(DCORSEL_7);
+            // Set DCO = 16MHz
+            CSCTL1 |= DCORSEL_5;
+            // DCOCLKDIV = 16MHz
+            CSCTL2 = FLLD_0 | ((MCLKFreqHz/XT1FreqHz)-1);
+            // Wait 3 cycles to take effect
+            __delay_cycles(3);
+        // Enable FLL
+        __bic_SR_register(SCG0);
+        
         // Wait until FLL locks
         while (CSCTL7 & (FLLUNLOCK0 | FLLUNLOCK1));
         
-        // set XT1 (~32768Hz) as ACLK source, ACLK = 32768Hz
-        // default DCOCLKDIV as MCLK and SMCLK source
+        // MCLK / SMCLK source = DCOCLKDIV
+        // ACLK source = XT1
         CSCTL4 = SELMS__DCOCLKDIV | SELA__XT1CLK;
-        
-        P1DIR |= BIT0 | BIT1 | BIT2 | BIT3;     // set ACLK MCLK SMCLK and LED pin as output
-        P1SEL1 |= BIT1 | BIT2 | BIT3;           // set ACLK MCLK and SMCLK pin as second function
     }
     
-    // ## Unlock GPIOs
+    // Unlock GPIOs
     {
         PM5CTL0 &= ~LOCKLPM5;
     }
     
-    // // ## Set MCLK = 16MHz
+    // // Make PA.1 output ACLK
     // {
-    //     // Configure one FRAM waitstate as required by the device datasheet for MCLK
-    //     // operation beyond 8MHz _before_ configuring the clock system.
-    //     FRCTL0 = FRCTLPW | NWAITS_1; // Change the NACCESS_x value to add the right amount of waitstates
-    //
-    //     DCOCTL = 0;
-    //     BCSCTL1 = CALBC1_16MHZ;
-    //     BCSCTL2 = 0;
-    //     DCOCTL = CALDCO_16MHZ;
+    //     PADIR  |=  (BIT1);
+    //     PASEL1 |=  (BIT1);
+    //     PASEL0 &= ~(BIT1);
     // }
     
-    for (;;) {
-        PAOUT ^= 1<<14;
-        // __delay_cycles(100000);
-    }
+    // // Make PA.3 output MCLK
+    // {
+    //     PADIR  |=  (BIT3);
+    //     PASEL1 |=  (BIT3);
+    //     PASEL0 &= ~(BIT3);
+    // }
+    
+    for (;;);
+    
+    // for (;;) {
+    //     PAOUT ^= 1<<14;
+    //     // __delay_cycles(100000);
+    // }
     
     return 0;
 }
