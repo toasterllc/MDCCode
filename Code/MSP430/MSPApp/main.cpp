56e5ca6b7681ba7a96870da7c45916dd3d81aa3a
Dave <dave@toaster.llc>
2021-07-01 21:31:21 -0700

    MSPApp: progress

--- Code/MSP430/MSPApp/main.cpp
+++ Code/MSP430/MSPApp/main.cpp
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include <inttypes.h>
 
-static void _configClock() {
+static void _clockInit() {
     constexpr uint32_t MCLKFreqHz = 16000000;
     constexpr uint32_t XT1FreqHz = 32768;
     
@@ -62,7 +62,7 @@ static void _sysInit() {
     
     // Configure clock system
     {
-        _configClock();
+        _clockInit();
     }
     
     // Unlock GPIOs
@@ -71,9 +71,71 @@ static void _sysInit() {
     }
 }
 
+uint8_t RXData = 0;
+uint8_t TXData = 0;
+
+__interrupt __attribute__((interrupt(USCI_A0_VECTOR)))
+static void _isrUSCIA0() {
+    switch (UCA0IV) {
+    case USCI_SPI_UCRXIFG:
+        RXData = UCA0RXBUF;
+        UCA0IFG &= ~UCRXIFG;
+        // Wake up to setup next TX
+        __bic_SR_register_on_exit(LPM0_bits);
+        break;
+    
+    case USCI_SPI_UCTXIFG:
+        UCA0TXBUF = TXData;
+        UCA0IE &= ~UCTXIE;
+        break;
+    
+    default:
+        break;
+    }
+}
+
+
+
+
+
+
+
+
+
 int main() {
     _sysInit();
-    for (;;);
+    
+    P1SEL0 |= BIT4 | BIT5 | BIT6;
+    
+    // Assert USCI reset
+    UCA0CTLW0 = UCSWRST;
+    
+    UCA0CTLW0 |=
+        // phase=0, polarity=0, MSB first, width=8-bit
+        UCCKPH_0 | UCCKPL__LOW | UCMSB_1 | UC7BIT__8BIT |
+        // mode=master, mode=3-pin SPI, mode=synchronous, clock=SMCLK
+        UCMST__MASTER | UCMODE_0 | UCSYNC__SYNC | UCSSEL__SMCLK;
+    
+    // fBitClock = fBRCLK / 1;
+    UCA0BRW = 0;
+    // No modulation
+    UCA0MCTLW = 0;
+    
+    // De-assert USCI reset
+    UCA0CTLW0 &= ~UCSWRST;
+    
+    // Enable USCI_A0 RX interrupt
+    UCA0IE |= UCRXIE;
+    
+    TXData = 0x01;                            // Holds TX data
+    
+    for (;;) {
+        UCA0IE |= UCTXIE;                     // Enable TX interrupt
+        __bis_SR_register(LPM0_bits | GIE);   // Enable global interrupts, enter LPM0
+        __no_operation();                     // For debug,Remain in LPM0
+        __delay_cycles(2000);                 // Delay before next transmission
+        TXData++;                             // Increment transmit data
+    }
     
     return 0;
 }
