f1457b5d2598d147d79b8d2c56d8c441264e5d8a
Dave <dave@toaster.llc>
2021-08-05 00:26:27 -0700

    Strip

--- Code/ICE40/ICEApp/Top.v
+++ Code/ICE40/ICEApp/Top.v
@@ -1,17 +1,12 @@
 `include "Util.v"
 `include "Sync.v"
 `include "ToggleAck.v"
-`include "SDController.v"
 `include "ICEAppTypes.v"
 `include "ClockGen.v"
 `include "ImgController.v"
 `include "ImgI2CMaster.v"
 
 `ifdef SIM
-// SDCARDSIM_LVS_INIT_IGNORE_5MS: Don't require waiting for 5ms, because that takes way too long in simulation
-`define SDCARDSIM_LVS_INIT_IGNORE_5MS
-`include "SDCardSim.v"
-
 `include "ImgSim.v"
 `include "ImgI2CSlaveSim.v"
 
@@ -179,57 +174,6 @@ module Top(
         .ram_dqm(ram_dqm),
         .ram_dq(ram_dq)
     );
-    
-    // reg img_captureDone = 0;
-    // reg img_readoutStarted = 0;
-    //
-    // localparam Img_State_Idle       = 0;    // +0
-    // localparam Img_State_Capture    = 1;    // +1
-    // localparam Img_State_Readout    = 3;    // +0
-    // localparam Img_State_Count      = 4;
-    // reg[`RegWidth(Img_State_Count-1)-1:0] img_state = 0;
-    // always @(posedge img_clk) begin
-    //     imgctrl_cmd <= `ImgController_Cmd_None;
-    //
-    //     case (img_state)
-    //     Img_State_Idle: begin
-    //     end
-    //
-    //     // TODO: consider making ImgController just use a toggle input for the 2 commands (capture and readout)?
-    //     Img_State_Capture: begin
-    //         // Start a capture
-    //         imgctrl_cmd <= `ImgController_Cmd_Capture;
-    //         img_state <= Img_State_Idle;
-    //     end
-    //
-    //     // Img_State_Capture+1: begin
-    //     //     // Wait for the capture to complete, and then start readout
-    //     //     if (imgctrl_status_captureDone) begin
-    //     //         imgctrl_cmd <= `ImgController_Cmd_Readout;
-    //     //         img_state <= Img_State_Readout;
-    //     //     end
-    //     // end
-    //
-    //     Img_State_Readout: begin
-    //         // Wait for readout to start, and then signal so via img_readoutStarted
-    //         if (imgctrl_status_readoutStarted) begin
-    //             img_readoutStarted <= !img_readoutStarted;
-    //             img_state <= Img_State_Idle;
-    //         end
-    //     end
-    //     endcase
-    //
-    //     if (img_captureTrigger) begin
-    //         // ice_led <= 4'b1111;
-    //         img_state <= Img_State_Capture;
-    //     end
-    //
-    //     // Convert `imgctrl_status_captureDone` pulse into a toggle
-    //     if (imgctrl_status_captureDone) img_captureDone <= !img_captureDone;
-    // end
-    
-    
-    
     
     
     
@@ -479,10 +423,6 @@ module Testbench();
     reg ice_msp_spi_clk = 0;
     wire ice_msp_spi_data;
     
-    wire sd_clk;
-    wire sd_cmd;
-    wire[3:0] sd_dat;
-    
     wire        img_dclk;
     wire[11:0]  img_d;
     wire        img_fv;
@@ -513,12 +453,6 @@ module Testbench();
     end
     
     Top Top(.*);
-    
-    SDCardSim SDCardSim(
-        .sd_clk(sd_clk),
-        .sd_cmd(sd_cmd),
-        .sd_dat(sd_dat)
-    );
     
     localparam ImageWidth = 64;
     localparam ImageHeight = 32;
@@ -686,339 +620,6 @@ module Testbench();
             $display("[Testbench] ice_led matches (%b) ✅", ice_led);
         end else begin
             $display("[Testbench] ice_led doesn't match (expected: %b, got: %b) ❌", val, ice_led);
-            `Finish;
-        end
-    end endtask
-    
-    
-    
-    
-    
-    
-    
-    localparam CMD0     = 6'd0;     // GO_IDLE_STATE
-    localparam CMD2     = 6'd2;     // ALL_SEND_BIT_CID
-    localparam CMD3     = 6'd3;     // SEND_BIT_RELATIVE_ADDR
-    localparam CMD6     = 6'd6;     // SWITCH_FUNC
-    localparam CMD7     = 6'd7;     // SELECT_CARD/DESELECT_CARD
-    localparam CMD8     = 6'd8;     // SEND_BIT_IF_COND
-    localparam CMD11    = 6'd11;    // VOLTAGE_SWITCH
-    localparam CMD12    = 6'd12;    // STOP_TRANSMISSION
-    localparam CMD25    = 6'd25;    // WRITE_MULTIPLE_BLOCK
-    localparam CMD41    = 6'd41;    // SD_SEND_BIT_OP_COND
-    localparam CMD55    = 6'd55;    // APP_CMD
-    
-    localparam ACMD23    = 6'd23;   // SET_WR_BLK_ERASE_COUNT
-    
-    task SendSDCmd(input[5:0] sdCmd, input[`Msg_Arg_SDSendCmd_RespType_Len-1:0] respType, input[`Msg_Arg_SDSendCmd_DatInType_Len-1:0] datInType, input[31:0] sdArg); begin
-        reg[`Msg_Arg_Len-1] arg;
-        arg = 0;
-        arg[`Msg_Arg_SDSendCmd_RespType_Bits] = respType;
-        arg[`Msg_Arg_SDSendCmd_DatInType_Bits] = datInType;
-        arg[`Msg_Arg_SDSendCmd_CmdData_Bits] = {2'b01, sdCmd, sdArg, 7'b0, 1'b1};
-        
-        SendMsg(`Msg_Type_SDSendCmd, arg);
-    end endtask
-    
-    task SendSDCmdResp(input[5:0] sdCmd, input[`Msg_Arg_SDSendCmd_RespType_Len-1:0] respType, input[`Msg_Arg_SDSendCmd_DatInType_Len-1:0] datInType, input[31:0] sdArg); begin
-        reg[15:0] i;
-        reg done;
-        SendSDCmd(sdCmd, respType, datInType, sdArg);
-        
-        // Wait for SD command to be sent
-        done = 0;
-        for (i=0; i<100 && !done; i++) begin
-            // Request SD status
-            SendMsg(`Msg_Type_SDStatus, 0);
-            // We're done when the SD command is sent
-            done = spi_resp[`Resp_Arg_SDStatus_CmdDone_Bits];
-            // If a response is expected, we're done when the response is received
-            if (respType !== `Msg_Arg_SDSendCmd_RespType_None) done &= spi_resp[`Resp_Arg_SDStatus_RespDone_Bits];
-            if (datInType !== `Msg_Arg_SDSendCmd_DatInType_None) done &= spi_resp[`Resp_Arg_SDStatus_DatInDone_Bits];
-            
-            // // Our clock is much faster than the SD slow clock (16 MHz vs .4 MHz),
-            // // so wait a bit before asking for the status again
-            // #(50_000);
-        end
-        
-        if (!done) begin
-            $display("[Testbench] SD card response timeout ❌");
-            `Finish;
-        end
-    end endtask
-    
-    task TestSDConfig(
-        input[`Msg_Arg_SDInit_Clk_Delay_Len-1:0] delay,
-        input[`Msg_Arg_SDInit_Clk_Speed_Len-1:0] speed,
-        input[`Msg_Arg_SDInit_Trigger_Len-1:0] trigger,
-        input[`Msg_Arg_SDInit_En_Len-1:0] en
-    ); begin
-        reg[`Msg_Arg_Len-1:0] arg;
-        
-        // $display("\n[Testbench] ========== TestSDConfig ==========");
-        arg[`Msg_Arg_SDInit_Clk_Delay_Bits] = delay;
-        arg[`Msg_Arg_SDInit_Clk_Speed_Bits] = speed;
-        arg[`Msg_Arg_SDInit_Trigger_Bits] = trigger;
-        arg[`Msg_Arg_SDInit_En_Bits] = en;
-        
-        SendMsg(`Msg_Type_SDInit, arg);
-    end endtask
-    
-    task TestSDInit; begin
-        reg[15:0] i;
-        reg[`Msg_Arg_Len-1:0] arg;
-        reg done;
-        
-        $display("\n[Testbench] ========== TestSDInit ==========");
-        
-        TestSDConfig(0, `Msg_Arg_SDInit_Clk_Speed_Off,  0, 1); // Clock=off,  InitMode=enabled
-        TestSDConfig(0, `Msg_Arg_SDInit_Clk_Speed_Slow, 0, 1); // Clock=slow, InitMode=enabled
-        // <-- Turn on power to SD card
-        TestSDConfig(0, `Msg_Arg_SDInit_Clk_Speed_Slow, 1, 1); // InitMode=enabled,trigger
-        
-`ifdef SDCARDSIM_LVS_INIT_IGNORE_5MS
-        // Wait 50us, because waiting 5ms takes forever in simulation
-        $display("[Testbench] Waiting 50us (and pretending it's 5ms)...");
-        #(50_000);
-`else
-        // Wait 5ms
-        $display("[Testbench] Waiting 5ms...");
-        #(5_000_000);
-`endif
-        $display("[Testbench] 5ms elapsed");
-        
-        TestSDConfig(0, `Msg_Arg_SDInit_Clk_Speed_Slow, 0, 0); // InitMode=disabled
-        
-        // // Wait for SD init to be complete
-        // done = 0;
-        // for (i=0; i<10 && !done; i++) begin
-        //     // Request SD status
-        //     SendMsg(`Msg_Type_SDStatus, 0);
-        //     // We're done when the `InitDone` bit is set
-        //     done = spi_resp[`Resp_Arg_SDStatus_InitDone_Bits];
-        // end
-        
-        $display("[Testbench] Init done ✅");
-    end endtask
-    
-    task TestSDCMD0; begin
-        // ====================
-        // Test SD CMD0 (GO_IDLE)
-        // ====================
-        $display("\n[Testbench] ========== TestSDCMD0 ==========");
-        SendSDCmdResp(CMD0, `Msg_Arg_SDSendCmd_RespType_None, `Msg_Arg_SDSendCmd_DatInType_None, 0);
-    end endtask
-    
-    task TestSDCMD8; begin
-        // ====================
-        // Test SD CMD8 (SEND_IF_COND)
-        // ====================
-        reg[`Resp_Arg_SDStatus_Resp_Len-1:0] sdResp;
-        
-        $display("\n[Testbench] ========== TestSDCMD8 ==========");
-        
-        // Send SD CMD8
-        SendSDCmdResp(CMD8, `Msg_Arg_SDSendCmd_RespType_48, `Msg_Arg_SDSendCmd_DatInType_None, 32'h000001AA);
-        if (spi_resp[`Resp_Arg_SDStatus_RespCRCErr_Bits] !== 1'b0) begin
-            $display("[Testbench] CRC error ❌");
-            `Finish;
-        end
-
-        sdResp = spi_resp[`Resp_Arg_SDStatus_Resp_Bits];
-        if (sdResp[15:8] !== 8'hAA) begin
-            $display("[Testbench] Bad response: %h ❌", spi_resp);
-            `Finish;
-        end
-    end endtask
-    
-    task TestSDDatOut; begin
-        // ====================
-        // Test writing data to SD card / DatOut
-        // ====================
-        
-        $display("\n========== TestSDDatOut ==========");
-        
-        // Send SD command ACMD23 (SET_WR_BLK_ERASE_COUNT)
-        SendSDCmdResp(CMD55, `Msg_Arg_SDSendCmd_RespType_48, `Msg_Arg_SDSendCmd_DatInType_None, 32'b0);
-        SendSDCmdResp(ACMD23, `Msg_Arg_SDSendCmd_RespType_48, `Msg_Arg_SDSendCmd_DatInType_None, 32'b1);
-        
-        // Send SD command CMD25 (WRITE_MULTIPLE_BLOCK)
-        SendSDCmdResp(CMD25, `Msg_Arg_SDSendCmd_RespType_48, `Msg_Arg_SDSendCmd_DatInType_None, 32'b0);
-        
-        // Clock out data on DAT lines
-        TestImgReadout();
-        
-        // Wait until we're done clocking out data on DAT lines
-        $display("[Testbench] Waiting while data is written...");
-        do begin
-            // Request SD status
-            SendMsg(`Msg_Type_SDStatus, 0);
-        end while(!spi_resp[`Resp_Arg_SDStatus_DatOutDone_Bits]);
-        $display("[Testbench] Done writing (SD resp: %b)", spi_resp[`Resp_Arg_SDStatus_Resp_Bits]);
-        
-        // Check CRC status
-        if (spi_resp[`Resp_Arg_SDStatus_DatOutCRCErr_Bits] === 1'b0) begin
-            $display("[Testbench] DatOut CRC OK ✅");
-        end else begin
-            $display("[Testbench] DatOut CRC bad ❌");
-            `Finish;
-        end
-        
-        // Stop transmission
-        SendSDCmdResp(CMD12, `Msg_Arg_SDSendCmd_RespType_48, `Msg_Arg_SDSendCmd_DatInType_None, 32'b0);
-    end endtask
-
-    task TestSDDatIn; begin
-        // ====================
-        // Test CMD6 (SWITCH_FUNC) + DatIn
-        // ====================
-        
-        $display("\n[Testbench] ========== TestSDDatIn ==========");
-        
-        // Send SD command CMD6 (SWITCH_FUNC)
-        SendSDCmdResp(CMD6, `Msg_Arg_SDSendCmd_RespType_48, `Msg_Arg_SDSendCmd_DatInType_512, 32'h80FFFFF3);
-        $display("[Testbench] Waiting for DatIn to complete...");
-        do begin
-            // Request SD status
-            SendMsg(`Msg_Type_SDStatus, 0);
-        end while(!spi_resp[`Resp_Arg_SDStatus_DatInDone_Bits]);
-        $display("[Testbench] DatIn completed");
-
-        // Check DatIn CRC status
-        if (spi_resp[`Resp_Arg_SDStatus_DatInCRCErr_Bits] === 1'b0) begin
-            $display("[Testbench] DatIn CRC OK ✅");
-        end else begin
-            $display("[Testbench] DatIn CRC bad ❌");
-            `Finish;
-        end
-        
-        // Check the access mode from the CMD6 response
-        if (spi_resp[`Resp_Arg_SDStatus_DatInCMD6AccessMode_Bits] === 4'h3) begin
-            $display("[Testbench] CMD6 access mode == 0x3 ✅");
-        end else begin
-            $display("[Testbench] CMD6 access mode == 0x%h ❌", spi_resp[`Resp_Arg_SDStatus_DatInCMD6AccessMode_Bits]);
-            `Finish;
-        end
-    end endtask
-    
-    task TestSDCMD2; begin
-        // ====================
-        // Test CMD2 (ALL_SEND_CID) + long SD card response (136 bits)
-        //   Note: we expect CRC errors in the response because the R2
-        //   response CRC doesn't follow the semantics of other responses
-        // ====================
-        
-        $display("\n[Testbench] ========== TestSDCMD2 ==========");
-        
-        // Send SD command CMD2 (ALL_SEND_CID)
-        SendSDCmdResp(CMD2, `Msg_Arg_SDSendCmd_RespType_136, `Msg_Arg_SDSendCmd_DatInType_None, 0);
-        $display("[Testbench] ====================================================");
-        $display("[Testbench] ^^^ WE EXPECT CRC ERRORS IN THE SD CARD RESPONSE ^^^");
-        $display("[Testbench] ====================================================");
-    end endtask
-    
-    task TestSDRespRecovery; begin
-        reg done;
-        reg[15:0] i;
-        
-        $display("\n[Testbench] ========== TestSDRespRecovery ==========");
-        
-        // Send an SD command that doesn't provide a response
-        SendSDCmd(CMD0, `Msg_Arg_SDSendCmd_RespType_48, `Msg_Arg_SDSendCmd_DatInType_None, 0);
-        $display("[Testbench] Verifying that Resp times out...");
-        done = 0;
-        for (i=0; i<10 && !done; i++) begin
-            SendMsg(`Msg_Type_SDStatus, 0);
-            $display("[Testbench] Pre-timeout status (%0d/10): sdCmdDone:%b sdRespDone:%b sdDatOutDone:%b sdDatInDone:%b",
-                i+1,
-                spi_resp[`Resp_Arg_SDStatus_CmdDone_Bits],
-                spi_resp[`Resp_Arg_SDStatus_RespDone_Bits],
-                spi_resp[`Resp_Arg_SDStatus_DatOutDone_Bits],
-                spi_resp[`Resp_Arg_SDStatus_DatInDone_Bits]);
-            
-            done = spi_resp[`Resp_Arg_SDStatus_RespDone_Bits];
-        end
-        
-        if (!done) begin
-            $display("[Testbench] Resp timeout ✅");
-            $display("[Testbench] Testing Resp after timeout...");
-            TestSDCMD8();
-            $display("[Testbench] Resp Recovered ✅");
-        
-        end else begin
-            $display("[Testbench] DatIn didn't timeout? ❌");
-            `Finish;
-        end
-    end endtask
-
-    // task TestSDDatOutRecovery; begin
-    //     reg done;
-    //     reg[15:0] i;
-    //
-    //     // Clock out data on DAT lines, but without the SD card
-    //     // expecting data so that we don't get a response
-    //     SendMsg(`Msg_Type_PixReadout, 0);
-    //
-    //     #50000;
-    //
-    //     // Verify that we timeout
-    //     $display("[Testbench] Verifying that DatOut times out...");
-    //     done = 0;
-    //     for (i=0; i<10 && !done; i++) begin
-    //         SendMsg(`Msg_Type_SDStatus, 0);
-    //         $display("[Testbench] Pre-timeout status (%0d/10): sdCmdDone:%b sdRespDone:%b sdDatOutDone:%b sdDatInDone:%b",
-    //             i+1,
-    //             spi_resp[`Resp_Arg_SDStatus_CmdDone_Bits],
-    //             spi_resp[`Resp_Arg_SDStatus_RespDone_Bits],
-    //             spi_resp[`Resp_Arg_SDStatus_DatOutDone_Bits],
-    //             spi_resp[`Resp_Arg_SDStatus_DatInDone_Bits]);
-    //
-    //         done = spi_resp[`Resp_Arg_SDStatus_DatOutDone_Bits];
-    //     end
-    //
-    //     if (!done) begin
-    //         $display("[Testbench] DatOut timeout ✅");
-    //         $display("[Testbench] Testing DatOut after timeout...");
-    //         TestSDDatOut();
-    //         $display("[Testbench] DatOut Recovered ✅");
-    //
-    //     end else begin
-    //         $display("[Testbench] DatOut didn't timeout? ❌");
-    //         `Finish;
-    //     end
-    // end endtask
-
-    task TestSDDatInRecovery; begin
-        reg done;
-        reg[15:0] i;
-        
-        $display("\n[Testbench] ========== TestSDDatInRecovery ==========");
-        
-        // Send SD command that doesn't respond on the DAT lines,
-        // but specify that we expect DAT data
-        SendSDCmd(CMD8, `Msg_Arg_SDSendCmd_RespType_48, `Msg_Arg_SDSendCmd_DatInType_512, 0);
-        $display("[Testbench] Verifying that DatIn times out...");
-        done = 0;
-        for (i=0; i<10 && !done; i++) begin
-            SendMsg(`Msg_Type_SDStatus, 0);
-            $display("[Testbench] Pre-timeout status (%0d/10): sdCmdDone:%b sdRespDone:%b sdDatOutDone:%b sdDatInDone:%b",
-                i+1,
-                spi_resp[`Resp_Arg_SDStatus_CmdDone_Bits],
-                spi_resp[`Resp_Arg_SDStatus_RespDone_Bits],
-                spi_resp[`Resp_Arg_SDStatus_DatOutDone_Bits],
-                spi_resp[`Resp_Arg_SDStatus_DatInDone_Bits]);
-
-            done = spi_resp[`Resp_Arg_SDStatus_DatInDone_Bits];
-        end
-
-        if (!done) begin
-            $display("[Testbench] DatIn timeout ✅");
-            $display("[Testbench] Testing DatIn after timeout...");
-            TestSDDatIn();
-            $display("[Testbench] DatIn Recovered ✅");
-
-        end else begin
-            $display("[Testbench] DatIn didn't timeout? ❌");
             `Finish;
         end
     end endtask
@@ -1253,20 +854,7 @@ module Testbench();
         
         // Do Img stuff before SD stuff, so that an image is ready for readout to the SD card
         TestImgReset();
-        // TestImgI2CWriteRead();
         TestImgCapture();
-        
-        // TestSDInit();
-        // TestSDConfig(0, `Msg_Arg_SDInit_Clk_Speed_Off, 0, 0);
-        // TestSDConfig(0, `Msg_Arg_SDInit_Clk_Speed_Fast, 0, 0);
-        // TestSDCMD0();
-        // TestSDCMD8();
-        // TestSDDatOut();
-        // TestSDCMD2();
-        // TestSDDatIn();
-        // TestSDRespRecovery();
-        // // TestSDDatOutRecovery();
-        // TestSDDatInRecovery();
         
         `Finish;
     end
