8c653b3039bd104d1bf6c1eba6ba8a1607676c83
Dave <dave@toaster.llc>
2021-08-12 00:37:51 -0700

    Debug

--- Code/ICE40/SimDebug/Top.v
+++ Code/ICE40/SimDebug/Top.v
@@ -1,31 +1,20 @@
 `timescale 1ns/1ps
 
-module Testbench();
-    initial begin
-        $dumpfile("Top.vcd");
-        $dumpvars(0, Testbench);
-    end
-    
-    wire PACKAGE_PIN;
-    reg OUTPUT_ENABLE = 0;
+module Top(
+    inout wire PACKAGE_PIN,
+    input wire OUTPUT_ENABLE,
+    input wire clk
+);
     reg D_OUT = 0;
-    wire D_IN;
-    
-    initial begin
-        #1;
-        OUTPUT_ENABLE = 1;
-        #1;
-        D_OUT = 0;
-        #1;
-        $finish;
-    end
-    
     SB_IO #(
-        .PIN_TYPE(6'b1010_01) // Output: tristate; input: unregistered
+        .PIN_TYPE(6'b1010_01)
     ) SB_IO (
         .PACKAGE_PIN(PACKAGE_PIN),
         .OUTPUT_ENABLE(OUTPUT_ENABLE),
-        .D_OUT_0(D_OUT),
-        .D_IN_0(D_IN)
+        .D_OUT_0(D_OUT)
     );
+    
+    always @(posedge clk) begin
+        D_OUT <= clk;
+    end
 endmodule
