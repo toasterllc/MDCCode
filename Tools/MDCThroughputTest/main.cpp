eea6fded4d39828fc5276d7d81c0dd37eddb7fcf
Dave <dave@toaster.llc>
2021-08-21 21:01:55 -0700

    Implemented measurement of raw USB throughput, without any QSPI comms with ICE40

--- Tools/MDCThroughputTest/main.cpp
+++ Tools/MDCThroughputTest/main.cpp
@@ -35,37 +35,41 @@ int main(int argc, const char* argv[]) {
 //        };
 //        usbDevice.write(STApp::Endpoints::CmdOut, cmd);
         
-        constexpr size_t BufCap = 512;
-        std::unique_ptr<uint8_t[]> buf = std::make_unique<uint8_t[]>(BufCap);
-        printf("Reading...\n");
-        usbDevice.read(STApp::Endpoints::DataIn, buf.get(), BufCap);
-        printf("Done\n");
-        const uint32_t* buf32 = (const uint32_t*)buf.get();
-        std::optional<uint32_t> last;
-        for (size_t i=0; i<BufCap/sizeof(uint32_t); i++) {
-            const uint32_t cur = buf32[i];
-            printf("%jx\n", (uintmax_t)(buf32[i]));
-//            if (last) assert(cur == *last+1);
-//            last = cur;
-        }
+//        constexpr size_t BufCap = 512;
+//        std::unique_ptr<uint8_t[]> buf = std::make_unique<uint8_t[]>(BufCap);
+//        printf("Reading...\n");
+//        usbDevice.read(STApp::Endpoints::DataIn, buf.get(), BufCap);
+//        printf("Done\n");
+//        const uint32_t* buf32 = (const uint32_t*)buf.get();
+//        std::optional<uint32_t> last;
+//        for (size_t i=0; i<BufCap/sizeof(uint32_t); i++) {
+//            const uint32_t cur = buf32[i];
+//            printf("%jx\n", (uintmax_t)(buf32[i]));
+////            if (last) assert(cur == *last+1);
+////            last = cur;
+//        }
         
 //        STApp::Status s = {};
 //        usbDevice.read(STApp::Endpoints::DataIn, s);
 //        if (s != STApp::Status::OK) abort();
         
-//        printf("OK\n");
+        printf("Started\n");
         
-//        constexpr size_t BufCap = 128*1024*1024;
-//        std::unique_ptr<uint8_t[]> buf = std::make_unique<uint8_t[]>(BufCap);
-//        for (;;) {
-//            TimeInstant start;
-//            usbDevice.read(STApp::Endpoints::DataIn, buf.get(), BufCap);
-//            
-//            const uintmax_t bits = BufCap*8;
-//            const uintmax_t throughput_bitsPerSec = (1000*bits)/start.durationMs();
-//            const uintmax_t throughput_MbitsPerSec = throughput_bitsPerSec/UINTMAX_C(1000000);
-//            printf("Throughput: %ju Mbits/sec\n", throughput_MbitsPerSec);
-//        }
+        constexpr size_t BufCap = 128*1024*1024;
+        std::unique_ptr<uint8_t[]> buf = std::make_unique<uint8_t[]>(BufCap);
+        for (;;) {
+            TimeInstant start;
+            usbDevice.read(STApp::Endpoints::DataIn, buf.get(), BufCap);
+            
+//            for (int i=0; i<1000; i++) {
+//                printf("%x\n", buf[i]);
+//            }
+            
+            const uintmax_t bits = BufCap*8;
+            const uintmax_t throughput_bitsPerSec = (1000*bits)/start.durationMs();
+            const uintmax_t throughput_MbitsPerSec = throughput_bitsPerSec/UINTMAX_C(1000000);
+            printf("Throughput: %ju Mbits/sec\n", throughput_MbitsPerSec);
+        }
     
     } catch (const std::exception& e) {
         fprintf(stderr, "Error: %s\n\n", e.what());
