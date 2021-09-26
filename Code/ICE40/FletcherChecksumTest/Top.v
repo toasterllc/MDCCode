3963ece9db06c56cc31a0ab598fd16139bf941b5
Dave <dave@toaster.llc>
2021-09-26 16:14:54 -0700

    Testing custom Fletcher implementation

--- Code/ICE40/FletcherChecksumTest/Top.v
+++ Code/ICE40/FletcherChecksumTest/Top.v
@@ -21,27 +21,31 @@ module Testbench();
     localparam ChecksumWidth = 32;
     localparam ChecksumWidthHalf = ChecksumWidth/2;
     
-    wire[ChecksumWidth-1:0] ExpectedChecksum;
-    case (ChecksumWidth)
-    16: assign ExpectedChecksum = 16'h0627;
-    32: assign ExpectedChecksum = 32'hebe19591;
-    64: assign ExpectedChecksum = 64'h312E2B28CCCAC8C6;
-    endcase
-    
     reg                         clk     = 0;
     reg                         rst     = 0;
     reg                         en      = 0;
     wire[ChecksumWidthHalf-1:0] din;
-    wire[ChecksumWidth-1:0]     dout;
+    wire[ChecksumWidth-1:0]     checksum;
+    wire[ChecksumWidth-1:0]     checksumCorrect;
     
     FletcherChecksum #(
         .Width(ChecksumWidth)
     ) FletcherChecksum(
-        .clk    (clk    ),
-        .rst    (rst    ),
-        .en     (en     ),
-        .din    (din    ),
-        .dout   (dout   )
+        .clk    (clk        ),
+        .rst    (rst        ),
+        .en     (en         ),
+        .din    (din        ),
+        .dout   (checksum   )
+    );
+    
+    FletcherChecksumCorrect #(
+        .Width(ChecksumWidth)
+    ) FletcherChecksumCorrect(
+        .clk    (clk                ),
+        .rst    (rst                ),
+        .en     (en                 ),
+        .din    (din                ),
+        .dout   (checksumCorrect    )
     );
     
     reg[63:0] data = 64'h6162636465666768; // abcdefgh
@@ -76,12 +80,12 @@ module Testbench();
             // we read the checksum output, the algorithm hasn't been accidentally peeking
             // ahead.
             data = (data<<ChecksumWidthHalf) | {(ChecksumWidthHalf/8){8'h00}};
-            if (i === 4) en = 0;
+            if (i === 2) en = 0;
             // data = (data<<ChecksumWidthHalf) | {ChecksumWidthHalf{'1}};
             #1;
             
-            if (dout===ExpectedChecksum)    $display("checksum: %h ✅\n", dout);
-            else                            $display("checksum: %h ❌\n", dout);
+            if (checksum===checksumCorrect) $display("checksum: %h ✅ [expected: %h]\n", checksum, checksumCorrect);
+            else                            $display("checksum: %h ❌ [expected: %h]\n", checksum, checksumCorrect);
         end
         
         $finish;
