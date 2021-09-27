217c294a35f30348cc5c7e5309a1cc82539ccea8
Dave <dave@toaster.llc>
2021-09-26 17:00:11 -0700

    Got FletcherChecksumCorrect to be exactly cycle correct, so that the first cycle after the en 1->0 transition has the correct checksum

--- Code/ICE40/FletcherChecksumTest/Top.v
+++ Code/ICE40/FletcherChecksumTest/Top.v
@@ -18,7 +18,7 @@ endmodule
 `ifdef SIM
 
 module Testbench();
-    localparam ChecksumWidth = 16;
+    localparam ChecksumWidth = 64;
     localparam ChecksumWidthHalf = ChecksumWidth/2;
     
     reg                         clk     = 0;
@@ -79,13 +79,15 @@ module Testbench();
             // that definitely affects the checksum, to make sure that at the time that
             // we read the checksum output, the algorithm hasn't been accidentally peeking
             // ahead.
-            data = (data<<ChecksumWidthHalf) | {(ChecksumWidthHalf/8){8'h41}};
-            if (i === 8-1) en = 0;
+            data = (data<<ChecksumWidthHalf) | {(ChecksumWidthHalf/8){8'hFF}};
+            if (i === 2-1) en = 0;
             // data = (data<<ChecksumWidthHalf) | {ChecksumWidthHalf{'1}};
             #1;
             
-            if (checksum===checksumCorrect) $display("checksum: %h ✅ [expected: %h]\n", checksum, checksumCorrect);
-            else                            $display("checksum: %h ❌ [expected: %h]\n", checksum, checksumCorrect);
+            $display("checksum: %h\n", checksumCorrect);
+            
+            // if (checksum===checksumCorrect) $display("checksum: %h ✅ [expected: %h]\n", checksum, checksumCorrect);
+            // else                            $display("checksum: %h ❌ [expected: %h]\n", checksum, checksumCorrect);
         end
         
         $finish;
