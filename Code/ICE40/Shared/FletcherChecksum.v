66ca85b4a6ae0b8b069f8e01f791acdead53cee0
Dave <dave@toaster.llc>
2021-10-03 20:21:08 -0700

    FletcherChecksum.v: try alternative implementation for perf
    Clk                                                   Min      Max      Avg      Med
    --------------------------------------------------------------------------------------
    SDController.clk_slow                                626.57   626.57   626.57   626.57
    ice_img_clk16mhz$SB_IO_IN_$glb_clk                   114.05   130.75   123.55   124.60
    img_dclk$SB_IO_IN_$glb_clk                           106.88   129.75   119.25   119.46
    imgctrl_readout_clk_$glb_clk                         106.41   143.76   127.22   127.34
    ram_clk$SB_IO_OUT_$glb_clk                           115.39   143.37   131.57   132.08
    sd_clk_int                                           246.67   365.23   308.47   305.06
    spi_clk_$glb_clk                                      85.26   108.18    99.27    99.58

--- Code/ICE40/Shared/FletcherChecksum.v
+++ Code/ICE40/Shared/FletcherChecksum.v
@@ -13,35 +13,33 @@ module FletcherChecksum #(
     input wire[WidthHalf-1:0]   din,
     output wire[Width-1:0]      dout
 );
-    reg[WidthHalf:0] a1 = 0;
-    reg[WidthHalf:0] a2 = 0;
+    wire[WidthHalf:0] a1 = a + din - {WidthHalf{'1}};
+    wire[WidthHalf:0] a2 = a + din;
     wire[WidthHalf:0] ax = (!`LeftBit(a1,0) ? a1 : a2);
+    reg[WidthHalf-1:0] a = 0;
     
-    reg[WidthHalf:0] b1 = 0;
-    reg[WidthHalf:0] b2 = 0;
+    wire[WidthHalf:0] b1 = b + a - {WidthHalf{'1}};
+    wire[WidthHalf:0] b2 = b + a;
     wire[WidthHalf:0] bx = (!`LeftBit(b1,0) ? b1 : b2);
+    reg[WidthHalf-1:0] b = 0;
     
     reg enprev = 0;
     
     always @(posedge clk) begin
         if (rst) begin
-            a1 <= 0;
-            a2 <= 0;
-            b1 <= 0;
-            b2 <= 0;
+            a <= 0;
+            b <= 0;
             enprev <= 0;
         
         end else begin
             enprev <= en;
             
             if (en) begin
-                a1 <= ax + din - {WidthHalf{'1}};
-                a2 <= ax + din;
+                a <= ax;
             end
             
             if (enprev) begin
-                b1 <= bx + ax - {WidthHalf{'1}};
-                b2 <= bx + ax;
+                b <= bx;
             end
             
             // $display("[FletcherChecksum]\t\t bx:%h \t ax:%h \t din:%h \t\t rst:%h en:%h [checksum: %h]",
@@ -50,7 +48,7 @@ module FletcherChecksum #(
             //     din, rst, en, dout);
         end
     end
-    assign dout = {bx[WidthHalf-1:0], ax[WidthHalf-1:0]};
+    assign dout = {b, a};
 endmodule
 
 module FletcherChecksumCorrect #(
