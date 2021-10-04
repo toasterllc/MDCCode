ec876f1cbba6188e8efdad39b920df1c6a5ccaa6
Dave <dave@toaster.llc>
2021-10-04 01:37:43 -0700

    Revert "FletcherChecksum: Trying to optimize"
    This reverts commit 9fa6d318b6f2d61aa785f64e3d540fcb94e049cf.

--- Code/ICE40/Shared/FletcherChecksum.v
+++ Code/ICE40/Shared/FletcherChecksum.v
@@ -13,42 +13,44 @@ module FletcherChecksum #(
     input wire[WidthHalf-1:0]   din,
     output wire[Width-1:0]      dout
 );
-    reg[WidthHalf+6:0] a = 0;
-    reg[WidthHalf+6:0] asub = 0;
+    reg[WidthHalf:0] a1 = 0;
+    reg[WidthHalf:0] a2 = 0;
+    wire[WidthHalf:0] ax = (!`LeftBit(a1,0) ? a1 : a2);
     
-    reg[WidthHalf+6:0] b = 0;
-    reg[WidthHalf+6:0] bsub = 0;
+    reg[WidthHalf:0] b1 = 0;
+    reg[WidthHalf:0] b2 = 0;
+    wire[WidthHalf:0] bx = (!`LeftBit(b1,0) ? b1 : b2);
     
     reg enprev = 0;
     
     always @(posedge clk) begin
         if (rst) begin
-            a <= 0;
-            asub <= 0;
-            b <= 0;
-            bsub <= 0;
+            a1 <= 0;
+            a2 <= 0;
+            b1 <= 0;
+            b2 <= 0;
             enprev <= 0;
         
         end else begin
             enprev <= en;
             
             if (en) begin
-                a <= a + din - asub;
-                asub <= (a>=255*2 ? 255*2 : 0);
+                a1 <= ax + din - {WidthHalf{'1}};
+                a2 <= ax + din;
             end
             
             if (enprev) begin
-                b <= b + a - bsub;
-                bsub <= (b>=255*3 ? 255*3 : 0);
+                b1 <= bx + ax - {WidthHalf{'1}};
+                b2 <= bx + ax;
             end
             
-            $display("[FletcherChecksum]\t\t b:%h bsub:%h \t a:%h asub:%h \t din:%h \t\t rst:%h en:%h [checksum: %h]",
-                b, bsub,
-                a, asub,
-                din, rst, en, dout);
+            // $display("[FletcherChecksum]\t\t bx:%h \t ax:%h \t din:%h \t\t rst:%h en:%h [checksum: %h]",
+            //     bx,
+            //     ax,
+            //     din, rst, en, dout);
         end
     end
-    assign dout = {b[WidthHalf-1:0], a[WidthHalf-1:0]};
+    assign dout = {bx[WidthHalf-1:0], ax[WidthHalf-1:0]};
 endmodule
 
 module FletcherChecksumCorrect #(
