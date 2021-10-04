ccb83fe59d1610842ba128b22135819185f333e5
Dave <dave@toaster.llc>
2021-10-03 20:01:55 -0700

    Debugging
    Need to revert this commit

--- Code/STM32/STApp/Source/System.cpp
+++ Code/STM32/STApp/Source/System.cpp
@@ -80,6 +80,16 @@ void System::run() {
         Assert(!status.respCRCErr());
     }
     
+    // ====================
+    // CMD12 | STOP_TRANSMISSION
+    //   State: Send Data -> Transfer
+    //   Finish reading
+    // ====================
+    {
+        auto status = _sd_sendCmd(SDSendCmdMsg::CMD12, 0);
+        Assert(!status.respCRCErr());
+    }
+    
     
     
 //    Task::Run(
@@ -569,7 +579,7 @@ SDStatusResp System::_sd_sendCmd(
         return status;
     }
     // Timeout sending SD command
-    abort();
+//    abort();
 }
 
 void System::_sd_task() {
