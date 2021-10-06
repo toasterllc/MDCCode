b9f78e6d3e1db30669cdf204f121856932b2946c
Dave <dave@toaster.llc>
2021-10-05 23:41:24 -0700

    Nextpnr.sh: make writing the .asc file optional

--- Code/ICE40/Nextpnr.sh
+++ Code/ICE40/Nextpnr.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -lt 3 ]; then
 	echo "Usage:"
-    echo "  Nextpnr.sh <DeviceType> <DevicePackage> <ProjName>"
+    echo "  Nextpnr.sh <DeviceType> <DevicePackage> <ProjName> [<OutputASCFilePath>]"
 	echo "    DeviceType: hx1k (iCEstick), hx8k (MDC Rev4, iCE40HX board)"
 	echo "    DevicePackage: tq144 (iCEstick), bg121:4k (MDC Rev4), ct256 (iCE40HX board)"
 	
 	echo
 	echo "Examples:"
-	echo "  Nextpnr.sh hx8k bg121:4k Blinky     # MDC Rev4"
-	echo "  Nextpnr.sh hx1k tq144 Blinky        # iCEstick"
-	echo "  Nextpnr.sh hx8k ct256 Blinky        # iCE40HX board"
+	echo "  Nextpnr.sh hx8k bg121:4k Blinky Top.asc     # MDC Rev4"
+	echo "  Nextpnr.sh hx1k tq144 Blinky                # iCEstick"
+	echo "  Nextpnr.sh hx8k ct256 Blinky                # iCE40HX board"
 	exit 1
 fi
 
@@ -27,15 +27,23 @@ if [ ! -n "$pkg" ]; then echo "Bad package" ; exit 1; fi
 proj="$3"
 if [ ! -n "$proj" ]; then echo "Bad project name" ; exit 1; fi
 
-nextpnr-ice40                                                       \
-    "--$dev"                                                        \
-    --package                   "$pkg"                              \
-    --json                      "$rootDir/$proj/Synth/Top.json"     \
-    --pcf                       "$rootDir/Pins.pcf"                 \
-    --randomize-seed                                                \
-    --pcf-allow-unconstrained                                       \
-    --placer-heap-alpha         0.025                               \
-    --placer-heap-beta          0.5                                 \
-    --placer-heap-critexp       3                                   \
-    --placer-heap-timingweight  11                                  \
-    --asc                       "$rootDir/$proj/Synth/Top.asc"      \
+outputASCFilePath="$4"
+
+args=(
+    "--$dev"
+    --package                   "$pkg"
+    --json                      "$rootDir/$proj/Synth/Top.json"
+    --pcf                       "$rootDir/Pins.pcf"
+    --randomize-seed
+    --pcf-allow-unconstrained
+    --placer-heap-alpha         0.025
+    --placer-heap-beta          0.5
+    --placer-heap-critexp       3
+    --placer-heap-timingweight  11
+)
+
+if [ ! -z "$outputASCFilePath" ]; then
+    args+=(--asc "$outputASCFilePath")
+fi
+
+nextpnr-ice40 "${args[@]}"
