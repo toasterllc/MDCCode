dac532dce8d8b5534bcdeb6716e00c7dcf360a53
Dave <dave@toaster.llc>
2021-11-04 16:12:17 -0700

    ImgController.v: skip perf experiment
    Clk                                                   Min      Max      Avg      Med
    --------------------------------------------------------------------------------------
    ICEApp.SDController.clk_slow                         626.57   626.57   626.57   626.57
    ICEApp.readoutfifo_r_clk_$glb_clk                    130.16   143.91   138.84   140.11
    ICEApp.sd_clk_int                                    365.23   365.23   365.23   365.23
    ICEApp.spi_clk_$glb_clk                              107.81   126.20   117.11   116.84
    ice_img_clk16mhz$SB_IO_IN_$glb_clk                   110.10   133.19   119.69   118.11
    img_dclk$SB_IO_IN_$glb_clk                           122.20   140.88   127.29   125.98
    ram_clk$SB_IO_OUT_$glb_clk                           123.89   138.16   130.65   130.11

--- Code/ICE40/Nextpnr.sh
+++ Code/ICE40/Nextpnr.sh
@@ -56,10 +56,10 @@ args=(
     #   ./nextpnr-mango.py nextpnr-ice40 --hx8k --package bg121:4k  \
     #     --json Top.json --pcf Pins.pcf --pcf-allow-unconstrained
     
-    --placer-heap-alpha         0.15
-    --placer-heap-beta          0.775
-    --placer-heap-critexp       9
-    --placer-heap-timingweight  11
+    --placer-heap-alpha         0.275
+    --placer-heap-beta          0.575
+    --placer-heap-critexp       4
+    --placer-heap-timingweight  16
 )
 
 if [ ! -z "$outputASCFilePath" ]; then
