3bcc39f03f331030ab58aa252fd1023a9987a213
Dave <dave@toaster.llc>
2021-11-10 11:16:27 -0800

    MSPApp: cleanup GPIO

--- Code/MSP430/MSPApp/GPIO.h
+++ Code/MSP430/MSPApp/GPIO.h
@@ -9,23 +9,33 @@ enum class GPIOPort {
 };
 
 enum class GPIOOption : uint8_t {
-    Out,
-    Dir,
-    Sel0,
-    Sel1,
-    REn,
-    IE,
-    IES,
+    // Direction + output value
+    Input,
+    Output0,
+    Output1,
+    
+    // Pullup/pulldown resistor
+    Resistor0,
+    Resistor1,
+    
+    // Function selection
+    Sel01,
+    Sel10,
+    Sel11,
+    
+    // Interrupts
+    Interrupt01,
+    Interrupt10,
 };
 
 struct _GPIORegs {
-    uint16_t out  = 0;
-    uint16_t dir  = 0;
-    uint16_t sel0 = 0;
-    uint16_t sel1 = 0;
-    uint16_t ren  = 0;
-    uint16_t ie   = 0;
-    uint16_t ies  = 0;
+    uint16_t Out  = 0;
+    uint16_t Dir  = 0;
+    uint16_t Sel0 = 0;
+    uint16_t Sel1 = 0;
+    uint16_t REn  = 0;
+    uint16_t IE   = 0;
+    uint16_t IES  = 0;
 };
 
 template <GPIOPort Port>
@@ -33,13 +43,13 @@ static constexpr void _GPIOInit(_GPIORegs regs) {
     static_assert(Port == GPIOPort::A, "");
     
     #warning modifying PAIE / PAIES can trigger an interrupt, so if we do that we should: disable interrupts, update regs, clear relevent interrupts, enable interrupts
-    PAOUT   = regs.out;
-    PADIR   = regs.dir;
-    PASEL0  = regs.sel0;
-    PASEL1  = regs.sel1;
-    PAREN   = regs.ren;
-    PAIE    = regs.ie;
-    PAIES   = regs.ies;
+    PAOUT   = regs.Out;
+    PADIR   = regs.Dir;
+    PASEL0  = regs.Sel0;
+    PASEL1  = regs.Sel1;
+    PAREN   = regs.REn;
+    PAIE    = regs.IE;
+    PAIES   = regs.IES;
     
 //    PAOUT   = 0xAAAA;
 //    PADIR   = 0xBBBB;
@@ -49,13 +59,13 @@ static constexpr void _GPIOInit(_GPIORegs regs) {
 //    PAIE    = 0xFFFF;
 //    PAIES   = 0x4243;
     
-//    printf("PAOUT   = 0x%04x\n", regs.out);
-//    printf("PADIR   = 0x%04x\n", regs.dir);
-//    printf("PASEL0  = 0x%04x\n", regs.sel0);
-//    printf("PASEL1  = 0x%04x\n", regs.sel1);
-//    printf("PAREN   = 0x%04x\n", regs.ren);
-//    printf("PAIE    = 0x%04x\n", regs.ie);
-//    printf("PAIES   = 0x%04x\n", regs.ies);
+//    printf("PAOUT   = 0x%04x\n", regs.Out);
+//    printf("PADIR   = 0x%04x\n", regs.Dir);
+//    printf("PASEL0  = 0x%04x\n", regs.Sel0);
+//    printf("PASEL1  = 0x%04x\n", regs.Sel1);
+//    printf("PAREN   = 0x%04x\n", regs.REn);
+//    printf("PAIE    = 0x%04x\n", regs.IE);
+//    printf("PAIES   = 0x%04x\n", regs.IES);
 }
 
 template<GPIOPort Port, typename GPIO, typename... GPIOs>
@@ -63,22 +73,22 @@ static constexpr void _GPIOInit(_GPIORegs regs) {
     static_assert(Port == GPIO::Port, "");
     
     // Clear the bit for the GPIO
-    regs.out    &= ~GPIO::Bit;
-    regs.dir    &= ~GPIO::Bit;
-    regs.sel0   &= ~GPIO::Bit;
-    regs.sel1   &= ~GPIO::Bit;
-    regs.ren    &= ~GPIO::Bit;
-    regs.ie     &= ~GPIO::Bit;
-    regs.ies    &= ~GPIO::Bit;
+    regs.Out    &= ~GPIO::Bit;
+    regs.Dir    &= ~GPIO::Bit;
+    regs.Sel0   &= ~GPIO::Bit;
+    regs.Sel1   &= ~GPIO::Bit;
+    regs.REn    &= ~GPIO::Bit;
+    regs.IE     &= ~GPIO::Bit;
+    regs.IES    &= ~GPIO::Bit;
     
     // Set the bit for the GPIO, if it's set
-    regs.out    |= (GPIO::InitConfig::Out()  ? GPIO::Bit : 0);
-    regs.dir    |= (GPIO::InitConfig::Dir()  ? GPIO::Bit : 0);
-    regs.sel0   |= (GPIO::InitConfig::Sel0() ? GPIO::Bit : 0);
-    regs.sel1   |= (GPIO::InitConfig::Sel1() ? GPIO::Bit : 0);
-    regs.ren    |= (GPIO::InitConfig::REn()  ? GPIO::Bit : 0);
-    regs.ie     |= (GPIO::InitConfig::IE()   ? GPIO::Bit : 0);
-    regs.ies    |= (GPIO::InitConfig::IES()  ? GPIO::Bit : 0);
+    regs.Out    |= (GPIO::InitConfig::Out()  ? GPIO::Bit : 0);
+    regs.Dir    |= (GPIO::InitConfig::Dir()  ? GPIO::Bit : 0);
+    regs.Sel0   |= (GPIO::InitConfig::Sel0() ? GPIO::Bit : 0);
+    regs.Sel1   |= (GPIO::InitConfig::Sel1() ? GPIO::Bit : 0);
+    regs.REn    |= (GPIO::InitConfig::REn()  ? GPIO::Bit : 0);
+    regs.IE     |= (GPIO::InitConfig::IE()   ? GPIO::Bit : 0);
+    regs.IES    |= (GPIO::InitConfig::IES()  ? GPIO::Bit : 0);
     
     _GPIOInit<Port, GPIOs...>(regs);
 }
@@ -92,13 +102,13 @@ template<typename... GPIOs>
 static constexpr void GPIOInit() {
     // Default state for all pins: output strong 0
     _GPIOInit<GPIOs...>(_GPIORegs{
-        .out  = 0x0000,
-        .dir  = 0xFFFF,
-        .sel0 = 0x0000,
-        .sel1 = 0x0000,
-        .ren  = 0x0000,
-        .ie   = 0x0000,
-        .ies  = 0x0000,
+        .Out  = 0x0000,
+        .Dir  = 0xFFFF,
+        .Sel0 = 0x0000,
+        .Sel1 = 0x0000,
+        .REn  = 0x0000,
+        .IE   = 0x0000,
+        .IES  = 0x0000,
     });
     
     // Unlock GPIOs
@@ -113,13 +123,13 @@ public:
     static constexpr uint16_t Bit   = UINT16_C(1)<<Pin;
     
     struct InitConfig {
-        static constexpr bool Out()     { return _InitGetter(GPIOOption::Out);   }
-        static constexpr bool Dir()     { return _InitGetter(GPIOOption::Dir);   }
-        static constexpr bool Sel0()    { return _InitGetter(GPIOOption::Sel0);  }
-        static constexpr bool Sel1()    { return _InitGetter(GPIOOption::Sel1);  }
-        static constexpr bool REn()     { return _InitGetter(GPIOOption::REn);   }
-        static constexpr bool IE()      { return _InitGetter(GPIOOption::IE);    }
-        static constexpr bool IES()     { return _InitGetter(GPIOOption::IES);   }
+        static constexpr bool Out()     { return _InitGetter(GPIOOption::Output1)       || _InitGetter(GPIOOption::Resistor1);      }
+        static constexpr bool Dir()     { return _InitGetter(GPIOOption::Output0)       || _InitGetter(GPIOOption::Output1);        }
+        static constexpr bool Sel0()    { return _InitGetter(GPIOOption::Sel01)         || _InitGetter(GPIOOption::Sel11);          }
+        static constexpr bool Sel1()    { return _InitGetter(GPIOOption::Sel10)         || _InitGetter(GPIOOption::Sel11);          }
+        static constexpr bool REn()     { return _InitGetter(GPIOOption::Resistor0)     || _InitGetter(GPIOOption::Resistor1);      }
+        static constexpr bool IE()      { return _InitGetter(GPIOOption::Interrupt01)   || _InitGetter(GPIOOption::Interrupt10);    }
+        static constexpr bool IES()     { return _InitGetter(GPIOOption::Interrupt10);                                              }
     };
     
     static constexpr void Init() {
@@ -132,24 +142,28 @@ public:
         IES(    InitConfig::IES()   );
     }
     
-    static constexpr bool Out()         { return _Getter(PAOUT);        }
-    static constexpr bool Dir()         { return _Getter(PADIR);        }
-    static constexpr bool Sel0()        { return _Getter(PASEL0);       }
-    static constexpr bool Sel1()        { return _Getter(PASEL1);       }
-    static constexpr bool REn()         { return _Getter(PAREN);        }
-    static constexpr bool IE()          { return _Getter(PAIE);         }
-    static constexpr bool IES()         { return _Getter(PAIES);        }
+    static constexpr bool Out()         { if (Port == GPIOPort::A) return _Getter(PAOUT);   }
+    static constexpr bool Dir()         { if (Port == GPIOPort::A) return _Getter(PADIR);   }
+    static constexpr bool Sel0()        { if (Port == GPIOPort::A) return _Getter(PASEL0);  }
+    static constexpr bool Sel1()        { if (Port == GPIOPort::A) return _Getter(PASEL1);  }
+    static constexpr bool REn()         { if (Port == GPIOPort::A) return _Getter(PAREN);   }
+    static constexpr bool IE()          { if (Port == GPIOPort::A) return _Getter(PAIE);    }
+    static constexpr bool IES()         { if (Port == GPIOPort::A) return _Getter(PAIES);   }
     
-    static constexpr void Out(bool x)   { return _Setter(PAOUT, x);     }
-    static constexpr void Dir(bool x)   { return _Setter(PADIR, x);     }
-    static constexpr void Sel0(bool x)  { return _Setter(PASEL0, x);    }
-    static constexpr void Sel1(bool x)  { return _Setter(PASEL1, x);    }
-    static constexpr void REn(bool x)   { return _Setter(PAREN, x);     }
-    static constexpr void IE(bool x)    { return _Setter(PAIE, x);      }
-    static constexpr void IES(bool x)   { return _Setter(PAIES, x);     }
+    static constexpr void Out(bool x)   { if (Port == GPIOPort::A) _Setter(PAOUT, x);   }
+    static constexpr void Dir(bool x)   { if (Port == GPIOPort::A) _Setter(PADIR, x);   }
+    static constexpr void Sel0(bool x)  { if (Port == GPIOPort::A) _Setter(PASEL0, x);  }
+    static constexpr void Sel1(bool x)  { if (Port == GPIOPort::A) _Setter(PASEL1, x);  }
+    static constexpr void REn(bool x)   { if (Port == GPIOPort::A) _Setter(PAREN, x);   }
+    static constexpr void IE(bool x)    { if (Port == GPIOPort::A) _Setter(PAIE, x);    }
+    static constexpr void IES(bool x)   { if (Port == GPIOPort::A) _Setter(PAIES, x);   }
     
     static bool Read() {
         if (Port == GPIOPort::A) return PAIN & Bit;
+    }
+    
+    static void Write(bool x) {
+        Out(x);
     }
     
 private:
@@ -162,21 +176,13 @@ private:
     
     template <typename T>
     static constexpr bool _Getter(T& reg) {
-        if (Port == GPIOPort::A) return reg & Bit;
+        return reg & Bit;
     }
     
     template <typename T>
     static constexpr void _Setter(T& reg, bool x) {
-        if (x) {
-            if (Port == GPIOPort::A) {
-                reg |= Bit;
-            }
-        
-        } else {
-            if (Port == GPIOPort::A) {
-                reg &= ~Bit;
-            }
-        }
+        if (x)  reg |= Bit;
+        else    reg &= ~Bit;
     }
 };
 
