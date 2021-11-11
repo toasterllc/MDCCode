fa457b8a5fac31c393529417160415fe04f008bf
Dave <dave@toaster.llc>
2021-11-11 12:35:36 -0800

    Debugging. This commit: MOTION_SIGNAL hooked to interrupt, but fails to wake MSP when MSP was cold-started.

--- Code/MSP430/MSPApp/main.cpp
+++ Code/MSP430/MSPApp/main.cpp
@@ -1,105 +1,96 @@
 #include <msp430.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <inttypes.h>
-#include <cstddef>
-#include "GPIO.h"
-#include "Clock.h"
-#include "Assert.h"
 
-static constexpr uint64_t MCLKFreqHz = 16000000;
-static constexpr uint32_t XT1FreqHz = 32768;
+static void ledFlash() {
+    for (;;) {
+        P2OUT ^= BIT6;
+        __delay_cycles(100000);
+    }
+}
 
-#define _sleepUs(us) __delay_cycles((((uint64_t)us)*MCLKFreqHz) / 1000000)
-#define _sleepMs(ms) __delay_cycles((((uint64_t)ms)*MCLKFreqHz) / 1000)
-
-struct Pin {
-    // Default GPIOs
-    using VDD_1V9_IMG_EN                    = GPIOA<0x0, GPIOOption::Output0>;
-    using VDD_2V8_IMG_EN                    = GPIOA<0x2, GPIOOption::Output0>;
-    using ICE_MSP_SPI_DATA_DIR              = GPIOA<0x3, GPIOOption::Output0, GPIOOption::Sel10>;
-    using ICE_MSP_SPI_DATA_IN               = GPIOA<0x4, GPIOOption::Input>;
-//    using ICE_MSP_SPI_DATA_IN               = GPIOA<0x4, GPIOOption::Interrupt01>;
-//    using ICE_MSP_SPI_DATA_UCA0SOMI         = GPIOA<0x5, GPIOOption::Output0>;
-    using ICE_MSP_SPI_DATA_UCA0SOMI         = GPIOA<0x5, GPIOOption::Input, GPIOOption::Resistor0>;
-    using ICE_MSP_SPI_CLK_MANUAL            = GPIOA<0x6, GPIOOption::Output1>;
-    using ICE_MSP_SPI_AUX                   = GPIOA<0x7, GPIOOption::Output0>;
-    using XOUT                              = GPIOA<0x8, GPIOOption::Sel10>;
-    using XIN                               = GPIOA<0x9, GPIOOption::Sel10>;
-    using ICE_MSP_SPI_AUX_DIR               = GPIOA<0xA, GPIOOption::Output1>;
-    using VDD_SD_EN                         = GPIOA<0xB, GPIOOption::Output0>;
-    using VDD_B_EN_                         = GPIOA<0xC, GPIOOption::Output1>;
-//    using MOTION_SIGNAL                     = GPIOA<0xD, GPIOOption::Input>;
-    using MOTION_SIGNAL                     = GPIOA<0xD, GPIOOption::Interrupt01>;
-    
-    using DEBUG_OUT                         = GPIOA<0xE, GPIOOption::Output0>;
-};
-
-using Clock = ClockType<XT1FreqHz, MCLKFreqHz>;
-
-#pragma mark - Interrupts
-
-static volatile bool Event = false;
+static void delay(int sec) {
+    for (int i=0; i<sec; i++) {
+        __delay_cycles(1000000);
+    }
+}
 
 __attribute__((interrupt(PORT2_VECTOR)))
-void _isr_port2() {
-    for (int i=0;; i++) {
-        Pin::DEBUG_OUT::Write(i&1);
-    }
+void _isr() {
+    // Wake from LPM3.5
+    ledFlash();
+    
+//    switch(__even_in_range(P2IV, P2IV_P2IFG3)) {
+//    case P2IV_P2IFG3:
+//        P1OUT ^= 0x01;
+//        break;
+//    default:
+//        break;
+//    }
 }
 
-#pragma mark - Main
-
-__attribute__((section(".data;"), noinline)) // Trailing semicolon is a hack to silence an assembler warning
 static void _sleep() {
-    GCCTL0 &= ~(FRPWR|FRLPMPWR); //clear FRPWR and FRLPMPWR
-    // Go to sleep in LPM3.5
-    __bis_SR_register(GIE | LPM3_bits);
-}
-
-int main() {
-    // Stop watchdog timer
-    WDTCTL = WDTPW | WDTHOLD;
-    
-    // Init GPIOs
-    GPIOInit<
-        Pin::VDD_1V9_IMG_EN,
-        Pin::VDD_2V8_IMG_EN,
-        Pin::ICE_MSP_SPI_DATA_DIR,
-        Pin::ICE_MSP_SPI_DATA_IN,
-        Pin::ICE_MSP_SPI_DATA_UCA0SOMI,
-        Pin::ICE_MSP_SPI_CLK_MANUAL,
-        Pin::ICE_MSP_SPI_AUX,
-        Pin::XOUT,
-        Pin::XIN,
-        Pin::ICE_MSP_SPI_AUX_DIR,
-        Pin::VDD_SD_EN,
-        Pin::VDD_B_EN_,
-        Pin::MOTION_SIGNAL,
-        Pin::DEBUG_OUT
-    >();
-    
-    // Init clock
-    Clock::Init();
-    
-    if (SYSRSTIV == SYSRSTIV_LPM5WU) {
-        for (int i=0;; i++) {
-            Pin::DEBUG_OUT::Write(i&1);
-        }
-    }
-    
-    _sleepMs(7000);
-    
-    P2IFG = 0;
-    __bis_SR_register(GIE);
-    
     // Disable regulator so we enter LPM3.5 (instead of just LPM3)
     PMMCTL0_H = PMMPW_H; // Open PMM Registers for write
     PMMCTL0_L |= PMMREGOFF;
     
     // Go to sleep in LPM3.5
-    _sleep();
+    __bis_SR_register(GIE | LPM3_bits);
+}
+
+//__attribute__((section(".data"), noinline)) // Trailing semicolon is a hack to silence an assembler warning
+//void _sleep() {
+//    // Disable regulator so we enter LPM3.5 (instead of just LPM3)
+//    PMMCTL0_H = PMMPW_H; // Open PMM Registers for write
+//    PMMCTL0_L |= PMMREGOFF;
+//    
+////    FRCTL0 = FRCTLPW;
+////    GCCTL0 &= ~(FRPWR|FRLPMPWR);
+////    FRCTL0_H = 0;
+//    // Go to sleep in LPM3.5
 //    __bis_SR_register(GIE | LPM3_bits);
+//}
+
+int main() {
+    // Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;
+    
+    // Cold start
+    P1OUT   = 0x00;
+    P1DIR   = 0x00;
+    P1SEL0  = 0x00;
+    P1SEL1  = 0x00;
+    P1REN   = 0x00;
+    P1IES   = 0x00;
+    
+    P2OUT   = BIT6;
+    P2DIR   = BIT6;
+    P2SEL0  = 0x00;
+    P2SEL1  = 0x00;
+    P2REN   = BIT5;
+    P2IES   = 0x00;
+    
+    PM5CTL0 &= ~LOCKLPM5;
+    
+    P1IE    = 0x00;
+    P2IE    = BIT5;
+    
+    if (SYSRSTIV == SYSRSTIV_LPM5WU) {
+        ledFlash();
+        __bis_SR_register(GIE);
+        for (;;);
+//        // Wake from LPM3.5
+//        P1DIR = 0x01;
+//        for (;;) {
+//            P1OUT ^= 0x01;
+//            __delay_cycles(100000);
+//        }
+    }
+    
+    delay(7);
+    
+    P1IFG   = 0x00;
+    P2IFG   = 0x00;
+    
+    _sleep();
     
     return 0;
 }
