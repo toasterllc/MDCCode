411d8c6e407c46d1d4f53837557f0b6ed3e9c272
Dave <dave@toaster.llc>
2021-11-11 12:50:12 -0800

    Simplify

--- Code/MSP430/MSPApp/main.cpp
+++ Code/MSP430/MSPApp/main.cpp
@@ -2,7 +2,7 @@
 
 static void ledFlash() {
     for (;;) {
-        P2OUT ^= BIT6;
+        PAOUT ^= BITE;
         __delay_cycles(100000);
     }
 }
@@ -12,31 +12,22 @@ int main() {
     WDTCTL = WDTPW | WDTHOLD;
     
     // Cold start
-    P1OUT   = 0x00;
-    P1DIR   = 0x00;
-    P1SEL0  = 0x00;
-    P1SEL1  = 0x00;
-    P1REN   = 0x00;
-    P1IES   = 0x00;
-    
-    P2OUT   = BIT6;
-    P2DIR   = BIT6;
-    P2SEL0  = 0x00;
-    P2SEL1  = 0x00;
-    P2REN   = BIT4;
-    P2IES   = 0x00;
+    PAOUT   = BITE;
+    PADIR   = BITE;
+    PASEL0  = 0;
+    PASEL1  = 0;
+    PAREN   = BITC;
+    PAIES   = 0;
     
     PM5CTL0 &= ~LOCKLPM5;
     
-    P1IE    = 0x00;
-    P2IE    = BIT4;
+    PAIE    = BITC;
     
     if (SYSRSTIV == SYSRSTIV_LPM5WU) {
         ledFlash();
     }
     
-    P1IFG   = 0x00;
-    P2IFG   = 0x00;
+    PAIFG   = 0;
     
     // Disable regulator so we enter LPM3.5 (instead of just LPM3)
     PMMCTL0_H = PMMPW_H; // Open PMM Registers for write
