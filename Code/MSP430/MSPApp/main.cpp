d49b8b1cb77ab836b2288d610ebcc0efc2798d04
Dave <dave@toaster.llc>
2021-11-11 12:47:06 -0800

    Debugging

--- Code/MSP430/MSPApp/main.cpp
+++ Code/MSP430/MSPApp/main.cpp
@@ -7,48 +7,6 @@ static void ledFlash() {
     }
 }
 
-static void delay(int sec) {
-    for (int i=0; i<sec; i++) {
-        __delay_cycles(1000000);
-    }
-}
-
-__attribute__((interrupt(PORT2_VECTOR)))
-void _isr() {
-    // Wake from LPM3.5
-    ledFlash();
-    
-//    switch(__even_in_range(P2IV, P2IV_P2IFG3)) {
-//    case P2IV_P2IFG3:
-//        P1OUT ^= 0x01;
-//        break;
-//    default:
-//        break;
-//    }
-}
-
-static void _sleep() {
-    // Disable regulator so we enter LPM3.5 (instead of just LPM3)
-    PMMCTL0_H = PMMPW_H; // Open PMM Registers for write
-    PMMCTL0_L |= PMMREGOFF;
-    
-    // Go to sleep in LPM3.5
-    __bis_SR_register(GIE | LPM3_bits);
-}
-
-//__attribute__((section(".data"), noinline)) // Trailing semicolon is a hack to silence an assembler warning
-//void _sleep() {
-//    // Disable regulator so we enter LPM3.5 (instead of just LPM3)
-//    PMMCTL0_H = PMMPW_H; // Open PMM Registers for write
-//    PMMCTL0_L |= PMMREGOFF;
-//    
-////    FRCTL0 = FRCTLPW;
-////    GCCTL0 &= ~(FRPWR|FRLPMPWR);
-////    FRCTL0_H = 0;
-//    // Go to sleep in LPM3.5
-//    __bis_SR_register(GIE | LPM3_bits);
-//}
-
 int main() {
     // Stop watchdog timer
     WDTCTL = WDTPW | WDTHOLD;
@@ -65,32 +23,27 @@ int main() {
     P2DIR   = BIT6;
     P2SEL0  = 0x00;
     P2SEL1  = 0x00;
-    P2REN   = BIT5;
+    P2REN   = BIT4;
     P2IES   = 0x00;
     
     PM5CTL0 &= ~LOCKLPM5;
     
     P1IE    = 0x00;
-    P2IE    = BIT5;
+    P2IE    = BIT4;
     
     if (SYSRSTIV == SYSRSTIV_LPM5WU) {
         ledFlash();
-        __bis_SR_register(GIE);
-        for (;;);
-//        // Wake from LPM3.5
-//        P1DIR = 0x01;
-//        for (;;) {
-//            P1OUT ^= 0x01;
-//            __delay_cycles(100000);
-//        }
     }
-    
-    delay(7);
     
     P1IFG   = 0x00;
     P2IFG   = 0x00;
     
-    _sleep();
+    // Disable regulator so we enter LPM3.5 (instead of just LPM3)
+    PMMCTL0_H = PMMPW_H; // Open PMM Registers for write
+    PMMCTL0_L |= PMMREGOFF;
+    
+    // Go to sleep in LPM3.5
+    __bis_SR_register(GIE | LPM3_bits);
     
     return 0;
 }
