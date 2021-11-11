65edd796470c9e74a784a19b0e2e266ae6998ca6
Dave <dave@toaster.llc>
2021-11-10 23:15:19 -0800

    Debugging

--- Code/MSP430/MSPApp/main.cpp
+++ Code/MSP430/MSPApp/main.cpp
@@ -18,7 +18,8 @@ struct Pin {
     using VDD_1V9_IMG_EN                    = GPIOA<0x0, GPIOOption::Output0>;
     using VDD_2V8_IMG_EN                    = GPIOA<0x2, GPIOOption::Output0>;
     using ICE_MSP_SPI_DATA_DIR              = GPIOA<0x3, GPIOOption::Output1>;
-    using ICE_MSP_SPI_DATA_IN               = GPIOA<0x4, GPIOOption::Interrupt01>;
+    using ICE_MSP_SPI_DATA_IN               = GPIOA<0x4, GPIOOption::Input>;
+//    using ICE_MSP_SPI_DATA_IN               = GPIOA<0x4, GPIOOption::Interrupt01>;
 //    using ICE_MSP_SPI_DATA_UCA0SOMI         = GPIOA<0x5, GPIOOption::Output0>;
     using ICE_MSP_SPI_DATA_UCA0SOMI         = GPIOA<0x5, GPIOOption::Input, GPIOOption::Resistor0>;
     using ICE_MSP_SPI_CLK_MANUAL            = GPIOA<0x6, GPIOOption::Output1>;
@@ -28,7 +29,8 @@ struct Pin {
     using ICE_MSP_SPI_AUX_DIR               = GPIOA<0xA, GPIOOption::Output1>;
     using VDD_SD_EN                         = GPIOA<0xB, GPIOOption::Output0>;
     using VDD_B_EN_                         = GPIOA<0xC, GPIOOption::Output1>;
-    using MOTION_SIGNAL                     = GPIOA<0xD, GPIOOption::Input>;
+//    using MOTION_SIGNAL                     = GPIOA<0xD, GPIOOption::Input>;
+    using MOTION_SIGNAL                     = GPIOA<0xD, GPIOOption::Interrupt01>;
     
     using DEBUG_OUT                         = GPIOA<0xE, GPIOOption::Output0>;
 };
@@ -39,23 +41,31 @@ using Clock = ClockType<XT1FreqHz, MCLKFreqHz>;
 
 static volatile bool Event = false;
 
-__attribute__((interrupt(PORT1_VECTOR)))
-void _isr_port1() {
-    
+__attribute__((interrupt(PORT2_VECTOR)))
+void _isr_port2() {
     for (int i=0;; i++) {
         Pin::DEBUG_OUT::Write(i&1);
     }
-    
-    // Accessing `P1IV` automatically clears the highest-priority interrupt
-    switch (__even_in_range(P1IV, P1IV__P1IFG7)) {
-    case P1IV__P1IFG4:
-        Event = true;
-        __bic_SR_register_on_exit(LPM3_bits);
-        break;
-    default:
-        break;
-    }
 }
+
+
+//__attribute__((interrupt(PORT1_VECTOR)))
+//void _isr_port1() {
+//    
+//    for (int i=0;; i++) {
+//        Pin::DEBUG_OUT::Write(i&1);
+//    }
+//    
+//    // Accessing `P1IV` automatically clears the highest-priority interrupt
+//    switch (__even_in_range(P1IV, P1IV__P1IFG7)) {
+//    case P1IV__P1IFG4:
+//        Event = true;
+//        __bic_SR_register_on_exit(LPM3_bits);
+//        break;
+//    default:
+//        break;
+//    }
+//}
 
 #pragma mark - Main
 
@@ -92,114 +102,33 @@ int main() {
     const bool wokeFromLPM35 = (SYSRSTIV == SYSRSTIV_LPM5WU);
     
     if (wokeFromLPM35) {
-        __bis_SR_register(GIE);
+//        for (int i=0; i<1000/5; i++) {
+//            _sleepMs(5);
+//            Pin::DEBUG_OUT::Write(i&1);
+//        }
         
-        if (Event) {
-            // Woke from LPM3.5
-            for (int i=0; i<1000; i++) {
-                _sleepMs(1);
-                Pin::DEBUG_OUT::Write(i&1);
-            }
-        } else {
-            // Woke from LPM3.5
-            for (int i=0; i<1000; i++) {
-                _sleepMs(5);
-                Pin::DEBUG_OUT::Write(i&1);
-            }
-        }
+        __bis_SR_register(GIE);
     
     } else {
         // Cold start
-        for (int i=0; i<500; i++) {
-            _sleepMs(10);
-            Pin::DEBUG_OUT::Write(i&1);
-        }
+        _sleepMs(3000);
         
-        P1IFG = 0;
+//        for (int i=0; i<500/5; i++) {
+//            _sleepMs(10);
+//            Pin::DEBUG_OUT::Write(i&1);
+//        }
+        
         P2IFG = 0;
         
         __bis_SR_register(GIE);
     }
     
-//    if (Event) {
-//        for (int i=0; i<100000; i++) {
-//            Pin::DEBUG_OUT::Write(i&1);
-//        }
-//    }
+//    // Disable regulator so we enter LPM3.5 (instead of just LPM3)
+//    PMMCTL0_H = PMMPW_H; // Open PMM Registers for write
+//    PMMCTL0_L |= PMMREGOFF;
     
-//    bool first = true;
-    for (;;) {
-        // Disable ints while we check for events
-        __bic_SR_register(GIE);
-        
-        if (!Event) {
-//            if (first) {
-//                // We don't have an event, so this should be a cold start (ie we
-//                // didn't wake up from LPM3.5)
-//                Assert(SYSRSTIV != SYSRSTIV_LPM5WU);
-//                first = false;
-//            }
-            
-//            P1IFG |= (1<<4);
-            
-//            // Artificially trigger an interrupt
-//            Pin::ICE_MSP_SPI_DATA_UCA0SOMI::Write(1);
-//            Pin::ICE_MSP_SPI_DATA_UCA0SOMI::Write(0);
-            
-            // Disable regulator so we enter LPM3.5 (instead of just LPM3)
-            PMMCTL0_H = PMMPW_H; // Open PMM Registers for write
-            PMMCTL0_L |= PMMREGOFF;
-            
-            // Go to sleep in LPM3.5
-            __bis_SR_register(GIE | LPM3_bits);
-        }
-        
-//        if (first) {
-//            // We do have an event, so we should have woken up from LPM3.5
-//            Assert(SYSRSTIV == SYSRSTIV_LPM5WU);
-//            first = false;
-//        }
-        
-        // Clear event flag
-        Event = false;
-        // Re-enable interrupts while we handle the event
-        __bis_SR_register(GIE);
-        
-//        Pin::DEBUG_OUT::Write(Debug);
-        
-//        do {
-//            // Disable ints while we check for events
-//            __bic_SR_register(GIE);
-//            
-//            if (!Event)
-//            
-//            // Check for events
-//            if (Event) {
-//                // Enable ints while we handle the event
-//                __bis_SR_register(GIE);
-//                
-//                Pin::DEBUG_OUT::Write(Debug);
-//                Debug = !Debug;
-//                Event = false;
-//            }
-//            
-//            // Enable ints while we check for events
-//            __bis_SR_register(GIE);
-//        }
-//        
-//        _sleepMs(50);
-////        __bic_SR_register(GIE);
-//        
-//        P1IFG |= (1<<4);
-//        
-//        
-//        // Disable regulator so we enter LPM3.5 (instead of just LPM3)
-//        PMMCTL0_H = PMMPW_H; // Open PMM Registers for write
-//        PMMCTL0_L |= PMMREGOFF;
-//        
-//        // Go to sleep in LPM3.5
-//        __bis_SR_register(GIE | LPM3_bits);
-    }
+    // Go to sleep in LPM3.5
+    __bis_SR_register(GIE | LPM3_bits);
     
     return 0;
 }
