f48e174c40188380033fe2d2078f7037f9c8b2bb
Dave <dave@toaster.llc>
2021-12-09 19:50:35 -0800

    MSPApp:
      - Linker.ld: use a wildcard for .fram_info.* / .ram_backup.* to workaround a gcc error
        ("xxx causes a section type conflict with yyy")
    
      - Startup::ColdStart(): return true if the current invocation is the first run after
        writing FRAM, or if PMMIFG.PMMLPM5IFG==0. This allows a cold start to be triggered
        whenever a program is re-loaded.
    
      - Startup::_startup(): switch back to only copying BAKMEM if it's a cold start, since
        that was the correct behavior
    
      - Move _SD object into .ram_backup section so it persists across LPM3.5

--- Code/MSP430/MSPApp/Startup.h
+++ Code/MSP430/MSPApp/Startup.h
@@ -1,9 +1,20 @@
 #pragma once
 #include <msp430.h>
+#include "GPIO.h"
+#include "FRAMWriteEn.h"
 
 class Startup {
 public:
     static bool ColdStart() {
+        __attribute__((section(".fram_info.startup")))
+        static bool _Init = false;
+        
+        if (!_Init) {
+            FRAMWriteEn writeEn; // Enable FRAM writing
+            _Init = true;
+            return true;
+        }
+        
         return !(PMMIFG & PMMLPM5IFG);
     }
     
@@ -19,11 +30,31 @@ private:
     // sense.
     //
     // See the `crt0.S` file in the newlib project for more info.
-    __attribute__((section(".crt_0401_startup"), naked, used))
+    __attribute__((section(".crt_0401.startup"), naked, used))
     static void _startup() {
-        extern uint8_t _ram_backup_src[];
-        extern uint8_t _ram_backup_dststart[];
-        extern uint8_t _ram_backup_dstend[];
-        memcpy(_ram_backup_dststart, _ram_backup_src, _ram_backup_dstend-_ram_backup_dststart);
+        // Toggle pin A.E to signal that _startup() was called
+//        {
+//            WDTCTL = WDTPW | WDTHOLD;
+//            PM5CTL0 &= ~LOCKLPM5;
+//            
+//            using DEBUG_OUT = GPIO::PortA::Pin<0xE, GPIO::Option::Output0>;
+//            DEBUG_OUT::Init();
+//            for (int i=0; i<10; i++) {
+//                DEBUG_OUT::Write(0);
+//                for (volatile uint16_t i=0; i<10000; i++);
+//                DEBUG_OUT::Write(1);
+//                for (volatile uint16_t i=0; i<10000; i++);
+//            }
+//        }
+        
+        // Only copy the data from FRAM -> BAKMEM if this is a cold start.
+        // Otherwise, BAKMEM content should remain untouched, because it's
+        // supposed to persist during sleep.
+        if (ColdStart()) {
+            extern uint8_t _ram_backup_src[];
+            extern uint8_t _ram_backup_dststart[];
+            extern uint8_t _ram_backup_dstend[];
+            memcpy(_ram_backup_dststart, _ram_backup_src, _ram_backup_dstend-_ram_backup_dststart);
+        }
     }
 };
