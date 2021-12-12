1137ea8f6776e22932a2bc7ca35c7b476387a0f5
Dave <dave@toaster.llc>
2021-12-11 21:31:18 -0800

    Progress

---
+++ Code/MSP430/ThreadTest-LaunchpadFR/Debug__GNU/subdir_vars.mk
@@ -0,0 +1,40 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../main.cpp 
+
+CMD_SRCS += \
+../lnk_msp430fr2433.cmd 
+
+O_SRCS += \
+../main.o 
+
+ELF_SRCS += \
+../msp430fr2433.elf 
+
+LD_SRCS += \
+../Linker.ld \
+../msp430fr2433.ld 
+
+OBJS += \
+./main.o 
+
+CPP_DEPS += \
+./main.d 
+
+OBJS__QUOTED += \
+"main.o" 
+
+CPP_DEPS__QUOTED += \
+"main.d" 
+
+CPP_SRCS__QUOTED += \
+"../main.cpp" 
+
+ELF_SRCS__QUOTED += \
+"../msp430fr2433.elf" 
+
+
