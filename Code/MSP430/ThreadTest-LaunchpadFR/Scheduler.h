641dd5b8c763c168609bf1d2caa739b7923fad1a
Dave <dave@toaster.llc>
2022-01-07 10:52:45 -0800

    ThreadTest-LaunchpadFR: get compiling again

--- Code/MSP430/ThreadTest-LaunchpadFR/Scheduler.h
+++ Code/MSP430/ThreadTest-LaunchpadFR/Scheduler.h
@@ -6,12 +6,14 @@ class TaskA;
 class TaskB;
 
 void _Sleep();
+void _Error(uint16_t);
 
 using Scheduler = Toastbox::Scheduler<
     512,                                        // T_UsPerTick: microseconds per tick
     Toastbox::IntState::SetInterruptsEnabled,   // T_SetInterruptsEnabled: function to change interrupt state
     _Sleep,                                     // T_Sleep: function to put processor to sleep;
                                                 //          invoked when no tasks have work to do
+    _Error,                                     // T_Error: function to call upon an unrecoverable error (eg stack overflow)
     nullptr,                                    // T_MainStack: main stack pointer (only used to monitor
                                                 //              main stack for overflow; unused if T_StackGuardCount==0)
     0,                                          // T_StackGuardCount: number of pointer-sized stack guard elements to use
