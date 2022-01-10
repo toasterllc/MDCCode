23de7859a115e7bf63fd4fc07ea479984afd90e0
Dave <dave@toaster.llc>
2022-01-10 15:49:28 -0800

    Add initial Tools/MDCStudio project

---
+++ Tools/MDCStudio/Source/AppDelegate.mm
@@ -0,0 +1,11 @@
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+@end
+
+@interface AppDelegate ()
+@property(weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+@end
