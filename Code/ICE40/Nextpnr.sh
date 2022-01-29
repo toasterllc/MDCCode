e4ae1234f78289f1ac682e99db5bd51be006f93e
Dave <dave@toaster.llc>
2022-01-29 15:37:27 -0800

    Nextpnr.sh: cleanup

--- Code/ICE40/Nextpnr.sh
+++ Code/ICE40/Nextpnr.sh
@@ -27,11 +27,10 @@ if [ ! -n "$pkg" ]; then echo "Bad package" ; exit 1; fi
 proj="$3"
 if [ ! -n "$proj" ]; then echo "Bad project name" ; exit 1; fi
 
-if [ -f "./$proj/NextpnrArgs.sh" ]; then
-    . ./$proj/NextpnrArgs.sh
+projNextpnrScript="./$proj/NextpnrArgs.sh"
+if [ -f "$projNextpnrScript" ]; then
+    . "$projNextpnrScript"
 fi
-# echo "${projNextpnrArgs[*]}"
-# exit 1
 
 outputASCFilePath="$4"
 
@@ -44,103 +43,6 @@ args=(
     --pcf-allow-unconstrained
     
     ${projNextpnrArgs[@]}
-    
-    # --placer-heap-alpha         0.4
-    # --placer-heap-beta          0.525
-    # --placer-heap-critexp       6
-    # --placer-heap-timingweight  26
-    
-    # --placer-heap-alpha         0.225
-    # --placer-heap-beta          0.9
-    # --placer-heap-critexp       6
-    # --placer-heap-timingweight  26
-    
-    # --placer-heap-alpha         0.125
-    # --placer-heap-beta          0.825
-    # --placer-heap-critexp       5
-    # --placer-heap-timingweight  21
-    
-    # Parameters generated via:
-    #   cd ~/repos/MDC/Code/ICE40/MSPApp/Synth
-    #   ~/repos/nextpnr-anneal/tunners/nextpnr-mango.py nextpnr-ice40 --hx8k --package bg121:4k --json Top.json --pcf ../../Pins.pcf --pcf-allow-unconstrained
-    
-    # --placer-heap-alpha         0.025
-    # --placer-heap-beta          0.5
-    # --placer-heap-critexp       1
-    # --placer-heap-timingweight  8
-    
-    # ICEApp.SDController.clk_slow                         626.57   626.57   626.57   626.57
-    # ICEApp.readoutfifo_r_clk_$glb_clk                    131.96   131.96   131.96   131.96
-    # ICEApp.sd_clk_int                                    365.23   365.23   365.23   365.23
-    # ICEApp.spi_clk_$glb_clk                              103.01   103.01   103.01   103.01
-    # ice_img_clk16mhz$SB_IO_IN                            121.21   121.21   121.21   121.21
-    # img_dclk$SB_IO_IN_$glb_clk                           137.36   137.36   137.36   137.36
-    # ram_clk$SB_IO_OUT_$glb_clk                           122.29   122.29   122.29   122.29
-    
-    # --placer-heap-alpha         0.425
-    # --placer-heap-beta          0.875
-    # --placer-heap-critexp       8
-    # --placer-heap-timingweight  1
-    
-    
-    
-    
-    # ICEApp.SDController.clk_slow                         626.57   626.57   626.57   626.57
-    # ICEApp.readoutfifo_r_clk_$glb_clk                    135.46   135.46   135.46   135.46
-    # ICEApp.sd_clk_int                                    327.55   327.55   327.55   327.55
-    # ICEApp.spi_clk_$glb_clk                              113.33   113.33   113.33   113.33
-    # ice_img_clk16mhz$SB_IO_IN                            129.80   129.80   129.80   129.80
-    # img_dclk$SB_IO_IN_$glb_clk                           126.42   126.42   126.42   126.42
-    # ram_clk$SB_IO_OUT_$glb_clk                           137.49   137.49   137.49   137.49
-    
-    # --placer-heap-alpha         0.075
-    # --placer-heap-beta          0.55
-    # --placer-heap-critexp       9
-    # --placer-heap-timingweight  11
-    
-    
-    
-    
-    # ICEApp.SDController.clk_slow                         626.57   626.57   626.57   626.57
-    # ICEApp.readoutfifo_r_clk_$glb_clk                    126.47   126.47   126.47   126.47
-    # ICEApp.sd_clk_int                                    365.23   365.23   365.23   365.23
-    # ICEApp.spi_clk_$glb_clk                              116.56   116.56   116.56   116.56
-    # ice_img_clk16mhz$SB_IO_IN                            123.62   123.62   123.62   123.62
-    # img_dclk$SB_IO_IN_$glb_clk                           135.45   135.45   135.45   135.45
-    # ram_clk$SB_IO_OUT_$glb_clk                           134.77   134.77   134.77   134.77
-    
-    # --placer-heap-alpha         0.075
-    # --placer-heap-beta          0.55
-    # --placer-heap-critexp       9
-    # --placer-heap-timingweight  9
-    
-    
-    # ICEAppMSP
-    # ICEApp.SDController.clk_slow                         626.57   626.57   626.57   626.57
-    # ICEApp.readoutfifo_r_clk_$glb_clk                    132.70   132.70   132.70   132.70
-    # ICEApp.sd_clk_int                                    365.23   365.23   365.23   365.23
-    # ICEApp.spi_clk_$glb_clk                              116.25   116.25   116.25   116.25
-    # ice_img_clk16mhz$SB_IO_IN                            120.90   120.90   120.90   120.90
-    # img_dclk$SB_IO_IN_$glb_clk                           132.59   132.59   132.59   132.59
-    # ram_clk$SB_IO_OUT_$glb_clk                           133.26   133.26   133.26   133.26
-    
-    # --placer-heap-alpha         0.075
-    # --placer-heap-beta          0.55
-    # --placer-heap-critexp       9
-    # --placer-heap-timingweight  8
-    
-    
-    # ICEAppSDReadoutSTM
-    # Clk                                                   Min      Max      Avg      Med
-    # --------------------------------------------------------------------------------------
-    # ICEApp.SDController.clk_slow                         626.57   626.57   626.57   626.57
-    # ICEApp.readoutfifo_prop_clk_$glb_clk                 137.49   137.49   137.49   137.49
-    # ICEApp.readoutfifo_r_clk_$glb_clk                     93.21    93.21    93.21    93.21
-    # ICEApp.sd_clk_int                                    365.23   365.23   365.23   365.23
-    # --placer-heap-alpha         0.5
-    # --placer-heap-beta          0.675
-    # --placer-heap-critexp       9
-    # --placer-heap-timingweight  21
 )
 
 if [ ! -z "$outputASCFilePath" ]; then
