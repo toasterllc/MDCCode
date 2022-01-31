45f6bc5c5c2ea34bd4ddb5e9fbd3b779feca9d0a
Dave <dave@toaster.llc>
2022-01-31 15:24:12 -0800

    Update NextpnrArgs.sh for ICEAppMSP/ICEAppSDReadoutSTM/ICEAppImgCaptureSTM
    * ICEAppMSP
    
    Clk                                                   Min      Max      Avg      Med
    --------------------------------------------------------------------------------------
    ICEApp.SDController.clk_slow                         626.57   626.57   626.57   626.57
    ICEApp.readoutfifo_r_clk_$glb_clk                    143.49   143.49   143.49   143.49
    ICEApp.sd_clk_int                                    365.23   365.23   365.23   365.23
    ICEApp.spi_clk_$glb_clk                              113.33   113.33   113.33   113.33
    ice_img_clk16mhz$SB_IO_IN                            132.84   132.84   132.84   132.84
    img_dclk$SB_IO_IN_$glb_clk                           135.28   135.28   135.28   135.28
    ram_clk$SB_IO_OUT_$glb_clk                           134.77   134.77   134.77   134.77
    
    * ICEAppSDReadoutSTM
    
    Clk                                                   Min      Max      Avg      Med
    --------------------------------------------------------------------------------------
    ICEApp.SDController.clk_slow                         626.57   626.57   626.57   626.57
    ICEApp.readoutfifo_prop_clk_$glb_clk                 139.65   139.65   139.65   139.65
    ICEApp.readoutfifo_r_clk_$glb_clk                    117.12   117.12   117.12   117.12
    ICEApp.sd_clk_int                                    365.23   365.23   365.23   365.23
    
    * ICEAppImgCaptureSTM
    
    Clk                                                   Min      Max      Avg      Med
    --------------------------------------------------------------------------------------
    ICEApp.readoutfifo_r_clk_$glb_clk                    118.40   118.40   118.40   118.40
    ice_img_clk16mhz$SB_IO_IN_$glb_clk                   121.94   121.94   121.94   121.94
    img_dclk$SB_IO_IN_$glb_clk                           130.46   130.46   130.46   130.46
    ram_clk$SB_IO_OUT_$glb_clk                           130.94   130.94   130.94   130.94

--- Code/ICE40/ICEAppSDReadoutSTM/NextpnrArgs.sh
+++ Code/ICE40/ICEAppSDReadoutSTM/NextpnrArgs.sh
@@ -1,14 +1,16 @@
 # ICEAppSDReadoutSTM
+# 
 # Clk                                                   Min      Max      Avg      Med
 # --------------------------------------------------------------------------------------
 # ICEApp.SDController.clk_slow                         626.57   626.57   626.57   626.57
-# ICEApp.readoutfifo_prop_clk_$glb_clk                 137.49   137.49   137.49   137.49
-# ICEApp.readoutfifo_r_clk_$glb_clk                     93.21    93.21    93.21    93.21
+# ICEApp.readoutfifo_prop_clk_$glb_clk                 139.65   139.65   139.65   139.65
+# ICEApp.readoutfifo_r_clk_$glb_clk                    117.12   117.12   117.12   117.12
 # ICEApp.sd_clk_int                                    365.23   365.23   365.23   365.23
 
+
 projNextpnrArgs=(
-    --placer-heap-alpha         0.5
-    --placer-heap-beta          0.675
-    --placer-heap-critexp       9
-    --placer-heap-timingweight  21
+    --placer-heap-alpha         0.05
+    --placer-heap-beta          0.725
+    --placer-heap-critexp       2
+    --placer-heap-timingweight  11
 )
