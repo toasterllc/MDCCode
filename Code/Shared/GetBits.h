294a3d67fbd401d6b3be6bf697d4092370494653
Dave <dave@toaster.llc>
2022-02-01 14:00:14 -0800

    Make GetBits a templated function

--- Code/Shared/GetBits.h
+++ Code/Shared/GetBits.h
@@ -1,20 +1,23 @@
 #include <cstdint>
 
-static bool GetBit(const uint8_t* bytes, size_t len, uint8_t idx) {
-    AssertArg(idx < len*8);
-    const uint8_t byteIdx = len-(idx/8)-1;
-    const uint8_t bitIdx = idx%8;
+template<uint8_t T_Idx, size_t T_Len>
+static bool GetBit(const uint8_t(&bytes)[T_Len]) {
+    static_assert(T_Idx < T_Len*8);
+    const uint8_t byteIdx = T_Len-(T_Idx/8)-1;
+    const uint8_t bitIdx = T_Idx%8;
     const uint8_t bitMask = 1<<bitIdx;
     return bytes[byteIdx] & bitMask;
 }
 
-static uint64_t GetBits(const uint8_t* bytes, size_t len, uint8_t start, uint8_t end) {
-    AssertArg(start < len*8);
-    AssertArg(start >= end);
-    const uint8_t leftByteIdx = len-(start/8)-1;
-    const uint8_t leftByteMask = (1<<((start%8)+1))-1;
-    const uint8_t rightByteIdx = len-(end/8)-1;
-    const uint8_t rightByteMask = ~((1<<(end%8))-1);
+template<uint8_t T_Start, uint8_t T_End, size_t T_Len>
+static uint64_t GetBits(const uint8_t(&bytes)[T_Len]) {
+    static_assert(T_Start < T_Len*8);
+    static_assert(T_Start >= T_End);
+    
+    const uint8_t leftByteIdx = T_Len-(T_Start/8)-1;
+    const uint8_t leftByteMask = (1<<((T_Start%8)+1))-1;
+    const uint8_t rightByteIdx = T_Len-(T_End/8)-1;
+    const uint8_t rightByteMask = ~((1<<(T_End%8))-1);
     uint64_t r = 0;
     for (uint8_t i=leftByteIdx; i<=rightByteIdx; i++) {
         uint8_t tmp = bytes[i];
@@ -23,8 +26,8 @@ static uint64_t GetBits(const uint8_t* bytes, size_t len, uint8_t start, uint8_t
         if (i == rightByteIdx)  tmp &= rightByteMask;
         // Make space for the incoming bits
         if (i == rightByteIdx) {
-            tmp >>= end%8; // Shift right the number of unused bits
-            r <<= 8-(end%8); // Shift left the number of used bits
+            tmp >>= T_End%8; // Shift right the number of unused bits
+            r <<= 8-(T_End%8); // Shift left the number of used bits
         } else {
             r <<= 8;
         }
