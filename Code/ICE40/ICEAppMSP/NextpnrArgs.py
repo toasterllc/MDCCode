248dea53adad1c751fe4c2b15fd1a3ba052e43f0
Dave <dave@toaster.llc>
2022-02-05 23:48:55 -0800

    Add ICEAppMSP/Clocks.py and NextpnrArgs.py

---
+++ Code/ICE40/ICEAppMSP/NextpnrArgs.py
@@ -0,0 +1,20 @@
+# * ICEAppMSP
+# ----------------------------------------------------------
+# Clk                                                   Freq
+# ----------------------------------------------------------
+# ICEApp.SDController.clk_slow                         149.66
+# ICEApp.readoutfifo_r_clk_$glb_clk                    131.72
+# ICEApp.sd_clk_int                                    365.23
+# ICEApp.spi_clk_$glb_clk                              104.92
+# ice_img_clk16mhz$SB_IO_IN                            123.30
+# img_dclk$SB_IO_IN_$glb_clk                           137.16
+# ram_clk$SB_IO_OUT_$glb_clk                           136.50
+
+['--placer-heap-alpha',
+ '0.17500000000000002',
+ '--placer-heap-beta',
+ '0.525',
+ '--placer-heap-critexp',
+ '1',
+ '--placer-heap-timingweight',
+ '31']
