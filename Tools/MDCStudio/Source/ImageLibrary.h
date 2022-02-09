e04f779a6ae7bbb6b3f92a62ce5ea6ab3da72c07
Dave <dave@toaster.llc>
2022-02-08 21:53:39 -0800

    RecordStore.h: make RecordStore index extensible by returning a ifstream/ofstream from read()/write()
    This allows ImageLibrary.h to read/write additional data, while having it tightly coupled with the RecordStore itself.

--- Tools/MDCStudio/Source/ImageLibrary.h
+++ Tools/MDCStudio/Source/ImageLibrary.h
@@ -24,10 +24,147 @@ struct [[gnu::packed]] ImageRef {
     uint8_t thumbData[ThumbWidth*ThumbHeight*ThumbPixelSize];
 };
 
-class ImageLibrary : public RecordStore<ImageRef::Version, ImageRef, 512> {
+class ImageLibrary : private RecordStore<ImageRef::Version, ImageRef, 512> {
 public:
+//    using Path = std::filesystem::path;
     using RecordStore::RecordStore;
-    std::mutex lock;
+    
+    void read() {
+        auto lock = std::unique_lock(_lock);
+        
+        // Reset our state, as RecordStore::read() does
+        _state = {};
+        
+        try {
+            std::ifstream f = RecordStore::read();
+            
+            _SerializedState state;
+            f.read((char*)&state, sizeof(state));
+            
+            if (state.version != _Version) {
+                throw Toastbox::RuntimeError("invalid state version (expected: 0x%jx, got: 0x%jx)",
+                    (uintmax_t)_Version,
+                    (uintmax_t)state.version
+                );
+            }
+            
+            _state.imgIdEnd = state.imgIdEnd;
+            
+        } catch (const std::exception& e) {
+            printf("Recreating ImageLibrary; cause: %s\n", e.what());
+        }
+    }
+    
+    void write() {
+        auto lock = std::unique_lock(_lock);
+        std::ofstream f = RecordStore::write();
+        const _SerializedState state {
+            .version = _Version,
+            .imgIdEnd = _state.imgIdEnd,
+        };
+        f.write((char*)&state, sizeof(state));
+    }
+    
+    size_t recordCount() {
+        auto lock = std::unique_lock(_lock);
+        return RecordStore::recordCount();
+    }
+    
+    void reserve(size_t count) {
+        auto lock = std::unique_lock(_lock);
+        RecordStore::reserve(count);
+    }
+    
+    // add(): adds the records previously reserved via reserve()
+    void add() {
+        auto lock = std::unique_lock(_lock);
+        RecordStore::add();
+        
+        // Update imgIdEnd
+        _state.imgIdEnd = (!RecordStore::empty() ? RecordStore::recordGet(RecordStore::back())->id+1 : 0);
+    }
+    
+    void remove(RecordRefConstIter begin, RecordRefConstIter end) {
+        auto lock = std::unique_lock(_lock);
+        RecordStore::remove(begin, end);
+    }
+    
+    RecordRefConstIter begin() {
+        auto lock = std::unique_lock(_lock);
+        return RecordStore::begin();
+    }
+    
+    RecordRefConstIter end() {
+        auto lock = std::unique_lock(_lock);
+        return RecordStore::end();
+    }
+    
+    ImageRef* recordGet(const RecordRef& ref) {
+        auto lock = std::unique_lock(_lock);
+        return RecordStore::recordGet(ref);
+    }
+    
+    ImageRef* recordGet(RecordRefConstIter iter) {
+        auto lock = std::unique_lock(_lock);
+        return RecordStore::recordGet(iter);
+    }
+    
+    Img::Id imgIdEnd() {
+        auto lock = std::unique_lock(_lock);
+        return _state.imgIdEnd;
+    }
+    
+private:
+    static constexpr uint32_t _Version = 0;
+    
+    struct [[gnu::packed]] _SerializedState {
+        uint32_t version = 0;
+        Img::Id imgIdEnd = 0;
+    };
+    
+    std::mutex _lock;
+    struct {
+        Img::Id imgIdEnd = 0;
+    } _state;
+    
+//    ImageLibrary(const Path& path) : RecordStore(path) {
+//        
+//    }
+//    
+//private:
+//    static constexpr uint32_t _Version = 0;
+//    
+//    std::mutex _lock; // Protects this object
+//    Img::Id _imgIdEnd = 0;
+//    
+//    struct [[gnu::packed]] _SerializedState {
+//        uint32_t version = 0;
+//        Img::Id imgIdEnd = 0;
+//    };
+//    
+//    static Path _StatePath(const Path& path) { return Path(path).replace_extension(".state"); }
+//    
+//    static _SerializedState _SerializedStateRead(const Path& path) {
+//        const Mmap mmap(_StatePath(path));
+//        
+//        const _SerializedState& state = *mmap.data<_SerializedState>(0);
+//        
+//        if (state.version != _Version) {
+//            throw Toastbox::RuntimeError("invalid state version (expected: 0x%jx, got: 0x%jx)",
+//                (uintmax_t)_Version,
+//                (uintmax_t)state.version
+//            );
+//        }
+//        
+//        return state;
+//    }
+//    
+//    static void _SerializedStateWrite(const Path& path, const _SerializedState& state) {
+//        std::ofstream f;
+//        f.exceptions(std::ofstream::failbit | std::ofstream::badbit);
+//        f.open(_StatePath(path));
+//        f.write((char*)&state, sizeof(state));
+//    }
 };
 
 using ImageLibraryPtr = std::shared_ptr<ImageLibrary>;
