00e7331d235320ba9dd1aa786594182ed3419824
Dave <dave@toaster.llc>
2022-02-10 19:37:59 -0800

    MDCStudio: MainView: resizing source list working

--- Tools/MDCStudio/Source/Util.h
+++ Tools/MDCStudio/Source/Util.h
@@ -10,4 +10,20 @@ T* DynamicCast(id obj) {
 
 #define DynamicCastProtocol(proto, obj) (id<proto>)([obj conformsToProtocol:@protocol(proto)] ? obj : nil)
 
+template <typename Fn>
+inline void TrackMouse(NSWindow* win, NSEvent* ev, Fn fn) {
+    for (;;) @autoreleasepool {
+        const bool done = ([ev type] == NSEventTypeLeftMouseUp);
+        fn(ev, done);
+        if (done) {
+            // The mouse-up event needs to propogate into the app to complement the
+            // mouse-down event. (The internal Cocoa APIs expect it.)
+            [win sendEvent:ev];
+            break;
+        }
+        
+        ev = [win nextEventMatchingMask:(NSEventMaskLeftMouseDown|NSEventMaskLeftMouseDragged|NSEventMaskLeftMouseUp)];
+    }
+}
+
 } // namespace MDCStudio
