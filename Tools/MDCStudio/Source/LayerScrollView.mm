984bf79367543e1146fb8c909be179cd19787d31
Dave <dave@toaster.llc>
2022-02-25 13:16:01 -0800

    Debugging ScrollViewTest

--- Tools/MDCStudio/Source/LayerScrollView.mm
+++ Tools/MDCStudio/Source/LayerScrollView.mm
@@ -17,7 +17,8 @@
     _layer = layer;
     CALayer* rootLayer = [CALayer new];
     [rootLayer addSublayer:_layer];
-//    [rootLayer setBackgroundColor:[[[NSColor redColor] colorWithAlphaComponent:.1] CGColor]];
+    [rootLayer setContents:[NSImage imageNamed:@"TestImage"]];
+    [rootLayer setBackgroundColor:[[[NSColor redColor] colorWithAlphaComponent:.1] CGColor]];
     
     NSView* documentView = [self documentView];
     [documentView setLayer:rootLayer];
@@ -34,30 +35,30 @@
     }
 }
 
-- (void)setFrame:(NSRect)frame {
-    [super setFrame:frame];
-    
-    // Keep the content anchored
-    // This keeps the content in place when resizing the containing window, which looks better
-    NSWindow* win = [self window];
-    NSView* doc = [self documentView];
-    NSClipView* clip = [self contentView];
-    
-    const CGPoint anchorPointWant = _anchorPointDocument;
-    const CGPoint anchorPointHave = [doc convertPoint:[win convertPointFromScreen:_anchorPointScreen] fromView:nil];
-    const CGPoint delta = {
-        anchorPointWant.x-anchorPointHave.x,
-        anchorPointWant.y-anchorPointHave.y,
-    };
-    
-    CGRect bounds = [clip bounds];
-    bounds.origin.x += delta.x;
-    bounds.origin.y += delta.y;
-    bounds = [clip constrainBoundsRect:bounds];
-    
-    [super scrollClipView:clip toPoint:bounds.origin];
-    [self reflectScrolledClipView:clip];
-}
+//- (void)setFrame:(NSRect)frame {
+//    [super setFrame:frame];
+//    
+//    // Keep the content anchored
+//    // This keeps the content in place when resizing the containing window, which looks better
+//    NSWindow* win = [self window];
+//    NSView* doc = [self documentView];
+//    NSClipView* clip = [self contentView];
+//    
+//    const CGPoint anchorPointWant = _anchorPointDocument;
+//    const CGPoint anchorPointHave = [doc convertPoint:[win convertPointFromScreen:_anchorPointScreen] fromView:nil];
+//    const CGPoint delta = {
+//        anchorPointWant.x-anchorPointHave.x,
+//        anchorPointWant.y-anchorPointHave.y,
+//    };
+//    
+//    CGRect bounds = [clip bounds];
+//    bounds.origin.x += delta.x;
+//    bounds.origin.y += delta.y;
+//    bounds = [clip constrainBoundsRect:bounds];
+//    
+//    [super scrollClipView:clip toPoint:bounds.origin];
+//    [self reflectScrolledClipView:clip];
+//}
 
 // MARK: - NSView Overrides
 
@@ -96,7 +97,7 @@
         _layerFrame = frame;
         _layerMagnification = mag;
         [_layer setFrame:_layerFrame];
-        [_layer setTranslation:_layerFrame.origin magnification:_layerMagnification];
+//        [_layer setTranslation:_layerFrame.origin magnification:_layerMagnification];
         [_layer setNeedsDisplay];
     }
 }
