333a8e8e086d9e398f6f67f0c0a41b9f2c9d08db
Dave <dave@toaster.llc>
2022-02-25 20:50:54 -0800

    Comments, update notes

--- Tools/MDCStudio/Source/LayerScrollView.h
+++ Tools/MDCStudio/Source/LayerScrollView.h
@@ -7,6 +7,10 @@
 
 @interface LayerScrollView : NSScrollView
 - (void)setScrollLayer:(CALayer<LayerScrollViewLayer>*)layer;
+
 - (void)magnifyToFit;
+// -magnifyToFitIfNeeded: only performs the magnify if the content is already nearly the
+// size of the container. Visually this appears to the user as if the content 'snaps' to
+// the size of the container when the content is within a threshold of the container size.
 - (void)magnifyToFitIfNeeded;
 @end
