edbbdfd7510dcbf37f1862df8d07c5e728fd79dc
Dave <dave@toaster.llc>
2022-08-30 13:04:29 -0700

    Thumbnail experiment

--- Tools/MDCStudio/Source/Document/Document.mm
+++ Tools/MDCStudio/Source/Document/Document.mm
@@ -40,6 +40,124 @@ using namespace MDCStudio;
     return @"MDCStudio";
 }
 
+
+
+
+static void _addImages(ImageLibraryPtr imgLib, MDCTools::Renderer& renderer, const uint8_t* data, size_t imgCount, SD::BlockIdx blockIdx) {
+    using namespace MDCTools;
+    using namespace MDCStudio::ImagePipeline;
+    
+    ImageId imageId = 0;
+    {
+        auto lock = std::unique_lock(*imgLib);
+        
+        // Reserve space for `imgCount` additional images
+        imgLib->reserve(imgCount);
+        
+        // Load `imageId` by looking at the last record's image id +1, and reserve space
+        if (imgLib->recordCount()) {
+            imageId = imgLib->recordGet(imgLib->back())->ref.id+1;
+        }
+    }
+    
+    Img::Id deviceImgIdLast = 0;
+    for (size_t idx=0; idx<imgCount; idx++) {
+        const uint8_t* imgData = data+idx*ImgSD::ImgPaddedLen;
+        const Img::Header& imgHeader = *(const Img::Header*)imgData;
+        // Accessing `imgLib` without a lock because we're the only entity using the image library's reserved space
+        const auto recordRefIter = imgLib->reservedBegin()+idx;
+        ImageThumb& imageThumb = *imgLib->recordGet(recordRefIter);
+        ImageRef& imageRef = imageThumb.ref; // Safe without a lock because we're the only entity using the image library's reserved space
+        
+        // Validate checksum
+        const uint32_t checksumExpected = ChecksumFletcher32(imgData, Img::ChecksumOffset);
+        uint32_t checksumGot = 0;
+        memcpy(&checksumGot, imgData+Img::ChecksumOffset, Img::ChecksumLen);
+        if (checksumGot != checksumExpected) {
+            throw Toastbox::RuntimeError("invalid checksum (expected:0x%08x got:0x%08x)", checksumExpected, checksumGot);
+        } else {
+            printf("Checksum OK\n");
+        }
+        
+        // Populate ImageRef fields
+        {
+            imageRef.id = imageId;
+            
+            // If the image has an absolute time, use it
+            // If the image has a relative time (ie time since device boot), drop it
+            if (imgHeader.timestamp & MSP::TimeAbsoluteBase) {
+                imageRef.timestamp = MSP::UnixTimeFromTime(imgHeader.timestamp);
+            }
+            
+            imageRef.addr           = blockIdx;
+            
+            imageRef.imageWidth     = imgHeader.imageWidth;
+            imageRef.imageHeight    = imgHeader.imageHeight;
+            
+            imageRef.coarseIntTime  = imgHeader.coarseIntTime;
+            imageRef.analogGain     = imgHeader.analogGain;
+            
+            imageId++;
+            blockIdx += ImgSD::ImgBlockCount;
+        }
+        
+        // Render the thumbnail into imageRef.thumbData
+        {
+            constexpr CFADesc _CFADesc = {
+                CFAColor::Green, CFAColor::Red,
+                CFAColor::Blue, CFAColor::Green,
+            };
+            
+            const ImageLibrary::Chunk& chunk = *recordRefIter->chunk;
+            
+            Pipeline::RawImage rawImage = {
+                .cfaDesc = _CFADesc,
+                .width = Img::PixelWidth,
+                .height = Img::PixelHeight,
+                .pixels = (ImagePixel*)(imgData+Img::PixelsOffset),
+            };
+            
+            const Pipeline::Options pipelineOpts = {
+                .reconstructHighlights  = { .en = true, },
+                .debayerLMMSE           = { .applyGamma = true, },
+            };
+            
+            Pipeline::Result renderResult = Pipeline::Run(renderer, rawImage, pipelineOpts);
+            const size_t thumbDataOff = (uintptr_t)&imageThumb.thumb - (uintptr_t)chunk.mmap.data();
+            
+            constexpr MTLResourceOptions BufOpts = MTLResourceCPUCacheModeDefaultCache | MTLResourceStorageModeShared;
+            id<MTLBuffer> buf = [renderer.dev newBufferWithBytesNoCopy:(void*)chunk.mmap.data() length:chunk.mmap.len() options:BufOpts deallocator:nil];
+            
+            const RenderThumb::Options thumbOpts = {
+                .thumbWidth = ImageThumb::ThumbWidth,
+                .thumbHeight = ImageThumb::ThumbHeight,
+                .dataOff = thumbDataOff,
+            };
+            
+            RenderThumb::RGB3FromTexture(renderer, thumbOpts, renderResult.txt, buf);
+        }
+        
+        deviceImgIdLast = imgHeader.id;
+    }
+    
+    // Make sure all rendering is complete before adding the images to the library
+    renderer.commitAndWait();
+    
+    {
+        auto lock = std::unique_lock(*imgLib);
+        // Add the records that we previously reserved
+        imgLib->add();
+        // Update the device's image id 'end' == last image id that we've observed from the device +1
+        imgLib->setDeviceImgIdEnd(deviceImgIdLast+1);
+    }
+}
+
+
+
+
+
+
+
 - (void)sourceListViewSelectionChanged:(SourceListView*)sourceListView {
 //    auto selection = [_sourceListView selection];
 //    if (selection.device) {
@@ -71,7 +189,7 @@ using namespace MDCStudio;
 //            ImageCachePtr ic;
 //        };
 //        
-//        ImageLibraryPtr il = std::make_shared<MDCTools::Lockable<ImageLibrary>>(std::filesystem::path("/Users/dave/Library/Application Support/com.heytoaster.MDCStudio/Devices/337336593137") / "ImageLibrary");
+//        ImageLibraryPtr il = std::make_shared<MDCTools::Lockable<ImageLibrary>>(std::filesystem::path("/Users/dave/Library/Application Support/com.heytoaster.MDCStudio/Devices/205132485632") / "ImageLibrary");
 //        il->read();
 //        
 //        ImageCachePtr ic = std::make_shared<ImageCache>(il, [] (const ImageRef& imageRef) { return nullptr; });
@@ -101,7 +219,7 @@ using namespace MDCStudio;
 //            ImageCachePtr ic;
 //        };
 //        
-//        ImageLibraryPtr il = std::make_shared<MDCTools::Lockable<ImageLibrary>>(std::filesystem::path("/Users/dave/Desktop/ImageLibrary"));
+//        ImageLibraryPtr il = std::make_shared<MDCTools::Lockable<ImageLibrary>>(std::filesystem::path("/Users/dave/Library/Application Support/com.heytoaster.MDCStudio3/Devices/205132485632") / "ImageLibrary");
 //        il->read();
 //        
 //        ImageCachePtr ic = std::make_shared<ImageCache>(il, [] (const ImageRef& imageRef) { return nullptr; });
@@ -115,6 +233,52 @@ using namespace MDCStudio;
 //    }
     
     
+    {
+        class FakeImageSource : public ImageSource {
+        public:
+            ImageLibraryPtr imageLibrary() override {
+                return il;
+            }
+            
+            ImageCachePtr imageCache() override {
+                return ic;
+            }
+            
+            ImageLibraryPtr il;
+            ImageCachePtr ic;
+        };
+        
+        ImageLibraryPtr il = std::make_shared<MDCTools::Lockable<ImageLibrary>>(std::filesystem::path("/Users/dave/Desktop/ImageLibraryTest"));
+        il->read();
+        
+        std::thread t([=] {
+            Toastbox::Mmap mmap("/Users/dave/Desktop/SDImagesRaw-512");
+            
+            id<MTLDevice> device = MTLCreateSystemDefaultDevice();
+            if (!device) throw std::runtime_error("MTLCreateSystemDefaultDevice returned nil");
+            MDCTools::Renderer renderer(device, [device newDefaultLibrary], [device newCommandQueue]);
+            
+            constexpr size_t ImageCount = 16;
+            _addImages(il, renderer, mmap.data(), ImageCount, 0);
+        });
+        t.detach();
+        
+        ImageCachePtr ic = std::make_shared<ImageCache>(il, [] (const ImageRef& imageRef) { return nullptr; });
+        
+        auto imageSource = std::make_shared<FakeImageSource>();
+        imageSource->il = il;
+        imageSource->ic = ic;
+        
+        ImageGridView* imageGridView = [[ImageGridView alloc] initWithImageSource:imageSource];
+        [_mainView setContentView:imageGridView animation:MainViewAnimation::None];
+    }
+    
+    
+    
+    
+    
+    
+    
     
 //    {
 //        auto imgLib = std::make_shared<MDCTools::Vendor<ImageLibrary>>(std::filesystem::path("/Users/dave/Library/Application Support/com.heytoaster.MDCStudio/Devices/337336593137") / "ImageLibrary");
@@ -123,15 +287,15 @@ using namespace MDCStudio;
 //        [self setContentView:imageGridView];
 //    }
     
-    ImageSourcePtr selection = [_sourceListView selection];
-    if (selection) {
-        ImageGridView* imageGridView = [[ImageGridView alloc] initWithImageSource:selection];
-        [imageGridView setDelegate:self];
-        [_mainView setContentView:imageGridView animation:MainViewAnimation::None];
-    
-    } else {
-        [_mainView setContentView:nil animation:MainViewAnimation::None];
-    }
+//    ImageSourcePtr selection = [_sourceListView selection];
+//    if (selection) {
+//        ImageGridView* imageGridView = [[ImageGridView alloc] initWithImageSource:selection];
+//        [imageGridView setDelegate:self];
+//        [_mainView setContentView:imageGridView animation:MainViewAnimation::None];
+//    
+//    } else {
+//        [_mainView setContentView:nil animation:MainViewAnimation::None];
+//    }
 }
 
 // _openImage: open a particular image id, or an image offset from a particular image id
