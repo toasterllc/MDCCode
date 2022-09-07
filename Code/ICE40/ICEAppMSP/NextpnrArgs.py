cb14c691b8f81b2d9013de246d4bcb20044063dc
Dave <dave@toaster.llc>
2022-09-07 13:40:16 -0700

    ImgController.v: perf: try adding another state to set readout_done instead of using this `ctrl_shiftout_nextReadoutDone` technique
    * ICEAppMSP
    -----------------------------------------------------------
    Clk                                                    Freq
    -----------------------------------------------------------
    ICEApp.SDController.clk_slow                         166.78
    ICEApp.readoutfifo_r_clk_$glb_clk                    130.28 *
    ICEApp.sd_clk_int                                    365.23
    ICEApp.spi_clk_$glb_clk                              119.43
    ice_img_clk16mhz$SB_IO_IN                            124.60
    img_dclk$SB_IO_IN_$glb_clk                           135.01 *
    ram_clk$SB_IO_OUT_$glb_clk                           126.98 *

--- Code/ICE40/ICEAppMSP/NextpnrArgs.py
+++ Code/ICE40/ICEAppMSP/NextpnrArgs.py
@@ -2,19 +2,19 @@
 # -----------------------------------------------------------
 # Clk                                                    Freq
 # -----------------------------------------------------------
-# ICEApp.SDController.clk_slow                         165.23 
-# ICEApp.readoutfifo_r_clk_$glb_clk                    137.49 *
+# ICEApp.SDController.clk_slow                         166.78 
+# ICEApp.readoutfifo_r_clk_$glb_clk                    130.28 *
 # ICEApp.sd_clk_int                                    365.23 
-# ICEApp.spi_clk_$glb_clk                              117.91 
-# ice_img_clk16mhz$SB_IO_IN                            124.70 
-# img_dclk$SB_IO_IN_$glb_clk                           138.31 *
-# ram_clk$SB_IO_OUT_$glb_clk                           128.80 *
+# ICEApp.spi_clk_$glb_clk                              119.43 
+# ice_img_clk16mhz$SB_IO_IN                            124.60 
+# img_dclk$SB_IO_IN_$glb_clk                           135.01 *
+# ram_clk$SB_IO_OUT_$glb_clk                           126.98 *
 
 ['--placer-heap-alpha',
- '0.025',
+ '0.325',
  '--placer-heap-beta',
- '0.5',
+ '0.625',
  '--placer-heap-critexp',
- '3',
+ '2',
  '--placer-heap-timingweight',
- '11']
+ '31']
