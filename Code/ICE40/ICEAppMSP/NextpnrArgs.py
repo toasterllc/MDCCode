c15d5e002bb8f3b523ea6399b24e49e59d6b7a18
Dave <dave@toaster.llc>
2022-09-07 13:17:42 -0700

    Optimize ICEAppMSP
    * ICEAppMSP
    -----------------------------------------------------------
    Clk                                                    Freq
    -----------------------------------------------------------
    ICEApp.SDController.clk_slow                         165.23
    ICEApp.readoutfifo_r_clk_$glb_clk                    137.49 *
    ICEApp.sd_clk_int                                    365.23
    ICEApp.spi_clk_$glb_clk                              117.91
    ice_img_clk16mhz$SB_IO_IN                            124.70
    img_dclk$SB_IO_IN_$glb_clk                           138.31 *
    ram_clk$SB_IO_OUT_$glb_clk                           128.80 *

--- Code/ICE40/ICEAppMSP/NextpnrArgs.py
+++ Code/ICE40/ICEAppMSP/NextpnrArgs.py
@@ -2,19 +2,19 @@
 # -----------------------------------------------------------
 # Clk                                                    Freq
 # -----------------------------------------------------------
-# ICEApp.SDController.clk_slow                         166.78 
-# ICEApp.readoutfifo_r_clk_$glb_clk                    139.57 *
+# ICEApp.SDController.clk_slow                         165.23 
+# ICEApp.readoutfifo_r_clk_$glb_clk                    137.49 *
 # ICEApp.sd_clk_int                                    365.23 
-# ICEApp.spi_clk_$glb_clk                              112.88 
-# ice_img_clk16mhz$SB_IO_IN                            128.06 
-# img_dclk$SB_IO_IN_$glb_clk                           137.65 *
-# ram_clk$SB_IO_OUT_$glb_clk                           127.88 *
+# ICEApp.spi_clk_$glb_clk                              117.91 
+# ice_img_clk16mhz$SB_IO_IN                            124.70 
+# img_dclk$SB_IO_IN_$glb_clk                           138.31 *
+# ram_clk$SB_IO_OUT_$glb_clk                           128.80 *
 
 ['--placer-heap-alpha',
- '0.25',
+ '0.025',
  '--placer-heap-beta',
- '0.55',
+ '0.5',
  '--placer-heap-critexp',
- '4',
+ '3',
  '--placer-heap-timingweight',
- '16']
+ '11']
