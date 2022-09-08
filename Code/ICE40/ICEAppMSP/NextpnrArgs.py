c2dfd698dc0b5c85c6823bc3f8d01fe23a15672b
Dave <dave@toaster.llc>
2022-09-08 15:33:46 -0700

    Revert "ImgController.v: perf: experiment with removing the Delay state"
    This reverts commit 2b49ea1e90877eac089045bf2a95d1e1308d5515.

--- Code/ICE40/ICEAppMSP/NextpnrArgs.py
+++ Code/ICE40/ICEAppMSP/NextpnrArgs.py
@@ -2,19 +2,19 @@
 # -----------------------------------------------------------
 # Clk                                                    Freq
 # -----------------------------------------------------------
-# ICEApp.SDController.clk_slow                         166.78 
-# ICEApp.readoutfifo_r_clk_$glb_clk                    140.11 *
+# ICEApp.SDController.clk_slow                         165.23 
+# ICEApp.readoutfifo_r_clk_$glb_clk                    134.57 *
 # ICEApp.sd_clk_int                                    365.23 
-# ICEApp.spi_clk_$glb_clk                              114.14 
-# ice_img_clk16mhz$SB_IO_IN                            123.95 
-# img_dclk$SB_IO_IN_$glb_clk                           132.66 *
-# ram_clk$SB_IO_OUT_$glb_clk                           125.47 *
+# ICEApp.spi_clk_$glb_clk                              124.81 
+# ice_img_clk16mhz$SB_IO_IN                            114.59 
+# img_dclk$SB_IO_IN_$glb_clk                           133.07 *
+# ram_clk$SB_IO_OUT_$glb_clk                           128.52 *
 
 ['--placer-heap-alpha',
- '0.4',
+ '0.25',
  '--placer-heap-beta',
- '0.525',
+ '0.5750000000000001',
  '--placer-heap-critexp',
- '5',
+ '1',
  '--placer-heap-timingweight',
- '21']
+ '31']
