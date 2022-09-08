c88fbef0d7da6a89b1243168f22b6b80af0d12c5
Dave <dave@toaster.llc>
2022-09-08 16:44:10 -0700

    Optimize
    * ICEAppMSP
    -----------------------------------------------------------
    Clk                                                    Freq
    -----------------------------------------------------------
    ICEApp.SDController.clk_slow                         161.68
    ICEApp.readoutfifo_r_clk_$glb_clk                    127.23
    ICEApp.sd_clk_int                                    365.23
    ICEApp.spi_clk_$glb_clk                              116.24
    ice_img_clk16mhz$SB_IO_IN                            126.25
    img_dclk$SB_IO_IN_$glb_clk                           164.96
    ram_clk$SB_IO_OUT_$glb_clk                           129.68 *

--- Code/ICE40/ICEAppMSP/NextpnrArgs.py
+++ Code/ICE40/ICEAppMSP/NextpnrArgs.py
@@ -2,19 +2,19 @@
 # -----------------------------------------------------------
 # Clk                                                    Freq
 # -----------------------------------------------------------
-# ICEApp.SDController.clk_slow                         183.02 
-# ICEApp.readoutfifo_r_clk_$glb_clk                    131.72 *
+# ICEApp.SDController.clk_slow                         161.68 
+# ICEApp.readoutfifo_r_clk_$glb_clk                    127.23 
 # ICEApp.sd_clk_int                                    365.23 
-# ICEApp.spi_clk_$glb_clk                               97.52 
-# ice_img_clk16mhz$SB_IO_IN                            127.94 
-# img_dclk$SB_IO_IN_$glb_clk                           149.97 *
-# ram_clk$SB_IO_OUT_$glb_clk                           125.75 *
+# ICEApp.spi_clk_$glb_clk                              116.24 
+# ice_img_clk16mhz$SB_IO_IN                            126.25 
+# img_dclk$SB_IO_IN_$glb_clk                           164.96 
+# ram_clk$SB_IO_OUT_$glb_clk                           129.68 *
 
 ['--placer-heap-alpha',
  '0.375',
  '--placer-heap-beta',
- '0.5',
+ '0.5750000000000001',
  '--placer-heap-critexp',
- '4',
+ '5',
  '--placer-heap-timingweight',
- '6']
+ '31']
