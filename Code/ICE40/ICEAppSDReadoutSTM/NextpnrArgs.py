9ff85b517fc596db369d6f4b05629b94700a02fb
Dave <dave@toaster.llc>
2022-09-08 20:10:11 -0700

    Optimize
    * ICEAppSDReadoutSTM
    -----------------------------------------------------------
    Clk                                                    Freq
    -----------------------------------------------------------
    ICEApp.SDController.clk_slow                         176.03
    ICEApp.readoutfifo_prop_clk_$glb_clk                 134.64 *
    ICEApp.readoutfifo_r_clk_$glb_clk                    113.38
    ICEApp.sd_clk_int                                    365.23

--- Code/ICE40/ICEAppSDReadoutSTM/NextpnrArgs.py
+++ Code/ICE40/ICEAppSDReadoutSTM/NextpnrArgs.py
@@ -3,15 +3,15 @@
 # Clk                                                    Freq
 # -----------------------------------------------------------
 # ICEApp.SDController.clk_slow                         176.03 
-# ICEApp.readoutfifo_prop_clk_$glb_clk                 138.70 *
-# ICEApp.readoutfifo_r_clk_$glb_clk                    120.50 
+# ICEApp.readoutfifo_prop_clk_$glb_clk                 134.64 *
+# ICEApp.readoutfifo_r_clk_$glb_clk                    113.38 
 # ICEApp.sd_clk_int                                    365.23 
 
 ['--placer-heap-alpha',
- '0.05',
+ '0.2',
  '--placer-heap-beta',
- '0.5750000000000001',
+ '0.6000000000000001',
  '--placer-heap-critexp',
- '3',
+ '8',
  '--placer-heap-timingweight',
- '6']
+ '16']
