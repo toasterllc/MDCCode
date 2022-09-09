f4ee8a86d4500795534e5ebd413816782993acf4
Dave <dave@toaster.llc>
2022-09-08 17:07:41 -0700

    ImgController.v: perf: try rearranging logic
    * ICEAppMSP
    -----------------------------------------------------------
    Clk                                                    Freq
    -----------------------------------------------------------
    ICEApp.SDController.clk_slow                         166.78
    ICEApp.readoutfifo_r_clk_$glb_clk                    133.56 *
    ICEApp.sd_clk_int                                    365.23
    ICEApp.spi_clk_$glb_clk                              117.38
    ice_img_clk16mhz$SB_IO_IN                            131.37
    img_dclk$SB_IO_IN_$glb_clk                           138.16
    ram_clk$SB_IO_OUT_$glb_clk                           126.42 *

--- Code/ICE40/ICEAppMSP/NextpnrArgs.py
+++ Code/ICE40/ICEAppMSP/NextpnrArgs.py
@@ -2,19 +2,19 @@
 # -----------------------------------------------------------
 # Clk                                                    Freq
 # -----------------------------------------------------------
-# ICEApp.SDController.clk_slow                         168.35 
-# ICEApp.readoutfifo_r_clk_$glb_clk                    135.85 *
+# ICEApp.SDController.clk_slow                         166.78 
+# ICEApp.readoutfifo_r_clk_$glb_clk                    133.56 *
 # ICEApp.sd_clk_int                                    365.23 
-# ICEApp.spi_clk_$glb_clk                              102.77 
-# ice_img_clk16mhz$SB_IO_IN                            133.64 
-# img_dclk$SB_IO_IN_$glb_clk                           146.37 *
-# ram_clk$SB_IO_OUT_$glb_clk                           127.26 *
+# ICEApp.spi_clk_$glb_clk                              117.38 
+# ice_img_clk16mhz$SB_IO_IN                            131.37 
+# img_dclk$SB_IO_IN_$glb_clk                           138.16 
+# ram_clk$SB_IO_OUT_$glb_clk                           126.42 *
 
 ['--placer-heap-alpha',
- '0.025',
+ '0.375',
  '--placer-heap-beta',
- '0.5',
+ '0.5750000000000001',
  '--placer-heap-critexp',
- '1',
+ '6',
  '--placer-heap-timingweight',
- '1']
+ '26']
