c064fba94e073949bdf32f6eca3bb03c3434e6f2
Dave <dave@toaster.llc>
2022-09-08 19:20:31 -0700

    Optimize
    * ICEAppMSP
    -----------------------------------------------------------
    Clk                                                    Freq
    -----------------------------------------------------------
    ICEApp.SDController.clk_slow                         148.41
    ICEApp.readoutfifo_r_clk_$glb_clk                    130.63 *
    ICEApp.sd_clk_int                                    365.23
    ICEApp.spi_clk_$glb_clk                              112.57
    ice_img_clk16mhz$SB_IO_IN                            136.72
    img_dclk$SB_IO_IN_$glb_clk                           145.10
    ram_clk$SB_IO_OUT_$glb_clk                           127.03 *

--- Code/ICE40/ICEAppMSP/NextpnrArgs.py
+++ Code/ICE40/ICEAppMSP/NextpnrArgs.py
@@ -2,13 +2,13 @@
 # -----------------------------------------------------------
 # Clk                                                    Freq
 # -----------------------------------------------------------
-# ICEApp.SDController.clk_slow                         168.35 
-# ICEApp.readoutfifo_r_clk_$glb_clk                    135.85 *
+# ICEApp.SDController.clk_slow                         148.41 
+# ICEApp.readoutfifo_r_clk_$glb_clk                    130.63 *
 # ICEApp.sd_clk_int                                    365.23 
-# ICEApp.spi_clk_$glb_clk                              102.77 
-# ice_img_clk16mhz$SB_IO_IN                            133.64 
-# img_dclk$SB_IO_IN_$glb_clk                           146.37 *
-# ram_clk$SB_IO_OUT_$glb_clk                           127.26 *
+# ICEApp.spi_clk_$glb_clk                              112.57 
+# ice_img_clk16mhz$SB_IO_IN                            136.72 
+# img_dclk$SB_IO_IN_$glb_clk                           145.10 
+# ram_clk$SB_IO_OUT_$glb_clk                           127.03 *
 
 ['--placer-heap-alpha',
  '0.025',
@@ -17,4 +17,4 @@
  '--placer-heap-critexp',
  '1',
  '--placer-heap-timingweight',
- '1']
+ '16']
