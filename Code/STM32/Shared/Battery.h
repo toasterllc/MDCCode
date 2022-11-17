1305f431549be8ca7b7e8f68d956534411c8f002
Dave <dave@toaster.llc>
2022-11-17 13:36:08 -0800

    Sampling Vrefint working, and applying correction seems to be working
    Accuracy looks like:
    
        Fluke: 3.690
        STM32: 3.686819
        Error: (3.690-3.686819)/3.690 = 0.000862 = 0.0862%

--- Code/STM32/Shared/Battery.h
+++ Code/STM32/Shared/Battery.h
@@ -17,47 +17,92 @@ public:
     void init() {
         // Enable ADC+GPIO clocks
         {
+            __HAL_RCC_ADC1_CLK_ENABLE();
             __HAL_RCC_ADC3_CLK_ENABLE();
             __HAL_RCC_GPIOE_CLK_ENABLE();
             __HAL_RCC_GPIOF_CLK_ENABLE();
         }
         
-        // Configure global features of the ADC (clock, resolution,
-        // data alignment, number of conversions)
+        // Configure ADC1
         {
-            _adc3.Instance = ADC3;
-            _adc3.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV8;
-            _adc3.Init.Resolution = ADC_RESOLUTION_12B;
-            _adc3.Init.ScanConvMode = ADC_SCAN_DISABLE;
-            _adc3.Init.ContinuousConvMode = DISABLE;
-            _adc3.Init.DiscontinuousConvMode = DISABLE;
-            _adc3.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-            _adc3.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-            _adc3.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-            _adc3.Init.NbrOfConversion = 1;
-            _adc3.Init.DMAContinuousRequests = DISABLE;
-            _adc3.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-            HAL_StatusTypeDef hs = HAL_ADC_Init(&_adc3);
-            Assert(hs == HAL_OK);
-        }
-        
-        // Configure ADC channel rank and sample time
-        {
-            ADC_ChannelConfTypeDef cfg = {};
-            cfg.Channel = ADC_CHANNEL_9;
-            cfg.Rank = ADC_REGULAR_RANK_1;
-            cfg.SamplingTime = ADC_SAMPLETIME_480CYCLES;
-            HAL_StatusTypeDef hs = HAL_ADC_ConfigChannel(&_adc3, &cfg);
-            Assert(hs == HAL_OK);
-        }
-        
-        // Configure ADC callbacks
-        {
-            _adc3.Ctx = this;
+            // Configure global features of the ADC (clock, resolution,
+            // data alignment, number of conversions)
+            {
+                _adc1.Instance = ADC1;
+                _adc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV8;
+                _adc1.Init.Resolution = ADC_RESOLUTION_12B;
+                _adc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+                _adc1.Init.ContinuousConvMode = DISABLE;
+                _adc1.Init.DiscontinuousConvMode = DISABLE;
+                _adc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+                _adc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+                _adc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+                _adc1.Init.NbrOfConversion = 1;
+                _adc1.Init.DMAContinuousRequests = DISABLE;
+                _adc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+                HAL_StatusTypeDef hs = HAL_ADC_Init(&_adc1);
+                Assert(hs == HAL_OK);
+            }
             
-            _adc3.ConvCpltCallback = [] (ADC_HandleTypeDef* me) {
-                ((Battery*)me->Ctx)->_handleSampleDone();
-            };
+            // Configure ADC channel rank and sample time
+            {
+                ADC_ChannelConfTypeDef cfg = {};
+                cfg.Channel = ADC_CHANNEL_VREFINT;
+                cfg.Rank = ADC_REGULAR_RANK_1;
+                cfg.SamplingTime = ADC_SAMPLETIME_480CYCLES;
+                HAL_StatusTypeDef hs = HAL_ADC_ConfigChannel(&_adc1, &cfg);
+                Assert(hs == HAL_OK);
+            }
+            
+            // Configure ADC callbacks
+            {
+                _adc1.Ctx = this;
+                
+                _adc1.ConvCpltCallback = [] (ADC_HandleTypeDef* me) {
+                    ((Battery*)me->Ctx)->_handleSampleDone();
+                };
+            }
+        }
+        
+        // Configure ADC3
+        {
+            // Configure global features of the ADC (clock, resolution,
+            // data alignment, number of conversions)
+            {
+                _adc3.Instance = ADC3;
+                _adc3.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV8;
+                _adc3.Init.Resolution = ADC_RESOLUTION_12B;
+                _adc3.Init.ScanConvMode = ADC_SCAN_DISABLE;
+                _adc3.Init.ContinuousConvMode = DISABLE;
+                _adc3.Init.DiscontinuousConvMode = DISABLE;
+                _adc3.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+                _adc3.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+                _adc3.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+                _adc3.Init.NbrOfConversion = 1;
+                _adc3.Init.DMAContinuousRequests = DISABLE;
+                _adc3.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+                HAL_StatusTypeDef hs = HAL_ADC_Init(&_adc3);
+                Assert(hs == HAL_OK);
+            }
+            
+            // Configure ADC channel rank and sample time
+            {
+                ADC_ChannelConfTypeDef cfg = {};
+                cfg.Channel = ADC_CHANNEL_9;
+                cfg.Rank = ADC_REGULAR_RANK_1;
+                cfg.SamplingTime = ADC_SAMPLETIME_480CYCLES;
+                HAL_StatusTypeDef hs = HAL_ADC_ConfigChannel(&_adc3, &cfg);
+                Assert(hs == HAL_OK);
+            }
+            
+            // Configure ADC callbacks
+            {
+                _adc3.Ctx = this;
+                
+                _adc3.ConvCpltCallback = [] (ADC_HandleTypeDef* me) {
+                    ((Battery*)me->Ctx)->_handleSampleDone();
+                };
+            }
         }
         
         // Configure ADC interrupts
@@ -75,7 +120,8 @@ public:
     }
     
     void isr() {
-        ISR_HAL_ADC(&_adc3);
+        Assert(_adcActive);
+        ISR_HAL_ADC(_adcActive);
     }
     
     STM::BatteryStatus status() {
@@ -89,6 +135,17 @@ public:
     }
     
 private:
+    uint16_t _sampleADC(ADC_HandleTypeDef& adc) {
+        _adcActive = &adc;
+        
+        HAL_StatusTypeDef hs = HAL_ADC_Start_IT(&adc);
+        Assert(hs == HAL_OK);
+        
+        T_Scheduler::Wait([&] { return !_adcActive; });
+        
+        return (uint16_t)HAL_ADC_GetValue(&adc);
+    }
+    
     STM::BatteryStatus::ChargeStatus _chargeStatus() {
         return STM::BatteryStatus::ChargeStatuses::Invalid;
 //        using namespace STM;
@@ -131,27 +188,46 @@ private:
 //        
 //        return HAL_ADC_GetValue(&_adc3);
         
-        
-        _busy = true;
-        
-        HAL_StatusTypeDef hs = HAL_ADC_Start_IT(&_adc3);
-        Assert(hs == HAL_OK);
-        
-        T_Scheduler::Wait([&] { return !_busy; });
-        
-        const uint64_t sample = HAL_ADC_GetValue(&_adc3);
-        
         constexpr uint64_t SampleMax = (1<<12)-1; // 12-bit samples
-        constexpr uint64_t VoltageMaxMicrovolts = 1800000;
+        constexpr uint64_t VrefintCalMicrovolts = 3300000;
         constexpr uint64_t VoltageDividerNumer = 1000;
         constexpr uint64_t VoltageDividerDenom = 1000+1808;
-        return (sample * VoltageMaxMicrovolts * VoltageDividerDenom) / (SampleMax * VoltageDividerNumer);
+        const uint64_t VrefintCal = *VREFINT_CAL_ADDR_CMSIS; // ~1489
+        
+        const uint16_t vrefintSample = _sampleADC(_adc1); // ~2730
+        const uint16_t batSample = _sampleADC(_adc3);
+        
+//        using Factor = std::ratio<VrefintCalMicrovolts*VoltageDividerDenom, SampleMax*VoltageDividerNumer>;
+//        constexpr uint32_t FactorNumer = Factor::type::num;
+//        constexpr uint32_t FactorDenom = Factor::type::den;
+        
+        return (VrefintCal * VrefintCalMicrovolts * VoltageDividerDenom * batSample) / (vrefintSample * SampleMax * VoltageDividerNumer);
+        
+//        (VrefintCal * VrefintCalMicrovolts * VoltageDividerDenom * batSample) / (vrefintSample * SampleMax * VoltageDividerNumer);
+//        
+//        
+//        3300000*(1000+1808) / 4095*1000 = 9266400000 / 4095000 = 15840 / 7
+//          
+//        
+//        
+//        
+//        (VrefintCal * VrefintCalMicrovolts * VoltageDividerDenom * batSample) / (vrefintSample * SampleMax * VoltageDividerNumer);
+//        
+//        (1489 * 3300000 * (1000+1808) * batSample) / (2730 * 4095 * 1000);
+//        
+//        1379766960 / 1117935 = 786192/637
+//        
+//        
+//        786192/637
+//        
+//        return (sample * VoltageMaxMicrovolts * VoltageDividerDenom) / (SampleMax * VoltageDividerNumer);
     }
     
     void _handleSampleDone() {
-        _busy = false;
+        _adcActive = nullptr;
     }
     
+    ADC_HandleTypeDef _adc1;
     ADC_HandleTypeDef _adc3;
-    bool _busy = false;
+    ADC_HandleTypeDef* _adcActive = nullptr;
 };
