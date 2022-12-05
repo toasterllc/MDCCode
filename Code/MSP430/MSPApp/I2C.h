46eb5d043d433ca5eb643642cd1b2b7f93d3e59d
Dave <dave@toaster.llc>
2022-12-04 18:47:56 -0800

    MSPApp: clean up I2C peripheral

--- Code/MSP430/MSPApp/I2C.h
+++ Code/MSP430/MSPApp/I2C.h
@@ -55,34 +55,34 @@ public:
     }
     
     static void Recv(T_Msg& msg) {
-        uint16_t ev = _WaitForEvent();
+        _Event ev = _WaitForEvent();
         // Confirm that we have a START condition
-        Assert(ev == USCI_I2C_UCSTTIFG);
+        Assert(ev == _Event::Start);
         
         uint8_t* b = reinterpret_cast<uint8_t*>(&msg);
         for (size_t i=0; i<sizeof(msg); i++) {
             ev = _WaitForEvent();
             // Confirm that we received another byte
-            Assert(ev == USCI_I2C_UCRXIFG0);
+            Assert(ev == _Event::Rx);
             // Store the byte
             b[i] = UCB0RXBUF_L;
         }
         
         ev = _WaitForEvent();
         // Confirm that we have a STOP condition
-        Assert(ev == USCI_I2C_UCSTPIFG);
+        Assert(ev == _Event::Stop);
     }
     
     static void Send(const T_Msg& msg) {
-        uint16_t ev = _WaitForEvent();
+        _Event ev = _WaitForEvent();
         // Confirm that we have a START condition
-        Assert(ev == USCI_I2C_UCSTTIFG);
+        Assert(ev == _Event::Start);
         
         const uint8_t* b = reinterpret_cast<const uint8_t*>(&msg);
         for (size_t i=0; i<sizeof(msg); i++) {
             ev = _WaitForEvent();
             // Confirm that we can write another byte
-            Assert(ev == USCI_I2C_UCTXIFG0);
+            Assert(ev == _Event::Tx);
             UCB0TXBUF_L = b[i];
         }
         
@@ -90,47 +90,53 @@ public:
         for (;;) {
             ev = _WaitForEvent();
             switch (ev) {
-            // Request for more data: send 0xFF after the end of our data
-            case USCI_I2C_UCTXIFG0:
+            case _Event::Tx:
+                // Send 0xFF after the end of our data
                 UCB0TXBUF_L = 0xFF;
                 continue;
-            // STOP condition
-            case USCI_I2C_UCSTPIFG:
+            case _Event::Stop:
                 return;
-            // Unexpected event
             default:
+                // Unexpected event
                 Assert(false);
             }
         }
     }
     
     static void ISR() {
-        // We should never be called unless _Event is cleared
-        Assert(!_Event);
-        const uint16_t ev = UCB0IV;
+        // We should never be called unless _Ev is cleared
+        Assert(!_Ev);
+        const _Event ev = (_Event)UCB0IV;
         // Ignore spurious interrupts
         if (!ev) return;
         
-        _Event = ev;
+        _Ev = ev;
         // Disable I2C interrupts until current event is handled by our thread
         _I2CIntsSetEnabled(false);
     }
     
 private:
+    enum class _Event : uint16_t {
+        Start = USCI_I2C_UCSTTIFG,
+        Stop  = USCI_I2C_UCSTPIFG,
+        Rx    = USCI_I2C_UCRXIFG0,
+        Tx    = USCI_I2C_UCTXIFG0,
+    };
+    
     static void _I2CIntsSetEnabled(bool en) {
         if (en) UCB0IE = UCSTTIE | UCSTPIE | UCTXIE0 | UCRXIE0;
         else    UCB0IE = 0;
     }
     
-    static uint16_t _WaitForEvent() {
-        _Event = std::nullopt;
+    static _Event _WaitForEvent() {
+        _Ev = std::nullopt;
         // Re-enable I2C interrupts now that we're ready for an event
         _I2CIntsSetEnabled(true);
-        T_Scheduler::Wait([&] { return _Event.has_value(); });
-        return *_Event;
+        T_Scheduler::Wait([&] { return _Ev.has_value(); });
+        return *_Ev;
     }
     
-    static inline std::optional<uint16_t> _Event;
+    static inline std::optional<_Event> _Ev;
 
 #undef Assert
 };
