f69ed11d30dae924d1559ea8514b664b55a2c42f
Dave <dave@toaster.llc>
2022-12-21 19:35:13 -0800

    Optimize ICEAppMSP
    * ICEAppMSP
    -----------------------------------------------------------
    Clk                                                    Freq
    -----------------------------------------------------------
    ICEApp.SDController.clk_slow                         188.32
    ICEApp.readoutfifo_r_clk_$glb_clk                    133.82 *
    ICEApp.sd_clk_int                                    316.06
    ICEApp.spi_clk_$glb_clk                              106.33
    ice_img_clk16mhz$SB_IO_IN                            133.07
    img_dclk$SB_IO_IN_$glb_clk                           155.52 *
    ram_clk$SB_IO_OUT_$glb_clk                           128.63 *

--- Code/ICE40/ICEAppMSP/NextpnrArgs.py
+++ Code/ICE40/ICEAppMSP/NextpnrArgs.py
@@ -2,18 +2,18 @@
 # -----------------------------------------------------------
 # Clk                                                    Freq
 # -----------------------------------------------------------
-# ICEApp.SDController.clk_slow                         186.12 
-# ICEApp.readoutfifo_r_clk_$glb_clk                    139.16 *
+# ICEApp.SDController.clk_slow                         188.32 
+# ICEApp.readoutfifo_r_clk_$glb_clk                    133.82 *
 # ICEApp.sd_clk_int                                    316.06 
-# ICEApp.spi_clk_$glb_clk                              112.88 
-# ice_img_clk16mhz$SB_IO_IN                            114.78 
-# img_dclk$SB_IO_IN_$glb_clk                           146.84 *
-# ram_clk$SB_IO_OUT_$glb_clk                           124.53 *
+# ICEApp.spi_clk_$glb_clk                              106.33 
+# ice_img_clk16mhz$SB_IO_IN                            133.07 
+# img_dclk$SB_IO_IN_$glb_clk                           155.52 *
+# ram_clk$SB_IO_OUT_$glb_clk                           128.63 *
 
 ['--placer-heap-alpha',
- '0.17500000000000002',
+ '0.025',
  '--placer-heap-beta',
- '0.625',
+ '0.55',
  '--placer-heap-critexp',
  '6',
  '--placer-heap-timingweight',
