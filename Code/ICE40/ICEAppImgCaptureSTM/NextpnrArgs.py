81d6ba33ea537ef4b267d8158d7911ded89f9616
Dave <dave@toaster.llc>
2022-12-25 13:40:16 -0800

    Optimize ICEAppImgCaptureSTM
    * ICEAppImgCaptureSTM
    -----------------------------------------------------------
    Clk                                                    Freq
    -----------------------------------------------------------
    ICEApp.readoutfifo_r_clk_$glb_clk                    117.52
    ice_img_clk16mhz$SB_IO_IN                            114.14
    img_dclk$SB_IO_IN_$glb_clk                           145.62 *
    ram_clk$SB_IO_OUT_$glb_clk                           127.71 *

--- Code/ICE40/ICEAppImgCaptureSTM/NextpnrArgs.py
+++ Code/ICE40/ICEAppImgCaptureSTM/NextpnrArgs.py
@@ -2,16 +2,16 @@
 # -----------------------------------------------------------
 # Clk                                                    Freq
 # -----------------------------------------------------------
-# ICEApp.readoutfifo_r_clk_$glb_clk                    132.45 
-# ice_img_clk16mhz$SB_IO_IN                            122.68 
-# img_dclk$SB_IO_IN_$glb_clk                           146.37 *
-# ram_clk$SB_IO_OUT_$glb_clk                           130.87 *
+# ICEApp.readoutfifo_r_clk_$glb_clk                    117.52 
+# ice_img_clk16mhz$SB_IO_IN                            114.14 
+# img_dclk$SB_IO_IN_$glb_clk                           145.62 *
+# ram_clk$SB_IO_OUT_$glb_clk                           127.71 *
 
 ['--placer-heap-alpha',
- '0.05',
+ '0.025',
  '--placer-heap-beta',
- '0.875',
+ '0.5',
  '--placer-heap-critexp',
- '8',
+ '1',
  '--placer-heap-timingweight',
- '31']
+ '21']
