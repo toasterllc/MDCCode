827404df85483493b76b0c51957e5020ad2395fe
Dave <dave@toaster.llc>
2023-02-23 12:57:10 -0800

    ICE40: Update SynthOptAll.sh

--- Code/ICE40/SynthOptAll.sh
+++ Code/ICE40/SynthOptAll.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
 
-./Synth.py --dev hx8k --pkg bg121:4k --proj ICEAppMSP --opt
-./Synth.py --dev hx8k --pkg bg121:4k --proj ICEAppSDReadoutSTM --opt
-./Synth.py --dev hx8k --pkg bg121:4k --proj ICEAppImgCaptureSTM --opt
+if [ "$#" -ne 1 ]; then
+    echo "Usage:"
+    echo "  SynthOptAll.sh <OptimizeIterations>"
+    exit 1
+fi
+
+optIter="$1"
+
+./Synth.py --dev hx8k --pkg bg121:4k --proj ICEAppMSP --opt "$optIter"
+./Synth.py --dev hx8k --pkg bg121:4k --proj ICEAppSDReadoutSTM --opt "$optIter"
+./Synth.py --dev hx8k --pkg bg121:4k --proj ICEAppImgCaptureSTM --opt "$optIter"
 
 cat ICEAppMSP/NextpnrArgs.py            | grep '# ' | sed -e 's/# //g'
 echo
