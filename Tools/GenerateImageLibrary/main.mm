ca785eaa757bdd3e5da95e3825d339b6dcde6ea6
Dave <dave@toaster.llc>
2023-03-05 15:57:41 -0800

    GenerateImageLibrary: writing BC7 at decent speed

--- Tools/GenerateImageLibrary/main.mm
+++ Tools/GenerateImageLibrary/main.mm
@@ -6,6 +6,7 @@
 #import "Toastbox/Mmap.h"
 #import "ImageLibrary.h"
 #import "bc7e_ispc.h"
+#import "rdo_bc_encoder.h"
 namespace fs = std::filesystem;
 using namespace MDCStudio;
 using namespace MDCTools;
@@ -61,44 +62,6 @@ static uintptr_t _CeilToPageSize(uintptr_t x) {
 //    
 //    renderer.debugShowTexture(debugTxt);
 //}
-
-struct color_quad_u8
-{
-	uint8_t m_c[4];
-	
-	inline color_quad_u8(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
-	{
-		set(r, g, b, a);
-	}
-
-	inline color_quad_u8(uint8_t y = 0, uint8_t a = 255)
-	{
-		set(y, a);
-	}
-
-	inline color_quad_u8 &set(uint8_t y, uint8_t a = 255)
-	{
-		m_c[0] = y;
-		m_c[1] = y;
-		m_c[2] = y;
-		m_c[3] = a;
-		return *this;
-	}
-	
-	inline color_quad_u8 &set(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
-	{
-		m_c[0] = r;
-		m_c[1] = g;
-		m_c[2] = b;
-		m_c[3] = a;
-		return *this;
-	}
-
-	inline uint8_t &operator[] (uint32_t i) { assert(i < 4);  return m_c[i]; }
-	inline uint8_t operator[] (uint32_t i) const { assert(i < 4); return m_c[i]; }
-
-	inline int get_luma() const { return (13938U * m_c[0] + 46869U * m_c[1] + 4729U * m_c[2] + 32768U) >> 16U; } // REC709 weightings
-};
 
 int main(int argc, const char* argv[]) {
 //    DebugShowThumb(40000-1);
@@ -178,16 +141,14 @@ int main(int argc, const char* argv[]) {
             // Encode thumbnails with BC7
             constexpr size_t BytesPerRow = ImageThumb::ThumbWidth*4;
             constexpr size_t BytesPerImage = ImageThumb::ThumbWidth*ImageThumb::ThumbHeight*4;
-            image_u8 srcImg;
+            utils::image_u8 srcImg;
             
+            struct bc7_block
+            {
+                uint64_t m_vals[2];
+            };
+            using bc7_block_vec = std::vector<bc7_block>;
             
-            
-            
-            const uint32_t blocks_x = source_image.width() / 4;
-            const uint32_t blocks_y = source_image.height() / 4;
-
-            bc7_block_vec packed_image(blocks_x * blocks_y);
-
             // Initialize the BC7 compressor (only need to call once). 
             // If you don't call this function (say by accident), the compressor will always return all-0 blocks.
             ispc::bc7e_compress_block_init();
@@ -223,80 +184,11 @@ int main(int argc, const char* argv[]) {
                 break;
             }
             
-            for (int32_t by = 0; by < static_cast<int32_t>(blocks_y); by++)
-            {
-                // Process 64 blocks at a time, for efficient SIMD processing.
-                // Ideally, N >= 8 (or more) and (N % 8) == 0.
-                const int N = 64;
-
-                for (uint32_t bx = 0; bx < blocks_x; bx += N)
-                {
-                    const uint32_t num_blocks_to_process = std::min<uint32_t>(blocks_x - bx, N);
-
-                    color_quad_u8 pixels[16 * N];
-
-                    // Extract num_blocks_to_process 4x4 pixel blocks from the source image and put them into the pixels[] array.
-                    for (uint32_t b = 0; b < num_blocks_to_process; b++)
-                        source_image.get_block(bx + b, by, 4, 4, pixels + b * 16);
-                    
-                    // Compress the blocks to BC7.
-                    // Note: If you've used Intel's ispc_texcomp, the input pixels are different. BC7E requires a pointer to an array of 16 pixels for each block.
-                    bc7_block *pBlock = &packed_image[bx + by * blocks_x];
-                    ispc::bc7e_compress_blocks(num_blocks_to_process, reinterpret_cast<uint64_t *>(pBlock), reinterpret_cast<const uint32_t *>(pixels), &pack_params);
-                }
-
-                if ((by & 63) == 0)
-                    printf(".");
-            }
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
             srcImg.init(ImageThumb::ThumbWidth, ImageThumb::ThumbHeight);
             
-            rdo_bc::rdo_bc_params rp;
-            rp.m_bc7_uber_level = 0;
-            rp.m_bc7enc_max_partitions_to_scan = 64;
-            rp.m_perceptual = false;
-            rp.m_y_flip = false;
-            rp.m_bc45_channel0 = 0;
-            rp.m_bc45_channel1 = 1;
-            rp.m_bc1_mode = rgbcx::bc1_approx_mode::cBC1Ideal;
-            rp.m_use_bc1_3color_mode = true;
-            rp.m_use_bc1_3color_mode_for_black = true;
-            rp.m_bc1_quality_level = 18;
-            rp.m_dxgi_format = DXGI_FORMAT_BC7_UNORM;
-            rp.m_rdo_lambda = 0;
-            rp.m_rdo_debug_output = false;
-            rp.m_rdo_smooth_block_error_scale = 15;
-            rp.m_custom_rdo_smooth_block_error_scale = false;
-            rp.m_lookback_window_size = 128;
-            rp.m_custom_lookback_window_size = false;
-            rp.m_bc7enc_rdo_bc7_quant_mode6_endpoints = true;
-            rp.m_bc7enc_rdo_bc7_weight_modes = true;
-            rp.m_bc7enc_rdo_bc7_weight_low_frequency_partitions = true;
-            rp.m_bc7enc_rdo_bc7_pbit1_weighting = true;
-            rp.m_rdo_max_smooth_block_std_dev = 18;
-            rp.m_rdo_allow_relative_movement = false;
-            rp.m_rdo_try_2_matches = true;
-            rp.m_rdo_ultrasmooth_block_handling = true;
-            rp.m_use_hq_bc345 = true;
-            rp.m_bc345_search_rad = 5;
-            rp.m_bc345_mode_mask = 3;
-            rp.m_bc7enc_mode6_only = false;
-            rp.m_rdo_multithreading = true;
-            rp.m_bc7enc_reduce_entropy = false;
-            rp.m_use_bc7e = true;
-            rp.m_status_output = false;
-            rp.m_rdo_max_threads = 1;
-            
-            rdo_bc::rdo_bc_encoder encoder;
+            const uint32_t blocks_x = srcImg.width() / 4;
+            const uint32_t blocks_y = srcImg.height() / 4;
+            bc7_block_vec packed_image(blocks_x * blocks_y);
             
             for (size_t txtIdx=0; txtIdx<txtCount; txtIdx++) @autoreleasepool {
                 Renderer::Txt& txtDst = txtsDst.at(txtIdx);
@@ -308,18 +200,39 @@ int main(int argc, const char* argv[]) {
                 [txtDst getBytes:srcImg.get_pixels().data() bytesPerRow:BytesPerRow
                     fromRegion:MTLRegionMake2D(0, 0, ImageThumb::ThumbWidth, ImageThumb::ThumbHeight) mipmapLevel:0];
                 
-                bool br = encoder.init(srcImg, rp);
-                assert(br);
+                for (int32_t by = 0; by < static_cast<int32_t>(blocks_y); by++)
+                {
+                    // Process 64 blocks at a time, for efficient SIMD processing.
+                    // Ideally, N >= 8 (or more) and (N % 8) == 0.
+                    const int N = 64;
+                    
+                    for (uint32_t bx = 0; bx < blocks_x; bx += N)
+                    {
+                        const uint32_t num_blocks_to_process = std::min<uint32_t>(blocks_x - bx, N);
+
+                        utils::color_quad_u8 pixels[16 * N];
+
+                        // Extract num_blocks_to_process 4x4 pixel blocks from the source image and put them into the pixels[] array.
+                        for (uint32_t b = 0; b < num_blocks_to_process; b++)
+                            srcImg.get_block(bx + b, by, 4, 4, pixels + b * 16);
+                        
+                        // Compress the blocks to BC7.
+                        // Note: If you've used Intel's ispc_texcomp, the input pixels are different. BC7E requires a pointer to an array of 16 pixels for each block.
+                        bc7_block *pBlock = &packed_image[bx + by * blocks_x];
+                        ispc::bc7e_compress_blocks(num_blocks_to_process, reinterpret_cast<uint64_t *>(pBlock), reinterpret_cast<const uint32_t *>(pixels), &pack_params);
+                    }
+                }
                 
-//                auto startTime = std::chrono::steady_clock::now();
-                br = encoder.encode();
-                assert(br);
+                {
+                    NSString*const path = [NSString stringWithFormat:@"/Users/dave/Desktop/dds/debug-%zu.dds", txtIdx];
+                    static constexpr size_t pixel_format_bpp = 8;
+                    static constexpr DXGI_FORMAT fmt = DXGI_FORMAT_BC7_UNORM;
+                    bool br = utils::save_dds([path UTF8String], srcImg.width(), srcImg.height(), &packed_image[0],
+                        pixel_format_bpp, fmt, perceptual, false);
+                    assert(br);
+                }
                 
-//                NSString*const path = [NSString stringWithFormat:@"/Users/dave/Desktop/dds/debug-%zu.dds", txtIdx];
-//                static constexpr size_t pixel_format_bpp = 8;
-//                br = utils::save_dds([path UTF8String], encoder.get_orig_width(), encoder.get_orig_height(), encoder.get_blocks(),
-//                    pixel_format_bpp, rp.m_dxgi_format, rp.m_perceptual, false);
-//                assert(br);
+//                exit(0);
                 
                 
 //                auto durationMs = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now()-startTime).count();
