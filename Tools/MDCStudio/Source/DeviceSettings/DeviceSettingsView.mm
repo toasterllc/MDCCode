6c0e4db40823c6bbd3f2f253051584e5a965f493
Dave <dave@toaster.llc>
2023-03-29 12:40:34 -0700

    Implementing device settings UI

--- Tools/MDCStudio/Source/DeviceSettings/DeviceSettingsView.mm
+++ Tools/MDCStudio/Source/DeviceSettings/DeviceSettingsView.mm
@@ -3,11 +3,33 @@
 @implementation DeviceSettingsView {
     IBOutlet NSView* _nibView;
     
+    IBOutlet NSView* _timeContainerView;
+    IBOutlet NSView* _repeatIntervalContainerView;
+    
     IBOutlet NSView* _timeView;
     IBOutlet NSView* _timeRangeView;
     IBOutlet NSView* _weeklyDaySelectorView;
     IBOutlet NSView* _monthlyDaySelectorView;
     IBOutlet NSView* _yearlyDaySelectorView;
+    
+    // Time
+    IBOutlet NSPopUpButton* _repeatIntervalButton;
+    
+    // Capture
+    IBOutlet NSTextField* _captureCountField;
+    IBOutlet NSTextField* _captureIntervalField;
+    IBOutlet NSPopUpButton* _captureIntervalUnitButton;
+    
+    IBOutlet NSButton* _flashLEDCheckbox;
+    IBOutlet NSPopUpButton* _flashLEDButton;
+    
+    // Limits
+    IBOutlet NSButton* _limitTriggerCountCheckbox;
+    IBOutlet NSTextField* _limitTriggerCountField;
+    IBOutlet NSPopUpButton* _limitTriggerCountPeriodButton;
+    
+    IBOutlet NSButton* _limitTotalTriggerCountCheckbox;
+    IBOutlet NSTextField* _limitTotalTriggerCountField;
 }
 
 static void _Init(DeviceSettingsView* self) {
@@ -36,6 +58,30 @@ static void _Init(DeviceSettingsView* self) {
     if (!(self = [super initWithCoder:coder])) return nil;
     _Init(self);    
     return self;
+}
+
+- (IBAction)_action_repeatInterval:(id)sender {
+    
+}
+
+- (IBAction)_action_captureCount:(id)sender {
+    
+}
+
+- (IBAction)_action_captureInterval:(id)sender {
+    
+}
+
+- (IBAction)_action_flashLED:(id)sender {
+    
+}
+
+- (IBAction)_action_limitTriggerCount:(id)sender {
+    
+}
+
+- (IBAction)_action_limitTotalTriggerCount:(id)sender {
+    
 }
 
 @end
