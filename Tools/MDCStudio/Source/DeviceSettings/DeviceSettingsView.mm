2c76637d043d75043df9ca3a2b6a499050367198
Dave <dave@toaster.llc>
2023-03-29 11:47:49 -0700

    Implementing device settings UI

---
+++ Tools/MDCStudio/Source/DeviceSettings/DeviceSettingsView.mm
@@ -0,0 +1,41 @@
+#import "DeviceSettingsView.h"
+
+@implementation DeviceSettingsView {
+    IBOutlet NSView* _nibView;
+    
+    IBOutlet NSView* _timeView;
+    IBOutlet NSView* _timeRangeView;
+    IBOutlet NSView* _weeklyDaySelectorView;
+    IBOutlet NSView* _monthlyDaySelectorView;
+    IBOutlet NSView* _yearlyDaySelectorView;
+}
+
+static void _Init(DeviceSettingsView* self) {
+    // Load view from nib
+    {
+        [self setTranslatesAutoresizingMaskIntoConstraints:false];
+        
+        bool br = [[[NSNib alloc] initWithNibNamed:NSStringFromClass([self class]) bundle:nil] instantiateWithOwner:self topLevelObjects:nil];
+        assert(br);
+        
+        NSView* nibView = self->_nibView;
+        [nibView setTranslatesAutoresizingMaskIntoConstraints:false];
+        [self addSubview:nibView];
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[nibView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(nibView)]];
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[nibView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(nibView)]];
+    }
+}
+
+- (instancetype)initWithFrame:(NSRect)frame {
+    if (!(self = [super initWithFrame:frame])) return nil;
+    _Init(self);
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder*)coder {
+    if (!(self = [super initWithCoder:coder])) return nil;
+    _Init(self);    
+    return self;
+}
+
+@end
