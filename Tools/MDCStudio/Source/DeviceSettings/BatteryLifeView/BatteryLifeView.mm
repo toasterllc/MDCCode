085c26a94c2057d9432d4e7e35c22368a21b00e9
Dave <dave@toaster.llc>
2023-04-04 20:40:28 -0700

    Working on DeviceSettingsView

---
+++ Tools/MDCStudio/Source/DeviceSettings/BatteryLifeView/BatteryLifeView.mm
@@ -0,0 +1,42 @@
+#import "BatteryLifeView.h"
+
+@implementation BatteryLifeView {
+@public
+    IBOutlet NSView* _nibView;
+}
+
+static void _Init(BatteryLifeView* self) {
+    // Load view from nib
+    {
+        [self setTranslatesAutoresizingMaskIntoConstraints:false];
+        
+        bool br = [[[NSNib alloc] initWithNibNamed:NSStringFromClass([self class]) bundle:nil] instantiateWithOwner:self topLevelObjects:nil];
+        assert(br);
+        
+        NSView* nibView = self->_nibView;
+        [nibView setTranslatesAutoresizingMaskIntoConstraints:false];
+        [self addSubview:nibView];
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[nibView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(nibView)]];
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[nibView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(nibView)]];
+    }
+}
+
+// MARK: - Creation
+
+- (instancetype)initWithFrame:(NSRect)frame {
+    if (!(self = [super initWithFrame:frame])) return nil;
+    _Init(self);
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder*)coder {
+    if (!(self = [super initWithCoder:coder])) return nil;
+    _Init(self);
+    return self;
+}
+
+- (NSView*)deviceSettingsView_HeaderEndView {
+    return self;
+}
+
+@end
