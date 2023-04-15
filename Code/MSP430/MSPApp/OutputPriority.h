62b86d1158ec7e98322cd07a640fd9de8cb3b8cc
Dave <dave@toaster.llc>
2023-04-15 12:28:05 -0700

    OutputPriority.h: switch away from using std::bitset to save space (~100 bytes)

--- Code/MSP430/MSPApp/OutputPriority.h
+++ Code/MSP430/MSPApp/OutputPriority.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "GPIO.h"
-#include <bitset>
 
 class OutputPriority {
 public:
@@ -11,28 +10,35 @@ public:
     
     std::optional<bool> get() {
         for (size_t i=0; i<_Width; i++) {
-            if (_en[i]) return _val[i];
+            if (_Get(_en, i)) return _Get(_val, i);
         }
         return std::nullopt;
     }
     
     void set(Priority pri, std::optional<bool> val) {
-        _en[pri] = val.has_value();
-        _val[pri] = val.value_or(false);
+        _Set(_en, pri, val.has_value());
+        _Set(_val, pri, val.value_or(false));
         _update();
     }
     
 private:
     using _WriteFn = void(*)(bool);
+    using _Bits = uint8_t;
+    static constexpr size_t _Width = sizeof(Priority)*8;
+    
+    static constexpr _Bits _Mask(Priority pri) { return ((_Bits)1) << pri; }
+    static constexpr bool _Get(_Bits bits, Priority pri) { return bits & _Mask(pri); }
+    static constexpr void _Set(_Bits& bits, Priority pri, bool b) {
+        bits &= ~_Mask(pri);
+        if (b) bits |= _Mask(pri);
+    }
     
     void _update() {
         const std::optional<bool> val = get();
         if (val) _write(*val);
     }
     
-    static constexpr size_t _Width = sizeof(Priority)*8;
-    
-    std::bitset<_Width> _en;
-    std::bitset<_Width> _val;
+    _Bits _en = 0;
+    _Bits _val = 0;
     _WriteFn _write = nullptr;
 };
