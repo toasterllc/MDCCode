91ee034da44ce6c10344adcaf0b1343860b84b9a
Dave <dave@toaster.llc>
2023-04-21 14:02:29 -0700

    Assert.h: update for arm32

--- Code/Shared/Assert.h
+++ Code/Shared/Assert.h
@@ -1,19 +1,36 @@
 #pragma once
 #include <cstdint>
 
-#define _Abort()                                                        \
-    do {                                                                \
-        if constexpr (sizeof(void*) == 2) {                             \
-            /* Small memory model */                                    \
+#if defined(__MSP430__) && !defined(__LARGE_CODE_MODEL__)
+    // MSP430, small memory model
+    #define _Abort()                                                    \
+        do {                                                            \
             asm volatile("mov pc, r12" : : : );     /* r12 = $PC */     \
             asm volatile("jmp Abort" : : : );       /* call Abort() */  \
-        } else {                                                        \
-            /* Large memory model */                                    \
+            __builtin_unreachable();                                    \
+        } while (0)
+
+#elif defined(__MSP430__) && defined(__LARGE_CODE_MODEL__)
+    // MSP430, large memory model
+    #define _Abort()                                                    \
+        do {                                                            \
             asm volatile("mov.a pc, r12" : : : );   /* r12 = $PC */     \
             asm volatile("jmp Abort" : : : );       /* call Abort() */  \
-        }                                                               \
-        __builtin_unreachable();                                        \
-    } while (0)
+            __builtin_unreachable();                                    \
+        } while (0)
+
+#elif defined(__arm__)
+    // ARM32
+    #define _Abort()                                                    \
+        do {                                                            \
+            asm volatile("mov r0, pc" : : : );  /* r0 = $PC */          \
+            asm volatile("jmp Abort" : : : );   /* call Abort() */      \
+            __builtin_unreachable();                                    \
+        } while (0)
+
+#else
+        #error Task: Unsupported architecture
+#endif
 
 #define Assert(x)    if (!(x)) _Abort()
 #define AssertArg(x) if (!(x)) _Abort()
