f3db970df7200442ae3abf075ab03414202de3ba
Dave <dave@toaster.llc>
2023-04-28 21:04:14 -0700

    SysTick progress

--- Code/MSP430/MSPApp/SysTick.h
+++ Code/MSP430/MSPApp/SysTick.h
@@ -5,9 +5,6 @@
 template<typename T_Scheduler, uint32_t T_ACLKFreqHz>
 class T_SysTick {
 public:
-    static void Init() {
-    }
-    
     static void Enabled(bool x) {
         Toastbox::IntState ints(false);
         // Short-circuit if our state hasn't changed
@@ -36,42 +33,51 @@ public:
             // an incoming TAIFG=1.
             TA0CTL &= ~(MC1|MC0|TAIE);
             
-            std::ratio<16000000>
-            
-            __delay_cycles();
-            // "A delay of at least 1.5 timer clocks is required to resynchronize before
-            // restarting the timer"
-            #warning TODO: how should we handle the above comment? it'd be nice if we didn't have to burn 1.5 cycles at 32kHz, which is ~700 MCLK cycles
+//            std::ratio<16000000>
+//            
+//            __delay_cycles();
+//            // "A delay of at least 1.5 timer clocks is required to resynchronize before
+//            // restarting the timer"
+//            #warning TODO: how should we handle the above comment? it'd be nice if we didn't have to burn 1.5 cycles at 32kHz, which is ~700 MCLK cycles
         }
     }
     
     static bool Enabled() {
-        return TA0CTL & 
+        return TA0CTL & (MC1|MC0);
     }
     
     static bool ISR(uint16_t iv) {
+        switch (__even_in_range(iv, TA0IV_TAIFG)) {
+        case TA0IV_TAIFG:
+            Assert(_ISRState.state==_State::TimerInterval || _ISRState.state==_State::TimerRemainder);
+            _StateUpdate();
+            return;
+        default:
+            Assert(false);
+        }
+        
         #warning TODO: check iv
         return T_Scheduler::Tick();
     }
     
-    static void _TimerStop() {
-        // Stop the timer and mask the interrupt
-        // Masking the interrupt seems like a better idea than clearing TAIFG, in case
-        // there's a race between us clearing TAIFG + stopping the timer, and a
-        // an incoming TAIFG=1.
-        TA0CTL &= ~(MC1|MC0|TAIE);
-        
-        // "A delay of at least 1.5 timer clocks is required to resynchronize before
-        // restarting the timer"
-        #warning TODO: 
-    }
-    
-    // _Reset(): resets the timer and our state (_ISRState)
-    // Interrupts must be disabled
-    static void _Reset() {
-        _ISRState = {};
-        _TimerStop();
-    }
+//    static void _TimerStop() {
+//        // Stop the timer and mask the interrupt
+//        // Masking the interrupt seems like a better idea than clearing TAIFG, in case
+//        // there's a race between us clearing TAIFG + stopping the timer, and a
+//        // an incoming TAIFG=1.
+//        TA0CTL &= ~(MC1|MC0|TAIE);
+//        
+//        // "A delay of at least 1.5 timer clocks is required to resynchronize before
+//        // restarting the timer"
+//        #warning TODO: 
+//    }
+//    
+//    // _Reset(): resets the timer and our state (_ISRState)
+//    // Interrupts must be disabled
+//    static void _Reset() {
+//        _ISRState = {};
+//        _TimerStop();
+//    }
     
 private:
     using _Period = T_Scheduler::TicksPeriod;
