d7d9832fe64f49ec72a8200a9af10b7cbd9ba085
Dave <dave@toaster.llc>
2023-05-07 19:09:34 -0700

    Working on time calibration

--- Tools/MDCUtil/main.cpp
+++ Tools/MDCUtil/main.cpp
@@ -9,6 +9,7 @@
 #include "Toastbox/NumForStr.h"
 #include "Toastbox/DurationString.h"
 #include "Toastbox/String.h"
+#include "Toastbox/Cast.h"
 #include "ChecksumFletcher32.h"
 #include "Img.h"
 #include "SD.h"
@@ -660,15 +661,27 @@ static void MSPTimeSet(const Args& args, MDCUSBDevice& device) {
     device.mspTimeSet(state);
 }
 
-// _TimeAdjustmentCalculate: calculates the adjustment to `state` to correct it to the current time.
-//
-// The return value takes the form of a ratio: .delta / .interval, which corresponds to the
-// time adjustment per elapsed time, which equals the negative drift per elapsed time.
-//
-// .delta is constrained to [1,TicksFreq], thereby capping unadjusted drift to a maximum of 1
-// second. (Ie, the raw unadjusted time is allowed to drift up to one second before it's corrected.)
-// The implementation searches for the .delta/.interval fixed-point ratio that's closest to the
-// target floating-point ratio.
+/// _TimeAdjustmentCalculate(): calculates the adjustment to `state` to correct it to
+/// the current time, and also quantifies the drift over the total elapsed time so
+/// that the device can continuously correct the time in the future.
+///
+/// The return value consists of four values: .value, .counter, .interval, and .delta:
+///
+///   .value: is the adjustment for the current instant to correct it to the current
+///       time.
+///
+///   .counter: only used by the device; 0 is returned.
+///
+///   .delta / .interval: corresponds to the drift over the device's total elapsed
+///       time, allowing the device to continuously correct its time. This is a ratio
+///       that equals the time adjustment per elapsed time, which equals the negative
+///       drift per elapsed time.
+///
+///       .delta is constrained to [1,TicksFreq], thereby capping unadjusted drift to
+///       a maximum of one second. (Ie, the raw unadjusted time is allowed to drift up
+///       to one second before it's corrected.)
+///       The implementation searches for the .delta/.interval fixed-point ratio that's
+///       closest to the target floating-point ratio.
 static MSP::TimeAdjustment _TimeAdjustmentCalculate(const MSP::TimeState& state) {
     assert(Time::Absolute(state.start));
     assert(Time::Absolute(state.time));
@@ -681,27 +694,35 @@ static MSP::TimeAdjustment _TimeAdjustmentCalculate(const MSP::TimeState& state)
     if (state.start >= nowInstant) throw Toastbox::RuntimeError("MSP::TimeState.start invalid");
     const Time::Ticks64 elapsed = nowInstant - state.start;
     const double target = (double)drift / elapsed;
-    const int deltaSign = (nowInstant>=state.time ? 1 : -1);
-    double bestErr = INFINITY;
-    MSP::TimeAdjustment best = {};
+    struct {
+        uint64_t interval = 0;
+        uint64_t delta = 0;
+        double err = INFINITY;
+    } best;
     
     static_assert(Time::TicksFreq::den == 1); // Check assumption
-    for (int delta=1; delta<=Time::TicksFreq::num; delta++) {
-        using Interval = decltype(best.interval);
+    for (uint64_t delta=1; delta<=Time::TicksFreq::num; delta++) {
         const uint64_t interval = (elapsed * delta) / drift;
         const double err = std::abs(((double)delta/interval) - target);
-        if (err < bestErr) {
-            if (interval > std::numeric_limits<Interval>::max())
-                throw Toastbox::RuntimeError("interval too large");
-            
+        if (err < best.err) {
             best = {
-                .interval = (Interval)interval,
-                .delta = (int16_t)(deltaSign * delta),
+                .interval = interval,
+                .delta    = delta,
+                .err      = err,
             };
-            bestErr = err;
         }
     }
-    return best;
+    
+    using Value    = decltype(MSP::TimeAdjustment::value);
+    using Interval = decltype(MSP::TimeAdjustment::interval);
+    using Delta    = decltype(MSP::TimeAdjustment::delta);
+    
+    const int16_t deltaSign = (nowInstant>=state.time ? 1 : -1);
+    return {
+        .value    = Toastbox::Cast<Delta>(deltaSign * (int64_t)drift),
+        .interval = Toastbox::Cast<Interval>(best.interval),
+        .delta    = Toastbox::Cast<Delta>(deltaSign * (int16_t)best.delta),
+    };
 }
 
 static void MSPTimeAdjust(const Args& args, MDCUSBDevice& device) {
