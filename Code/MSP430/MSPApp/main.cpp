97831f37c11f61235f7f4506650574a34b987e71
Dave <dave@toaster.llc>
2023-05-11 00:57:33 -0700

    Progress

--- Code/MSP430/MSPApp/main.cpp
+++ Code/MSP430/MSPApp/main.cpp
@@ -54,7 +54,7 @@ using _Button = T_Button<_Scheduler, _Pin::BUTTON_SIGNAL_>;
 using _Charging = T_Charging<_Pin::VDD_B_3V3_STM>;
 
 static constexpr uint32_t _FlickerPeriodMs      = 5000;
-static constexpr uint32_t _FlickerOnDurationMs  = 50;
+static constexpr uint32_t _FlickerOnDurationMs  = 20;
 using _LEDFlicker = T_LEDFlicker<_Pin::LED_GREEN_, _ACLKFreqHz, _FlickerPeriodMs, _FlickerOnDurationMs>;
 
 static OutputPriority _LEDGreen_(_Pin::LED_GREEN_{});
