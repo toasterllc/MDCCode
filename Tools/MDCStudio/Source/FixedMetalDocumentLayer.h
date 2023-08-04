6ac953c8d3dc771ef01c83f04eded6e21715d612
Dave <dave@toaster.llc>
2023-08-04 16:22:24 -0700

    FixedMetalDocumentLayer.h: comment

--- Tools/MDCStudio/Source/FixedMetalDocumentLayer.h
+++ Tools/MDCStudio/Source/FixedMetalDocumentLayer.h
@@ -12,9 +12,9 @@
 - (CGFloat)fixedMagnification;
 
 // -fixedTransform: returns the matrix that incorporates the current magnification and translation.
-// The matrix converts normalized [0,1] coordinates within the receiver's full-size content
-// to normalized device coordinates (NDC) in the range [-1,1] where this NDC range x,y=[-1,1]
-// represents the visible region of the receiver, after magnification and translation.
+// The matrix converts normalized [0,1] coordinates within the receiver's full-size content to
+// normalized device coordinates (NDC), where the NDC range [-1,1] represents the visible region of
+// the receiver, after magnification and translation.
 - (simd_float4x4)fixedTransform;
 
 @end
