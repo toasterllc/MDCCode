124675c66d3d6b783c836b2b41ca66004d37d8da
Dave <dave@toaster.llc>
2023-10-13 13:46:09 -0700

    Cleanup

--- Tools/MDCStudio/Source/DeviceImageGridHeaderView/DeviceImageGridHeaderView.mm
+++ Tools/MDCStudio/Source/DeviceImageGridHeaderView/DeviceImageGridHeaderView.mm
@@ -22,49 +22,6 @@ using namespace MDCStudio;
     _delegate = x;
 }
 
-//__weak auto selfWeak = self;
-//{
-//    _device->observerAdd([=] {
-//        dispatch_async(dispatch_get_main_queue(), ^{ [selfWeak _update]; });
-//        return true;
-//    });
-//}
-//
-//// Add ourself as an observer of the image library
-//{
-//    auto lock = std::unique_lock(*_imageLibrary);
-//    _imageLibrary->observerAdd([=](const ImageLibrary::Event& ev) {
-//        dispatch_async(dispatch_get_main_queue(), ^{ [selfWeak _update]; });
-//        return true;
-//    });
-//}
-//
-//static std::optional<size_t> _LoadCount(const MDCDevice::Status& status, ImageLibrary& imgLib) {
-//    // Short-circut if syncing is in progress.
-//    // In that case, we don't want to show the 'Load' button in the header
-//    if (status.sync) return std::nullopt;
-//    
-//    {
-//        auto lock = std::unique_lock(imgLib);
-//        const Img::Id libImgIdEnd = (!imgLib.empty() ? imgLib.back()->info.id+1 : 0);
-//        
-//        // Calculate how many images to add to the end of the library: device has, lib doesn't
-//        if (libImgIdEnd > status.imgIdEnd) {
-//            #warning TODO: how do we properly handle this situation?
-//            throw Toastbox::RuntimeError("image library claims to have newer images than the device (libImgIdEnd: %ju, status.imgIdEnd: %ju)",
-//                (uintmax_t)libImgIdEnd,
-//                (uintmax_t)status.imgIdEnd
-//            );
-//        }
-//        
-//        return status.imgIdEnd - std::max(status.imgIdBegin, libImgIdEnd);
-//    }
-//}
-
-//static std::optional<size_t> _LoadCount(const MDCDevice::Status& status MDCDevicePtr device) {
-//    return _LoadCount(device->status(), device->imageLibrary());
-//}
-
 - (void)setLoadCount:(size_t)x {
     if (x) {
         [_loadPhotosCountLabel setStringValue:[NSString stringWithFormat:@"%ju", (uintmax_t)x]];
@@ -77,32 +34,6 @@ using namespace MDCStudio;
 - (void)setProgress:(float)x {
     [_progressIndicator setDoubleValue:x];
 }
-
-//- (void)_update {
-//    const MDCDevice::Status status = _device->status();
-//    
-//    // Update status
-//    {
-//        [_statusLabel setStringValue:@(ImageLibraryStatus(*_imageLibrary, "No photos").c_str())];
-//    }
-//    
-//    // Update unloaded photo count
-//    {
-//        const std::optional<size_t> loadCount = _LoadCount(status, *_imageLibrary);
-//        if (loadCount && *loadCount) {
-//            [_loadPhotosCountLabel setStringValue:[NSString stringWithFormat:@"%ju", (uintmax_t)*loadCount]];
-//            [_hideLoadPhotosConstraint setPriority:NSLayoutPriorityDefaultLow];
-//        } else {
-//            [_hideLoadPhotosConstraint setPriority:NSLayoutPriorityRequired];
-//        }
-//    }
-//    
-//    // Upload load progress
-//    {
-//        const float progress = (status.sync ? status.sync->progress : 0);
-//        [_progressIndicator setDoubleValue:progress];
-//    }
-//}
 
 - (IBAction)load:(id)sender {
     [_delegate deviceImageGridHeaderViewLoad:self];
