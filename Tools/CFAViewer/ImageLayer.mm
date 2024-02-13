4e2a706df9cd89e692d305d32e43fcd9e051500f
Dave <dave@toaster.llc>
2024-02-13 15:44:38 -0800

    Save images so we can use them for training ML

--- Tools/CFAViewer/ImageLayer.mm
+++ Tools/CFAViewer/ImageLayer.mm
@@ -5,9 +5,9 @@
 #import "Code/Lib/Toastbox/Mac/Renderer.h"
 using namespace CFAViewer;
 
-// _PixelFormat: Our pixels are in the linear RGB space (LSRGB), and need conversion to the display color space.
-// To do so, we declare that our pixels are LSRGB (ie we _don't_ use the _sRGB MTLPixelFormat variant!),
-// and we opt-in to color matching by setting the colorspace on our CAMetalLayer via -setColorspace:.
+// _PixelFormat == BGRA8Unorm with -setColorspace:LinearSRGB appears to be the correct
+// combination such that we supply color data in the linear SRGB colorspace and the
+// system handles conversion into SRGB.
 // (Without calling -setColorspace:, CAMetalLayers don't perform color matching!)
 static constexpr MTLPixelFormat _PixelFormat = MTLPixelFormatBGRA8Unorm;
 
