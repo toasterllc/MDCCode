438af88421a2e31951b7d2d4e0d4ccf3b2cca944
Dave <dave@toaster.llc>
2024-03-18 10:43:43 -0700

    Cleanup

--- Tools/MDCStudio/Source/main.mm
+++ Tools/MDCStudio/Source/main.mm
@@ -1,25 +1,12 @@
 #import <Cocoa/Cocoa.h>
 #import <filesystem>
 #import "TmpDir.h"
-#import "Code/Shared/TimeAdjustment.h"
-#import "Code/Shared/TimeString.h"
-#import <iostream>
 
 int main(int argc, const char* argv[]) {
     // Make C++ APIs locale-aware
     std::locale::global(std::locale(""));
     
     MDCStudio::TmpDir::Cleanup();
-    
-//    auto now = Time::Clock::now();
-//    auto lastWeek = now - std::chrono::hours(24*10);
-//    MSP::TimeState x = {
-//        .start = Time::Clock::TimeInstantFromTimePoint(lastWeek),
-//        .time = Time::Clock::TimeInstantFromTimePoint(now + std::chrono::milliseconds(500)),
-//    };
-//    const MSP::TimeAdjustment adj = Time::TimeAdjustmentCalculate(x);
-//    std::cout << Time::StringForTimeAdjustment(adj);
-//    exit(0);
     
 //    std::filesystem::remove_all("/Users/dave/Library/Containers/llc.toaster.photon-transfer/Data/Library/Application Support/llc.toaster.photon-transfer");
 //    std::filesystem::remove_all("/Users/dave/Desktop/DemoImageSource");
