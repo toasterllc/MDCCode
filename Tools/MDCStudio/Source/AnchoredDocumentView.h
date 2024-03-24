1ba8ac60398b328b79f3174b7d9cac31d547d53b
Dave <dave@toaster.llc>
2024-03-23 18:38:39 -0700

    Add submodule: Code/Lib/AnchoredScrollView

--- Tools/MDCStudio/Source/AnchoredDocumentView.h
+++
@@ -1,8 +0,0 @@
-#import <Cocoa/Cocoa.h>
-#import "AnchoredScrollView.h"
-
-// AnchoredDocumentView: a document view (to be used with AnchoredScrollView) that merely hosts a layer.
-// AnchoredDocumentView forwards the AnchoredScrollViewDocument protocol methods to its layer.
-@interface AnchoredDocumentView : NSView <AnchoredScrollViewDocument>
-- (instancetype)initWithAnchoredLayer:(CALayer<AnchoredScrollViewDocument>*)layer;
-@end
