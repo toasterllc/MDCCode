bdf78c9c5b5849211c275ae801e40e5485718e25
Dave <dave@toaster.llc>
2024-06-26 21:21:34 -0700

    STM32 Linux compilation with clean makefile

---
+++ Code/STM32/Makefile.mk
@@ -0,0 +1,43 @@
+OUTPUT := STApp.elf
+BUILDDIR := Build
+TOOLCHAIN := "../../../Tools/gcc-stm32"
+TOOLCHAINSETUP := $(shell $(TOOLCHAIN)/setup.sh) # Prepare toolchain
+TOOLCHAINBIN := $(TOOLCHAIN)/platform/bin
+
+SRC := $(shell find Source -name '*.cpp' -o -name '*.s')			\
+	   $(shell cd ..; find Shared -name '*.cpp' -o -name '*.s')
+
+OBJS := $(addprefix $(BUILDDIR)/, $(addsuffix .o, $(basename $(SRC))))
+
+# Link final product
+$(BUILDDIR)/$(OUTPUT): $(OBJS)
+	$(TOOLCHAINBIN)/arm-none-eabi-g++ -o $@ $(OBJS) -mcpu=cortex-m7 -T'Linker.ld' --specs=nosys.specs -Wl,-Map=$(BUILDDIR)/$(OUTPUT:.elf=.map)	\
+		-Wl,--gc-sections -static -L../Shared --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb								\
+		-Wl,--start-group -lc -lm -lstdc++ -lsupc++ -Wl,--end-group
+
+# C++ rules
+$(BUILDDIR)/Shared/%.o: ../Shared/%.cpp
+	mkdir -p $(dir $@)
+	$(TOOLCHAINBIN)/arm-none-eabi-g++ "$<" -mcpu=cortex-m7 -std=c++17 -g3 -DUSE_HAL_DRIVER -DSTM32F730xx -c -Os -ffunction-sections -fdata-sections		\
+		-fno-exceptions -fno-rtti -fno-threadsafe-statics -fno-use-cxa-atexit -Wall -std=c++1z -iquote '../../..'						\
+		-iquote '../Shared' -iquote '../Shared/ST' -iquote 'Source' -fstack-usage -MMD -MP -MF"$(@:.o=.d)" -MT"$@"						\
+		--specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+$(BUILDDIR)/Source/%.o: Source/%.cpp
+	mkdir -p $(dir $@)
+	$(TOOLCHAINBIN)/arm-none-eabi-g++ "$<" -mcpu=cortex-m7 -std=c++17 -g3 -DUSE_HAL_DRIVER -DSTM32F730xx -c -Os -ffunction-sections -fdata-sections		\
+		-fno-exceptions -fno-rtti -fno-threadsafe-statics -fno-use-cxa-atexit -Wall -std=c++1z -iquote '../../..'						\
+		-iquote '../Shared' -iquote '../Shared/ST' -iquote 'Source' -fstack-usage -MMD -MP -MF"$(@:.o=.d)" -MT"$@"						\
+		--specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+# Assembly rule
+$(BUILDDIR)/Shared/%.o: ../Shared/%.s
+	mkdir -p $(dir $@)
+	$(TOOLCHAINBIN)/arm-none-eabi-gcc -mcpu=cortex-m7 -g -c -x assembler-with-cpp -MMD -MP -MF"$(@:.o=.d)" -MT"$@" --specs=nano.specs					\
+		-mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
+clean:
+	rm -Rf Build
+
+# Include all .d files
+-include $(OBJS:%.o=%.d)
