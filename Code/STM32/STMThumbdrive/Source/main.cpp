2b19890c671571c140e5fe5d1f8ca53e9354802f
Dave <dave@toaster.llc>
2024-07-11 11:25:31 -0700

    Add delay to main

--- Code/STM32/STMThumbdrive/Source/main.cpp
+++ Code/STM32/STMThumbdrive/Source/main.cpp
@@ -186,8 +186,8 @@ int main(void)
   /* Configure the system clock */
   SystemClock_Config();
   
-  Toastbox::IntState ints(true);
-  HAL_Delay(1000);
+//  Toastbox::IntState ints(true);
+//  HAL_Delay(1000);
 
 //  const uint32_t rcc = RCC->PLLSAICFGR;
 //  const uint32_t PLLSAIN = (rcc>>6)&0x1FF;
