15b747cdf80bc7b1e30195acd3b27dc35a526c66
Dave <dave@toaster.llc>
2024-07-11 11:28:13 -0700

    SCSI cleanup

--- Code/STM32/STMThumbdrive/Source/USB/usbd_msc_scsi.c
+++ Code/STM32/STMThumbdrive/Source/USB/usbd_msc_scsi.c
@@ -120,95 +120,15 @@ static int8_t SCSI_UpdateBotData(USBD_MSC_BOT_HandleTypeDef *hmsc,
 */
 int8_t SCSI_ProcessCmd(USBD_HandleTypeDef *pdev, uint8_t lun, uint8_t *cmd)
 {
-  int8_t ret;
-  USBD_MSC_BOT_HandleTypeDef *hmsc = (USBD_MSC_BOT_HandleTypeDef *)pdev->pClassData;
-
-  switch (cmd[0])
-  {
-  case SCSI_TEST_UNIT_READY:
-    // USED
-    ret = SCSI_TestUnitReady(pdev, lun, cmd);
-    break;
-
-//  case SCSI_REQUEST_SENSE:
-//    abort();
-//    ret = SCSI_RequestSense(pdev, lun, cmd);
-//    break;
-
-  case SCSI_INQUIRY:
-    // USED
-    ret = SCSI_Inquiry(pdev, lun, cmd);
-    break;
-
-//  case SCSI_START_STOP_UNIT:
-//    abort();
-//    ret = SCSI_StartStopUnit(pdev, lun, cmd);
-//    break;
-
-  case SCSI_ALLOW_MEDIUM_REMOVAL:
-    // USED
-    ret = SCSI_AllowPreventRemovable(pdev, lun, cmd);
-    break;
-
-  case SCSI_MODE_SENSE6:
-    // USED
-    ret = SCSI_ModeSense6(pdev, lun, cmd);
-    break;
-
-//  case SCSI_MODE_SENSE10:
-//    abort();
-//    ret = SCSI_ModeSense10(pdev, lun, cmd);
-//    break;
-
-//  case SCSI_READ_FORMAT_CAPACITIES:
-//    abort();
-//    ret = SCSI_ReadFormatCapacity(pdev, lun, cmd);
-//    break;
-
-  case SCSI_READ_CAPACITY10:
-    // USED
-    ret = SCSI_ReadCapacity10(pdev, lun, cmd);
-    break;
-
-//  case SCSI_READ_CAPACITY16:
-//    abort();
-//    ret = SCSI_ReadCapacity16(pdev, lun, cmd);
-//    break;
-
-  case SCSI_READ10:
-    // USED
-    ret = SCSI_Read10(pdev, lun, cmd);
-    break;
-
-//  case SCSI_READ12:
-//    abort();
-//    ret = SCSI_Read12(pdev, lun, cmd);
-//    break;
-
-//  case SCSI_WRITE10:
-//    abort();
-//    ret = SCSI_Write10(pdev, lun, cmd);
-//    break;
-
-//  case SCSI_WRITE12:
-//    abort();
-//    ret = SCSI_Write12(pdev, lun, cmd);
-//    break;
-
-//  case SCSI_VERIFY10:
-//    abort();
-//    ret = SCSI_Verify10(pdev, lun, cmd);
-//    break;
-
-  default:
-    abort();
-    SCSI_SenseCode(pdev, lun, ILLEGAL_REQUEST, INVALID_CDB);
-    hmsc->bot_status = USBD_BOT_STATUS_ERROR;
-    ret = -1;
-    break;
+  switch (cmd[0]) {
+  case SCSI_TEST_UNIT_READY:        return SCSI_TestUnitReady(pdev, lun, cmd);
+  case SCSI_INQUIRY:                return SCSI_Inquiry(pdev, lun, cmd);
+  case SCSI_ALLOW_MEDIUM_REMOVAL:   return SCSI_AllowPreventRemovable(pdev, lun, cmd);
+  case SCSI_MODE_SENSE6:            return SCSI_ModeSense6(pdev, lun, cmd);
+  case SCSI_READ_CAPACITY10:        return SCSI_ReadCapacity10(pdev, lun, cmd);
+  case SCSI_READ10:                 return SCSI_Read10(pdev, lun, cmd);
   }
-
-  return ret;
+  abort();
 }
 
 
