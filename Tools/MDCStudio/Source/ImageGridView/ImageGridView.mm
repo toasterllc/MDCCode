c937ff86e8d8b9d0c075e19bad0851e7ca069eec
Dave <dave@toaster.llc>
2024-09-25 16:22:23 -0700

    Progress making thumb size variable

--- Tools/MDCStudio/Source/ImageGridView/ImageGridView.mm
+++ Tools/MDCStudio/Source/ImageGridView/ImageGridView.mm
@@ -268,11 +268,6 @@ static MTLTextureDescriptor* _TextureDescriptor(size_t sliceCount) {
         return it->val;
     }
     
-//    const auto chunkBegin = ImageLibrary::FindChunkBegin(ImageLibrary::BeginSorted(*_imageLibrary, _sortNewestFirst), iter);
-//    const auto chunkEnd = ImageLibrary::FindChunkEnd(ImageLibrary::EndSorted(*_imageLibrary, _sortNewestFirst), iter);
-//    assert(chunkBegin != chunkEnd);
-//    const size_t sliceCount = chunkEnd-chunkBegin;
-    
     auto startTime = std::chrono::steady_clock::now();
     
     static MTLTextureDescriptor* txtDesc = _TextureDescriptor(_imageLibrary->config().chunkRecordCap);
